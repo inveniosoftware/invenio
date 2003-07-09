22c622340f8c6792e69894561bb89497ff9ebcfd
Tibor Simko <tibor.simko@cern.ch>
2003-07-09 19:44:16 +0000

    Introduce new `ap' argument (``alternative patterns'') that governs the behaviour in case of no-exact-match-found: shall we try alternative patterns or not?

--- modules/websearch/web/search.py
+++ modules/websearch/web/search.py
@@ -46,8 +46,12 @@ __version__ = "$Id$"
 def index(req, cc=cdsname, c=None, p="", f="", rg="10", sf="", so="d", sp="", of="hb", ot="", as="0",
           p1="", f1="", m1="", op1="", p2="", f2="", m2="", op2="", p3="", f3="", m3="", sc="0", jrec="0",
           recid="-1", recidb="-1", sysno="", id="-1", idb="-1", sysnb="", action="SEARCH",
-          d1y="", d1m="", d1d="", d2y="", d2m="", d2d="", dbg="0"):
-    """Main entry point to WebSearch."""
+          d1y="", d1m="", d1d="", d2y="", d2m="", d2d="", dbg="0", ap="1"):
+    """Main entry point to WebSearch search engine.  See the docstring
+       of search_engine.perform_request_search for the detailed
+       explanation of arguments.
+    """
+    
     need_authentication = 0
     # check c
     if type(c) is list:
@@ -75,14 +79,17 @@ def index(req, cc=cdsname, c=None, p="", f="", rg="10", sf="", so="d", sp="", of
         return search_engine.perform_request_search(req, cc, c, p, f, rg, sf, so, sp, of, ot, as,
                                                     p1, f1, m1, op1, p2, f2, m2, op2, p3, f3, m3, sc, jrec,
                                                     recid, recidb, sysno, id, idb, sysnb, action,
-                                                    d1y, d1m, d1d, d2y, d2m, d2d, dbg) 
+                                                    d1y, d1m, d1d, d2y, d2m, d2d, dbg, ap) 
 
 
 def authenticate(req, cc=cdsname, c=None, p="", f="", rg="10", sf="", so="d", sp="", of="hb", ot="", as="0",
                  p1="", f1="", m1="", op1="", p2="", f2="", m2="", op2="", p3="", f3="", m3="", sc="0", jrec="0",
                  recid="-1", recidb="-1", sysno="", id="-1", idb="-1", sysnb="", action="SEARCH",
-                 d1y="", d1m="", d1d="", d2y="", d2m="", d2d="", dbg="0"):
-    """Authenticate the user before launching the search."""
+                 d1y="", d1m="", d1d="", d2y="", d2m="", d2d="", dbg="0", ap="1"):
+    """Authenticate the user before launching the search.  See the
+       docstring of search_engine.perform_request_search for the
+       detailed explanation of arguments.
+    """    
 
     __auth_realm__ = "restricted collection"
 
@@ -106,7 +113,7 @@ def authenticate(req, cc=cdsname, c=None, p="", f="", rg="10", sf="", so="d", sp
     return search_engine.perform_request_search(req, cc, c, p, f, rg, sf, so, sp, of, ot, as,
                                                 p1, f1, m1, op1, p2, f2, m2, op2, p3, f3, m3, sc, jrec,
                                                 recid, recidb, sysno, id, idb, sysnb, action,
-                                                d1y, d1m, d1d, d2y, d2m, d2d, dbg)
+                                                d1y, d1m, d1d, d2y, d2m, d2d, dbg, ap)
 
 def cache(req, action="show"):
     """Manipulates the search engine cache."""
