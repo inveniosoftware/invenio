1c09d3a05dd115604635da2cde19460a824afaf8
Tibor Simko <tibor.simko@cern.ch>
2003-12-15 20:31:40 +0000

    Added bibtaskex action. Removed SQL comments at the end. Added role/action descriptions.

--- modules/webaccess/lib/access_control_config.py
+++ modules/webaccess/lib/access_control_config.py
@@ -43,22 +43,23 @@ MAXPAGEUSERS = 25
 # default data for the add_default_settings function
 # roles
 #           name          description
-def_roles = ((SUPERADMINROLE, 'all rights'),
-             ('photoadmin',   'administrator of the photo collections'),
-             (WEBACCESSADMINROLE,     'access to web administrator interface'))
+def_roles = ((SUPERADMINROLE, 'superuser with all rights'),
+             ('photoadmin',   'Photo collection administrator'),
+             (WEBACCESSADMINROLE,     'WebAccess administrator'))
 # users
 # list of e-mail addresses
 def_users = []
 # actions
 #            name                  desc     allowedkeywords   optional
-def_actions = (('cfgwebsearch',         '',       '',              'no'),
-               ('cfgbibformat',         '',       '',              'no'),
-               ('runbibwords',          '',       '',              'no'),
-               ('runbibupload',         '',       '',              'no'),
-               ('runwebcoll',           '',       'collection',    'yes'),
-               ('runbibformat',         '',       'format',        'yes'),
-               (WEBACCESSACTION,        '',       '',              'no'),
-               (DELEGATEADDUSERROLE,    '',       'role',          'no'))
+def_actions = (('cfgwebsearch',         'configure WebSearch',       '',              'no'),
+               ('cfgbibformat',         'configure BibFormat',       '',              'no'),
+               ('runbibwords',          'run BibWords',       '',              'no'),
+               ('runbibupload',         'run BibUpload',       '',              'no'),
+               ('runwebcoll',           'run webcoll',       'collection',    'yes'),
+               ('runbibformat',         'run BibFormat',       'format',        'yes'),
+               (WEBACCESSACTION,        'configure WebAccess',       '',              'no'),
+               (DELEGATEADDUSERROLE,    'delegate subroles inside WebAccess',       'role',          'no'),
+               ('runbibtaskex',         'run BibTaskEx example',       '',              'no'))
 # authorizations
 #          role              action          arglistid  optional   arguments
 def_auths = ((SUPERADMINROLE,    'cfgwebsearch',         -1,      0,       {}),
@@ -67,37 +68,7 @@ def_auths = ((SUPERADMINROLE,    'cfgwebsearch',         -1,      0,       {}),
              (SUPERADMINROLE,    'runbibupload',         -1,      0,       {}),
              (SUPERADMINROLE,    'runbibformat',         -1,      1,       {}),
              (SUPERADMINROLE,    WEBACCESSACTION,        -1,      0,       {}), 
-             ('photoadmin',      'runwebcoll',           -1,      0,       {'collection': 'Photos'}),
-             (WEBACCESSADMINROLE,WEBACCESSACTION,        -1,      0,       {}))
+             ('photoadmin',      'runwebcoll',           -1,      0,       {'collection': 'Pictures'}),
+             (WEBACCESSADMINROLE,WEBACCESSACTION,        -1,      0,       {}),
+             (SUPERADMINROLE,    'runtaskex',            -1,      0,       {}))
 </protect>
-
-
-# # webaccess defaults, add to tabfill.sql.wml
-# from access_control_config import * 
-# # roles
-# INSERT INTO accROLE VALUES (1,SUPERADMINROLE,'all rights');
-# INSERT INTO accROLE VALUES (2,'photoadmin','administrator of the photo collections');
-# INSERT INTO accROLE VALUES (3,WEBACCESSADMINROLE,'access to web administrator interface');
-# # user_roles
-# INSERT INTO user_accROLE VALUES (1,1)
-# INSERT INTO user_accROLE VALUES (1,3)
-# # actions
-# INSERT INTO accACTION VALUES (1,'cfgwebsearch',     '','',          'no');
-# INSERT INTO accACTION VALUES (2,'cfgbibformat',     '','',          'no');
-# INSERT INTO accACTION VALUES (3,'runbibwords',      '','',          'no');
-# INSERT INTO accACTION VALUES (4,'runbibupload',     '','',          'no');
-# INSERT INTO accACTION VALUES (5,'runwebcoll',       '','collection','yes');
-# INSERT INTO accACTION VALUES (6,'runbibformat',     '','format',    'yes');
-# INSERT INTO accACTION VALUES (7,WEBACCESSACTION,    '','',          'no');
-# INSERT INTO accACTION VALUES (8,DELEGATEADDUSERROLE,'','role',      'no');
-# # arguments
-# INSERT INTO accARGUMENT VALUES (1,'collection','Photos');
-# # authorizations
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,1, 0, 0);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,2, 0, 0);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,3, 0, 0);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,4, 0, 0);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,6,-1,-1);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (1,7, 0, 0);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (2,5, 1, 1);
-# INSERT INTO accROLE_accACTION_accARGUMENT VALUES (3,7, 0, 0);
