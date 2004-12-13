dc529f1ce09aa0f9502762a4f170b6d49d761c9d
Tibor Simko <tibor.simko@cern.ch>
2004-12-13 17:30:03 +0000

    Added a basic unit test file for the stemmer.

---
+++ modules/bibindex/lib/bibindex_engine_stemmer_tests.py
@@ -0,0 +1,51 @@
+## $Id$
+## CDSware Search Engine in mod_python.
+
+## This file is part of the CERN Document Server Software (CDSware).
+## Copyright (C) 2002 CERN.
+##
+## The CDSware is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## The CDSware is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.  
+##
+## You should have received a copy of the GNU General Public License
+## along with CDSware; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+<protect># -*- coding: utf-8 -*-</protect>
+
+"""Unit tests for the indexing engine."""
+
+__lastupdated__ = """<: print `date +"%d %b %Y %H:%M:%S %Z"`; :>"""
+
+<protect> ## okay, rest of the Python code goes below #######
+
+__version__ = "$Id$"
+
+import bibindex_engine_stemmer
+import unittest
+
+class TestStemmer(unittest.TestCase):
+    """Test stemming, if available."""
+
+    def test_stemmer_none(self):
+        """bibindex engine - no stemmer"""
+        self.assertEqual("information",
+                         bibindex_engine_stemmer.stem("information", None))
+    def test_stemmer_english(self):
+        """bibindex engine - English stemmer"""
+        self.assertEqual("inform",
+                         bibindex_engine_stemmer.stem("information", "en"))
+
+def create_test_suite():
+    """Return test suite for the indexing engine."""
+    return unittest.TestSuite((unittest.makeSuite(TestStemmer,'test'),))
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(create_test_suite())
