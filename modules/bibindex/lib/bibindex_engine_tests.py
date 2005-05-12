d41001de533e934e36c826c9e679127c2514512c
Tibor Simko <tibor.simko@cern.ch>
2005-05-12 16:09:04 +0000

    Getting rid of WML.

--- modules/bibindex/lib/bibindex_engine_tests.py
+++ modules/bibindex/lib/bibindex_engine_tests.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
+##
 ## $Id$
-## CDSware Search Engine in mod_python.
-
+##
 ## This file is part of the CERN Document Server Software (CDSware).
 ## Copyright (C) 2002, 2003, 2004, 2005 CERN.
 ##
@@ -18,13 +19,7 @@
 ## along with CDSware; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-<protect># -*- coding: utf-8 -*-</protect>
-
 """Unit tests for the indexing engine."""
-
-__lastupdated__ = """<: print `date +"%d %b %Y %H:%M:%S %Z"`; :>"""
-
-<protect> ## okay, rest of the Python code goes below #######
 
 __version__ = "$Id$"
 
