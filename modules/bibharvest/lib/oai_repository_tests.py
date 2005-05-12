cfeecc47f7ae10b1c9e6d1327811ceeb5ae937fe
Tibor Simko <tibor.simko@cern.ch>
2005-05-12 16:21:52 +0000

    Getting rid of WML.

--- modules/bibharvest/lib/oai_repository_tests.py
+++ modules/bibharvest/lib/oai_repository_tests.py
@@ -1,6 +1,8 @@
+# -*- coding: utf-8 -*-
+##
 ## $Id$
 ## CDSware OAI repository unit tests.
-
+##
 ## This file is part of the CERN Document Server Software (CDSware).
 ## Copyright (C) 2002, 2003, 2004, 2005 CERN.
 ##
@@ -18,13 +20,7 @@
 ## along with CDSware; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-<protect># -*- coding: utf-8 -*-</protect>
-
 """Unit tests for the oai repository."""
-
-__lastupdated__ = """<: print `date +"%d %b %Y %H:%M:%S %Z"`; :>"""
-
-<protect> ## okay, rest of the Python code goes below #######
 
 __version__ = "$Id$"
 
