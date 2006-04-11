c08849422f97ff17d3cf47d8d7157645cc164488
Tibor Simko <tibor.simko@cern.ch>
2006-04-11 08:48:18 +0000

    Removed the old way of importing I18N messages.

--- modules/websearch/lib/websearchadminlib.py
+++ modules/websearch/lib/websearchadminlib.py
@@ -33,7 +33,6 @@ from zlib import compress,decompress
 from mod_python import apache
 
 from cdsware.bibrankadminlib import write_outcome,modify_translations,get_def_name,get_i8n_name,get_name,get_rnk_nametypes,get_languages,check_user,is_adminuser,adderrorbox,addadminbox,tupletotable,tupletotable_onlyselected,addcheckboxes,createhiddenform,serialize_via_numeric_array_dumps,serialize_via_numeric_array_compr,serialize_via_numeric_array_escape,serialize_via_numeric_array,deserialize_via_numeric_array,serialize_via_marshal,deserialize_via_marshal
-from cdsware.messages import *
 from cdsware.dbquery import run_sql
 from cdsware.config import *
 from cdsware.webpage import page, pageheaderonly, pagefooteronly
