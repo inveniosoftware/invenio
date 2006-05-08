0280bac15faf03221b232f6b59a150e70a7cc42c
Tibor Simko <tibor.simko@cern.ch>
2006-05-08 12:06:17 +0000

    English phrase cleanup.

--- modules/webcomment/lib/webcomment_config.py
+++ modules/webcomment/lib/webcomment_config.py
@@ -18,17 +18,17 @@
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-__lastupdated__ = """FIXME: last updated"""
+__lastupdated__ = """$Date$"""
 
 cfg_webcomment_error_messages = \
 {   'ERR_WEBCOMMENT_RECID_INVALID'       :  '_("%s is an invalid record ID")',
     'ERR_WEBCOMMENT_RECID_NAN'           :  '_("Record ID %s is not a number")',
     'ERR_WEBCOMMENT_UID_INVALID'         :  '_("%s is an invalid user ID")',
     'ERR_WEBCOMMENT_DB_ERROR'            :  '%s',
-    'ERR_WEBCOMMENT_COMMENTS_NOT_ALLOWED':  '_("Comments on library record have been disallowed by the Administrator")',
+    'ERR_WEBCOMMENT_COMMENTS_NOT_ALLOWED':  '_("Comments on records have been disallowed by the administrator")',
     'ERR_WEBCOMMENT_ARGUMENT_NAN'        :  '_("%s is not a number")',
     'ERR_WEBCOMMENT_ARGUMENT_INVALID'   :  '_("%s invalid argument")',
-    'ERR_WEBCOMMENT_PROGRAMMING_ERROR'   :  '_("Programming error, please inform the Administrator")',
+    'ERR_WEBCOMMENT_PROGRAMMING_ERROR'   :  '_("Programming error, please inform the administrator")',
     'ERR_WEBCOMMENT_FOR_TESTING_PURPOSES':  ' THIS IS FOR TESTING PURPOSES ONLY var1=%s var2=%s var3=%s var4=%s var5=%s var6=%s ',
     'ERR_WEBCOMMENT_REPLY_REVIEW'        :  '_("Cannot reply to a review")',
     'ERR_WEBCOMMENT_DB_INSERT_ERROR' : '_("Failed to insert your comment to the database. Please try again.")'
@@ -42,14 +42,14 @@ cfg_webcomment_warning_messages = \
     'WRN_WEBCOMMENT_FEEDBACK_RECORDED': '_("Your feedback has been recorded, many thanks")',
     'WRN_WEBCOMMENT_FEEDBACK_NOT_RECORDED': '_("Your feedback could not be recorded, please try again")',
     'WRN_WEBCOMMENT_ALREADY_VOTED': '_("Sorry, you have already voted. This vote hasn\'t been recorded.")',
-    'WRN_WEBCOMMENT_ALREADY_REPORTED': '_("You already have reported an abuse with this comment.")', 
+    'WRN_WEBCOMMENT_ALREADY_REPORTED': '_("You have already reported an abuse for this comment.")', 
     'WRN_WEBCOMMENT_ADD_NO_TITLE': '_("You must enter a title")',
     'WRN_WEBCOMMENT_ADD_NO_SCORE': '_("You must choose a score")',
     'WRN_WEBCOMMENT_ADD_NO_BODY': '_("You must enter a text")',
     'WRN_WEBCOMMENT_ADD_UNKNOWN_ACTION': '_("Unknown action --> showing you the default add comment form")',
-    'WRN_WEBCOMMENT_ADMIN_COMID_NAN': '_("comment ID must be a number, try again")',
-    'WRN_WEBCOMMENT_ADMIN_INVALID_COMID': '_("Invalid comment ID, try again")',
-    'WRN_WEBCOMMENT_ADMIN_COMID_INEXISTANT': '_("Comment ID %s does not exist, try again")',
+    'WRN_WEBCOMMENT_ADMIN_COMID_NAN': '_("comment ID must be a number")',
+    'WRN_WEBCOMMENT_ADMIN_INVALID_COMID': '_("Invalid comment ID")',
+    'WRN_WEBCOMMENT_ADMIN_COMID_INEXISTANT': '_("Comment ID %s does not exist")',
     'ERR_WEBCOMMENT_RECID_MISSING': '_("No record ID was given")',
     'ERR_WEBCOMMENT_RECID_INEXISTANT': '_("Record ID %s does not exist in the database")',
     'ERR_WEBCOMMENT_RECID_INVALID': '_("Record ID %s is an invalid ID")',
