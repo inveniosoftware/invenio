1c9637304754745f8f51e743c67356e7c4522e9b
Gregory Favre <gregory.favre@cern.ch>
2006-06-14 13:41:54 +0000

    Added new tests before proceeding to migration. Now user is warned if no migration is necessary or if needed tables are missing or already full.

--- modules/webbasket/lib/webbasket_migration_kit.py
+++ modules/webbasket/lib/webbasket_migration_kit.py
@@ -19,7 +19,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-""" Migration from webbasket [v.0.7.1] to [v.0.9.0]
+""" Migration from webbasket [v0.7.1] to [v0.9.0]
 Usage: python webbasket_migration_kit.py
 This utility will copy records and baskets from CDSware 0.7.1 to CDS Invenio 0.9
 It will check for database consistency before copying. Inconsistencies can be:
@@ -34,22 +34,26 @@ Main function: migrate_v071_baskets()
 """
 
 from MySQLdb import escape_string
-from time import localtime
 
 from invenio.dbquery import run_sql
-from invenio.webbasket_config import cfg_webbasket_share_levels, \
-                                     cfg_webbasket_actions, \
-                                     cfg_webbasket_share_levels_ordered
-from invenio.dateutils import convert_datestruct_to_datetext
+from invenio.webbasket_config import cfg_webbasket_share_levels
+
 import sys
 
 
 def migrate_v071_baskets():
     default_topic_name = ''
     default_share_level = ''
-    print "This is basket migration wizard"
+    print "Basket migration kit (for CDSware prior to v0.7.1 -> v0.90)"
     print "==============================="
-    print "In new webbasket module, baskets are stored in topics."
+    print "Checking your installation..."
+    if __check_update_possibility():
+        print "Tables are OK, please answer the next two questions in order to migrate your existing baskets"
+    else:
+        sys.exit(1)
+    print "There are %i baskets to migrate" % __count_current_baskets()
+    print "==============================="
+    print "In new WebBasket module, baskets are stored in topics."
     print 'Name of default topic [Default topic]: ', 
     default_topic_name = raw_input() or "Default topic"
     def choose_share_level():
@@ -67,7 +71,7 @@ def migrate_v071_baskets():
         default_share_level = choose_share_level()
         try:
             default_share_level = int(default_share_level)
-            if default_share_level in range(0,5):
+            if default_share_level in range(0, 5):
                 choosed = 1
             else: 
                 choosed = 0
@@ -135,7 +139,6 @@ def migrate_v071_baskets():
     nb_records = "N/A"
     try:
         nb_records = __import_records()
-        print 'ada'
     except:
         print "There was an error while importing records."
         print "Table bskREC was perhaps not empty?"
@@ -144,7 +147,12 @@ def migrate_v071_baskets():
     print "%s records haven been imported." % str(nb_records)
     print "==============================="
     print "Updating auto-increment values."
-    __set_auto_increment_value()
+    try:
+        __set_auto_increment_value()
+    except:
+        print "There was an error:"
+        print "Could not set new auto-increment value to table 'bskBASKET'"
+        sys.exit(1)
     def choose_delete():
         print "==============================="
         print "Would you like to remove old tables (type y or n)? [n]"
@@ -161,11 +169,61 @@ def migrate_v071_baskets():
         except:
             choosed = 0
     if delete == 'y':
-        __drop_baskets()
+        try:
+            __drop_baskets()
+        except:
+            print "There was an error while deleting old tables"
+            sys.exit(1)
         print "Old baskets have been erased"
     print "==============================="
     print "Migration to new basket system has been successful."
-    
+
+def __check_update_possibility():
+    """"""
+    res = run_sql("SHOW TABLES LIKE '%basket%'");
+    def v071filter(element):
+        if element[0] in ['user_basket', 'basket', 'basket_record']:
+            return 1
+        return 0
+    if len(filter(v071filter, res)) != 3:
+        print "Tables 'basket', 'user_basket' and 'basket_record' do not seem to exist."
+        print "Migration is not necessary"
+        return 0
+    res = run_sql("SHOW TABLES LIKE '%bsk%'")
+    def v090filter(element):
+        if element[0] in ['user_bskBASKET', 'bskREC', 'bskBASKET', 'usergroup_bskBASKET']:
+            return 1
+        return 0
+    if len(filter(v090filter, res)) != 4:
+        print "Tables 'bskBASKET', 'user_bskBASKET', 'usergroup_bskBASKET and 'bskREC' do not seem to exist."
+        print "Please run make create-tables in a shell"
+        return 0
+    query1 = "SELECT count(*) from bskBASKET"
+    new_bsk_nb = run_sql(query1)[0][0]
+    if new_bsk_nb:
+        print "Table bskBASKET does not seem to be empty. Migration has already been done"
+        return 0
+    query2 = "SELECT count(*) from bskREC"
+    new_rec_nb = run_sql(query2)[0][0]
+    if new_rec_nb:
+        print "Table bskREC does not seem to be empty. Migration has already been done"
+        return 0
+    query3 = "SELECT count(*) from user_bskBASKET"
+    new_ubsk_nb = run_sql(query3)[0][0]
+    if new_ubsk_nb:
+        print "Table user_bskBASKET does not seem to be empty. Migration has already been done"
+        return 0
+    query4 = "SELECT count(*) from usergroup_bskBASKET"
+    new_ugbsk_nb = run_sql(query4)[0][0]
+    if new_ugbsk_nb:
+        print "Table usergroup_bskBASKET does not seem to be empty. Migration has already been done"
+        return 0
+    return 1
+
+def __count_current_baskets():
+    query = "SELECT count(*) FROM basket"
+    return run_sql(query)[0][0]
+
 def __check_basket_is_owned_by_only_one_user():
     """"""
     query = "SELECT id_basket, count(id_basket) FROM user_basket GROUP BY id_basket"
@@ -189,7 +247,7 @@ def __update_baskets(default_topic_name="Imported baskets",
                      default_share_level=cfg_webbasket_share_levels['READITM'],
                      drop_old_tables=0):
     """"""
-    __import_baskets(default_topic_name)
+    __import_baskets(default_topic_name, default_share_level)
     __import_records()
     __set_auto_increment_value()
     if drop_old_tables:
@@ -271,12 +329,16 @@ def __import_records():
    
     query2 = """INSERT INTO bskREC 
                 (id_bibrec_or_bskEXTREC, id_bskBASKET, id_user_who_added_item, score, date_added) VALUES %s"""
-    iter = 0
-    while iter < len(records):
-        temp_val = reduce(lambda x, y: x + ',' + y, map(records_updater, records[iter:iter+10000]))
+    iterator = 0
+    while iterator < len(records):
+        temp_val = reduce(lambda x, y: x + ',' + y, map(records_updater, records[iterator:iterator+10000]))
         run_sql(query2 % temp_val)
-        print "  Inserting records %i-%i out of %i" % (iter, iter+10000, len(records)  )  
-        iter = iter + 10000
+        if iterator + 10000 <= len(records):
+            last_rec = iterator + 10000
+        else: 
+            last_rec = len(records)
+        print "  Inserting records %i-%i out of %i" % (iterator, last_rec, len(records)  )          
+        iterator = iterator + 10000
     return len(records)
     
 def __set_auto_increment_value():
