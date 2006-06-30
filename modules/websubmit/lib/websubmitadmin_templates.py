5d85e241ef25b42603378273ea5fba83cad6a8de
Tibor Simko <tibor.simko@cern.ch>
2006-06-30 09:17:34 +0000

    In the navtrail, point to the common new-old WebSubmit Admin page. Also, added copyright message.

--- modules/websubmit/lib/websubmitadmin_templates.py
+++ modules/websubmit/lib/websubmitadmin_templates.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+## $Id$
+##
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 import cgi
 from invenio.config import weburl, cdslang
@@ -150,7 +168,7 @@ class Template:
            return html formatted navtrail
         """
         nav_h1 =  '<a class="navtrail" href="%s/admin/">Admin Area</a>'
-        nav_h1 += ' &gt; <a class="navtrail" href="%s/admin/websubmit/websubmitadmin.py/">WebSubmit Admin</a>'
+        nav_h1 += ' &gt; <a class="navtrail" href="%s/admin/websubmit/">WebSubmit Admin</a>'
         return  nav_h1 % (weburl, weburl)
 
     def _create_adminbox(self, header="", datalist=[], cls="admin_wvar"):
