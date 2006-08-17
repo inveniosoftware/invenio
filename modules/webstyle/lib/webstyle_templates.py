6465ce65cb5db49924b580ded371bdcaa020091d
Tibor Simko <tibor.simko@cern.ch>
2006-08-17 13:03:32 +0000

    In the error box, initialize properly host/page/browser/etc information, if we cannot extract it from the Apache request object. Fixes some cases of the usage of error_page().

--- modules/webstyle/lib/webstyle_templates.py
+++ modules/webstyle/lib/webstyle_templates.py
@@ -507,8 +507,9 @@ class Template:
                 host_s = req.hostname
                 page_s = req.unparsed_uri
                 client_s = req.connection.remote_ip 
-            except:
-                pass
+            except: # FIXME: bad except
+                browser_s += ': ' + info_not_available
+                host_s = page_s = client_s = info_not_available
         else:    
             browser_s += ': ' + info_not_available
             host_s = page_s = client_s = info_not_available
