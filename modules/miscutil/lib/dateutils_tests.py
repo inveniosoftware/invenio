9b0352a2f2f3dbae1bd7e6fdedeb20491554f7c7
Tibor Simko <tibor.simko@cern.ch>
2006-09-21 10:07:22 +0000

    Added tests for dateutils datecvs conversion.

---
+++ modules/miscutil/lib/dateutils_tests.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##
+## $Id$
+## 
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.  
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Unit tests for dateutils library."""
+
+__revision__ = "$Id$"
+
+import unittest
+import dateutils
+
+class ConvertDateCVSTest(unittest.TestCase):
+    """
+    Testing conversion of CVS dates.
+    """
+    
+    def test_convert_good_cvsdate(self):
+        """dateutils - conversion of good CVS dates"""
+        datecvs = "$Date$"
+        datestruct_beginning_expected = (2006, 9, 20, 17, 27, 11)
+        self.assertEqual(dateutils.convert_datecvs_to_datestruct(datecvs)[:6],
+                         datestruct_beginning_expected)
+
+    def test_convert_bad_cvsdate(self):
+        """dateutils - conversion of bad CVS dates"""
+        datecvs = "$Date$"
+        datestruct_beginning_expected = (0, 0, 0, 0, 0, 0)
+        self.assertEqual(dateutils.convert_datecvs_to_datestruct(datecvs)[:6],
+                         datestruct_beginning_expected)
+
+def create_test_suite():
+    """
+    Return test suite for the dateutils.
+    """
+    return unittest.TestSuite((unittest.makeSuite(\
+        ConvertDateCVSTest, 'test'),
+                               ))
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=2).run(create_test_suite())
