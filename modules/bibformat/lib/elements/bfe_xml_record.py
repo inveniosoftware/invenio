bdd05026d67a877157728389f11386c2265a59c2
Jerome Caffaro <jerome.caffaro@cern.ch>
2006-12-18 10:08:25 +0000

    Made bfe_xml_record unescaped by default.

--- modules/bibformat/lib/elements/bfe_xml_record.py
+++ modules/bibformat/lib/elements/bfe_xml_record.py
@@ -42,3 +42,9 @@ def format(bfo, type='xml', encodeForXML='yes'):
     else:
         return out
  
+def escape_values(bfo):
+    """
+    Called by BibFormat in order to check if output of this element
+    should be escaped.
+    """
+    return 0
