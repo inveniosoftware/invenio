22ad3cbf3a915be7b124c53b4b9bfe068d30c0c7
Jerome Caffaro <jerome.caffaro@cern.ch>
2007-02-22 06:50:43 +0000

    New element for displaying all report numbers.

---
+++ modules/bibformat/lib/elements/bfe_report_numbers.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+##
+## $Id$
+##
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.  
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""BibFormat element - Prints report numbers
+"""
+
+__revision__ = ""
+
+def format(bfo, separator):
+    """
+    Prints the report numbers of the record (037__a and 088__a)
+    
+    @param separator the separator between report numbers.
+    """
+    
+    numbers = bfo.fields("037__a")
+    numbers.extend(bfo.fields("088__a"))
+    return separator.join(numbers)
