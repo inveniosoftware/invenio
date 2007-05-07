572fdb9c34b19e91c799eb5c0d929c644e9f1587
Jerome Caffaro <jerome.caffaro@cern.ch>
2007-05-07 07:47:23 +0000

    Corrected detection of installed xsl parser.

--- modules/bibconvert/lib/bibconvert_xslt_engine.py
+++ modules/bibconvert/lib/bibconvert_xslt_engine.py
@@ -61,19 +61,19 @@ CFG_BIBCONVERT_FUNCTION_NS = "http://cdsweb.cern.ch/bibconvert/fn"
 processor_type = -1
 try:
     # libxml2 & libxslt
-    processor_type = 0
     import libxml2
     import libxslt
+    processor_type = 0
 except ImportError:
     pass
 
 if processor_type == -1:
     try:
         # 4suite
-        processor_type = 1
         from Ft.Xml.Xslt import Processor
         from Ft.Xml import InputSource
         from xml.dom import Node
+        processor_type = 1
     except ImportError:
         pass
 
