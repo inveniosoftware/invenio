c652cd36aa69b9fe8720108ad0f5addc337a1caf
Tibor Simko <tibor.simko@cern.ch>
2007-09-09 19:54:31 +0000

    Fixed problem with mutiple printing of the same subject category. Thanks to Kam-ming Ku for reporting the problem.  Also, removed trailing whitespace.

--- modules/bibformat/lib/elements/bfe_topbanner.py
+++ modules/bibformat/lib/elements/bfe_topbanner.py
@@ -13,13 +13,16 @@
 ## CDS Invenio is distributed in the hope that it will be useful, but
 ## WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## General Public License for more details.  
+## General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-"""BibFormat element - Prints HTML topbanner with categorz, rep. number, etc.
+
 """
+BibFormat element - Prints HTML topbanner with category, rep. number, etc.
+"""
+
 __revision__ = "$Id$"
 
 import cgi
@@ -35,14 +38,17 @@ def format(bfo, kb_name="dbcollid2coll"):
     subject_2 = bfo.field("65027a")
     subject_2 = cgi.escape(subject_2)
     additional_report_numbers = bfo.fields("088__a")
-    
+
     source_of_aquisition = bfo.field("037__a")
     source_of_aquisition = cgi.escape(source_of_aquisition)
 
 
-    if subject != "XX" and subject:
+    if subject:
         subject = " / " + subject
-    
+
+    if subject_2:
+        subject_2 = " / " + subject_2
+
     if len(source_of_aquisition) > 0:
         source_of_aquisition = '<td align="right"><strong>'+ source_of_aquisition + "</strong></td>"
 
@@ -55,20 +61,20 @@ def format(bfo, kb_name="dbcollid2coll"):
     out = '''
     <table border="0" width="100%%">
       <tr>
-        <td>%(collection_indicator)s<small>%(subject)s %(subject)s</small></td>
+        <td>%(collection_indicator)s<small>%(subject)s%(subject_2)s</small></td>
         <td><small><strong>%(report_number)s</strong></small></td>
         %(source_of_aquisition)s
       </tr>
     </table>
     ''' % {'collection_indicator': collection_indicator,
            'subject': subject,
-           'subject2': subject_2,
+           'subject_2': subject_2,
            'report_number': report_numbers_out,
            'source_of_aquisition': source_of_aquisition}
 
     if collection_indicator or \
            subject or \
-           subject2 or \
+           subject_2 or \
            source_of_aquisition or \
            report_numbers_out:
         return out
