d04c6a1ef5644bb51a6e6ee4943b129fb4883305
Gabriel Hase <gabriel.hase@cern.ch>
2007-11-19 08:54:00 +0000

    changed headers to cds invenio copyright, except for find journals function if there are no journals

--- modules/webjournal/lib/webjournal_config.py
+++ modules/webjournal/lib/webjournal_config.py
@@ -1,4 +1,22 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+## $Id$
+##
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 from invenio.config import adminemail, supportemail, etcdir, weburl, cdslang
 from invenio.messages import gettext_set_language
@@ -107,8 +125,10 @@ def webjournal_missing_info_box(req, title, msg_title, msg):
     box_text = _(msg)
     box_list_title = _("Available Journals")
     find_journals = lambda path: [entry for entry in os.listdir(str(path)) if os.path.isdir(str(path)+str(entry))]
-    all_journals = find_journals('%s/webjournal/' % etcdir)
-    
+    try:
+        all_journals = find_journals('%s/webjournal/' % etcdir)
+    except:
+        all_journals = []
     box = '''<div style="text-align: center;">
                 <fieldset style="width:400px; margin-left: auto; margin-right: auto;background: url('%s/img/blue_gradient.gif') top left repeat-x;">
                     <legend style="color:#a70509;background-color:#fff;"><i>%s</i></legend>
