47347bf43af6421c575991b03905783b707650cd
Gabriel Hase <gabriel.hase@cern.ch>
2007-11-20 14:34:11 +0000

    bugfix in alert

--- modules/webjournal/lib/webjournal_webinterface.py
+++ modules/webjournal/lib/webjournal_webinterface.py
@@ -440,7 +440,7 @@ L'équipe du %s
             
             subject = "%s %s released!" % (display_name, issue)
             message = createhtmlmail(html_string, plain_text, subject)
-            server.smtplib.SMTP("localhost", 25)
+            server = smtplib.SMTP("localhost", 25)
             server.sendmail('Bulletin-Support@cern.ch', argd['recipients'], message)
 
             return page(title="Alert sent successfully!", body="")
