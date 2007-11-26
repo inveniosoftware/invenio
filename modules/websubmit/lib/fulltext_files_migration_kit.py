d0116f9fefec329fb0b786577f011871f847e0b8
Samuele Kaplun <samuele.kaplun@cern.ch>
2007-11-26 17:16:14 +0000

    Nice informative messages added and implemented table backup.

--- modules/websubmit/lib/fulltext_files_migration_kit.py
+++ modules/websubmit/lib/fulltext_files_migration_kit.py
@@ -24,7 +24,8 @@ to make it coherent with docfile implementation (docfile.py structure is backwar
 compatible with file.py structure, but the viceversa is not true).
 """
 
-from invenio.dbquery import run_sql
+import sys
+from invenio.dbquery import run_sql, OperationalError
 from invenio.docfile import BibRecDocs, InvenioWebSubmitFileError
 
 def retrieve_fulltext_recids():
@@ -44,17 +45,83 @@ def fix_recid(recid):
             new_bibdocs = bibrec.fix(docname)
             new_bibdocnames = [bibdoc.get_docname() for bibdoc in new_bibdocs]
             if new_bibdocnames:
-                print "(created bibdocs: '%s')" % new_bibdocnames.join("', '"),
+                print "(created bibdocs: '%s')" % "', '".join(new_bibdocnames),
     except InvenioWebSubmitFileError, e:
         print "%s -> ERROR", e
     else:
         print "-> OK"
 
+def backup_tables(drop=False):
+    """This function create a backup of bibrec_bibdoc, bibdoc and bibdoc_bibdoc tables. Returns False in case dropping of previous table is needed."""
+    if drop:
+        run_sql('DROP TABLE bibrec_bibdoc_backup')
+        run_sql('DROP TABLE bibdoc_backup')
+        run_sql('DROP TABLE bibdoc_bibdoc_backup')
+    try:
+        run_sql("""CREATE TABLE bibrec_bibdoc_backup (KEY id_bibrec(id_bibrec),
+                KEY id_bibdoc(id_bibdoc)) SELECT * FROM bibrec_bibdoc""")
+        run_sql("""CREATE TABLE bibdoc_backup (PRIMARY KEY id(id))
+                SELECT * FROM bibdoc""")
+        run_sql("""CREATE TABLE bibdoc_bibdoc_backup (KEY id_bibdoc1(id_bibdoc1),
+                KEY id_bibdoc2(id_bibdoc2)) SELECT * FROM bibdoc_bibdoc""")
+    except OperationalError, e:
+        if not drop:
+            return False
+        raise e
+    return True
+
+def check_yes():
+    """Return True if the user types 'yes'."""
+    return raw_input().strip() == 'yes'
+
 def main():
     """Core loop."""
     recids = retrieve_fulltext_recids()
-    print "%s records to migrate" % len(recids)
+    print "*******************************************************************"
+    print "* This script migrate the filesystem structure used to store      *"
+    print "* fulltext files to the new stricter structure.                   *"
+    print "* This script must not be run during normal Invenio operations.   *"
+    print "* It is safe to run this script. No file will be deleted.         *"
+    print "* Anyway it is recommended to run a backup of the filesystem      *"
+    print "* structure just in case.                                         *"
+    print "* A backup of the database tables involved will be automatically  *"
+    print "* performed.                                                      *"
+    print "*******************************************************************"
+    print
+    print "%s records will be migrated/fixed." % len(recids)
+    print "Please type yes if you want to go further:",
+
+    if not check_yes():
+        print "INTERRUPTED"
+        sys.exit(1)
     print "-" * 40
+    print "Backing up database tables",
+    try:
+        if not backup_tables():
+            print
+            print "*******************************************************************"
+            print "* It appears that is not the first time that you run this script. *"
+            print "* Backup tables have been already created by a previous run.      *"
+            print "* In order for the script to go further they need to be removed.  *"
+            print "*******************************************************************"
+            print
+            print "Please, type yes if you agree to remove them and go further:",
+
+            if not check_yes():
+                print "INTERRUPTED"
+                sys.exit(1)
+            print "-" * 40
+            print "Backing up database tables (after dropping previous backup)",
+            backup_tables()
+            print "-> OK"
+        else:
+            print "-> OK"
+    except Exception, e:
+        print
+        print "Unexpected error while backing up tables. Please, do your checks."
+        print e
+        sys.exit(1)
+
     for recid in recids:
         fix_recid(recid)
     print "-" * 40
