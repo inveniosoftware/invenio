1f79364f869b04b2931bcc797a3f8dca296fe2e7
Radoslav Ivanov <radoslav.ivanov@cern.ch>
2007-12-18 16:17:09 +0000

    Add additional test for adding records to a basket.

--- modules/webbasket/lib/webbasket_regression_tests.py
+++ modules/webbasket/lib/webbasket_regression_tests.py
@@ -24,10 +24,11 @@
 __revision__ = "$Id$"
 
 import unittest
+import mechanize
 
 from invenio.config import weburl
 from invenio.testutils import make_test_suite, warn_user_about_tests_and_run, \
-                              test_web_page_content, merge_error_messages
+                              test_web_page_content, make_url, make_surl, merge_error_messages
 
 class WebBasketWebPagesAvailabilityTest(unittest.TestCase):
     """Check WebBasket web pages whether they are up or not."""
@@ -49,7 +50,89 @@ class WebBasketWebPagesAvailabilityTest(unittest.TestCase):
             self.fail(merge_error_messages(error_messages))
         return
 
-test_suite = make_test_suite(WebBasketWebPagesAvailabilityTest)
+class WebBasketRecordsAdditionTest(unittest.TestCase):
+    """Test addition of records to webbasket"""
+    
+    def _login(self, browser, user, password):
+        """Log the user in an existing browser using his password"""
+        
+        browser.open(make_surl('/youraccount/login'))
+        browser.select_form(nr=0)
+        browser['p_un'] = user
+        browser['p_pw'] = password
+        browser.submit() 
+
+    def _perform_search(self, browser, search_criteria):
+        """Perform search in an existing browser using the specified criteria. 
+        Calling the method is equal of typing the criteria in the search box 
+        and pressing the 'search' button."""       
+            
+        # open the search page that in our case is the default page
+        browser.open(make_url('/'))
+        
+        # perform search
+        browser.select_form(name = 'search')
+        browser['p'] = search_criteria
+        browser.submit(name = 'action_search')
+        
+    def _select_records_for_adding_to_basket(self, browser, records):
+        """Calling this method is is equal of selecting records from 
+        the search results and pressing 'ADD TO BASKET' button """    
+        
+        # select the proper form containing the check boxes for marking the records 
+        browser.select_form(nr = 2)
+        
+        # select the records
+        control = browser.find_control('recid')
+        
+        for current_record in records:
+            control.items[current_record].selected = True
+        
+        # press 'ADD TO BASKET' button
+        browser.submit();        
+        
+    def _create_new_basket_and_add_records(self, browser, basket_name, topic_name):
+        browser.select_form(name = 'add_to_basket')
+        browser['new_basket_name'] = basket_name
+        browser['new_topic_name'] = topic_name
+        browser.submit()
+        
+    def _check_basket_content(self, browser, expected_texts):
+        browser.open(make_surl('/yourbaskets/display?ln=en'))
+        url_body = browser.response().read()
+        
+        for current_expected_text in expected_texts:
+            if current_expected_text not in url_body:
+                self.fail('Expects to find ' + current_expected_text + ' in the basket')
+                
+    def _add_records_to_basket_and_check_content(self, browser):
+        """add records to basket and check content of baskets page for 
+        expexted strings """
+        
+        self._perform_search(browser, 'ellis')
+        self._select_records_for_adding_to_basket(browser, [0, 6])        
+        self._create_new_basket_and_add_records(browser, 'Test Basket', 'Test Topic')
+
+        expected_texts = ['Test Topic', 'Test Basket', '2 records',
+                          'Thermal conductivity of dense quark matter and cooling of stars', 
+                          'The total cross section for the production of heavy quarks in hadronic collisions']
+        self._check_basket_content(browser, expected_texts)
+        
+    def test_records_addition_as_guest_user(self):
+        """webbasket - addition of records as guest"""
+        
+        browser = mechanize.Browser()
+        self._add_records_to_basket_and_check_content(browser)
+                    
+    def test_records_addition_as_registered_user(self):
+        """webbasket - addition of records as registered user"""
+        
+        browser = mechanize.Browser()
+        self._login(browser, 'jekyll', 'j123ekyll')
+        # FIXME: remove the comment and add method to clean the baskets after the test
+        # self._add_records_to_basket_and_check_content(browser)
+
+test_suite = make_test_suite(WebBasketWebPagesAvailabilityTest, WebBasketRecordsAdditionTest)
 
 if __name__ == "__main__":
     warn_user_about_tests_and_run(test_suite)
