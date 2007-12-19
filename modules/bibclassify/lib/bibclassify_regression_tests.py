d9646e4bdc170851e168ed78f2c9f7a38731cccb
Tibor Simko <tibor.simko@cern.ch>
2007-12-19 15:29:15 +0000

    Added BibClassify regression test suite.

---
+++ modules/bibclassify/lib/bibclassify_regression_tests.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+##
+## $Id$
+##
+## This file is part of CDS Invenio.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""BibClassify Regression Test Suite."""
+
+__revision__ = "$Id$"
+
+import unittest
+
+from invenio.config import weburl
+from invenio.testutils import make_test_suite, warn_user_about_tests_and_run, \
+                              test_web_page_content, merge_error_messages, \
+                              test_web_page_existence
+
+class BibClassifyWebPagesAvailabilityTest(unittest.TestCase):
+    """Check BibClassify web pages whether they are up or not."""
+
+    def test_availability_bibclassify_admin_guide(self):
+        """bibclassify - availability of BibClassify Admin Guide page"""
+	self.assertEqual([],
+                         test_web_page_content(weburl + '/help/admin/bibclassify-admin-guide',
+                                               expected_text="BibClassify Admin Guide"))
+        return
+
+    def test_availability_bibclassify_admin_guide_images(self):
+        """bibclassify - availability of BibClassify Admin Guide images"""
+        test_web_page_existence(weburl + '/img/admin/bibclassify-admin-guide-cloud.jpeg')
+
+    def test_availability_bibclassify_hacking_pages(self):
+        """bibclassify - availability of BibClassify Hacking Guide pages"""
+	self.assertEqual([],
+                         test_web_page_content(weburl + '/help/hacking/bibclassify-internals',
+                                               expected_text="BibClassify Internals"))
+	self.assertEqual([],
+                         test_web_page_content(weburl + '/help/hacking/bibclassify-hep-taxonomy',
+                                               expected_text="The HEP taxonomy: rationale and extensions"))
+	self.assertEqual([],
+                         test_web_page_content(weburl + '/help/hacking/bibclassify-extraction-algorithm',
+                                               expected_text="The code behind BibClassify: the extraction algorithm"))
+        return
+
+test_suite = make_test_suite(BibClassifyWebPagesAvailabilityTest)
+
+if __name__ == "__main__":
+    warn_user_about_tests_and_run(test_suite)
