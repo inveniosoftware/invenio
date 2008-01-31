c0c9cbc6584462294223409631e6ba175589ed8a
Tibor Simko <tibor.simko@cern.ch>
2008-01-31 13:15:49 +0000

    Updated global docstring.

--- modules/websearch/lib/websearch_webcoll.py
+++ modules/websearch/lib/websearch_webcoll.py
@@ -17,7 +17,7 @@
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Creates CDS Invenio collection specific pages, using WML and MySQL configuration tables."""
+"""Create CDS Invenio collection cache."""
 
 __revision__ = "$Id$"
 
