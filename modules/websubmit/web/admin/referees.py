ddefb5059f6b8972ed74635ef784d655048a3955
Jerome Caffaro <jerome.caffaro@cern.ch>
2008-02-06 08:54:34 +0000

    Cleaned Python code and HTML markup.

--- modules/websubmit/web/admin/referees.py
+++ modules/websubmit/web/admin/referees.py
@@ -16,138 +16,147 @@
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+"""WebSubmit interface for the management of referees."""
 
 __revision__ = "$Id$"
 
 ## import interesting modules:
-import string
-import os
-import sys
-import time
 import types
 import re
-import shutil
 
 from invenio.config import \
      cdslang, \
      cdsname, \
      images, \
-     version, \
      weburl
 from invenio.dbquery import run_sql, Error
 from invenio.access_control_engine import acc_authorize_action
-from invenio.access_control_admin import *
+from invenio.access_control_admin import \
+     acc_delete_user_role, \
+     acc_get_role_id, \
+     acc_add_role, \
+     acc_add_action,\
+     acc_add_role_action_arguments, \
+     acc_add_argument, \
+     acc_get_user_roles, \
+     acc_add_user_role, \
+     acc_get_action_id, \
+     acc_get_all_roles, \
+     acc_get_role_users
 from invenio.webpage import page, create_error_box
 from invenio.webuser import getUid, get_email, list_registered_users, page_not_authorized
 from invenio.messages import wash_language
-from invenio.websubmit_config import *
 
-def index(req,c=cdsname,ln=cdslang,todo="",id="",doctype="",categ="",addusers="",warningText="",role=""):
+def index(req, c=cdsname, ln=cdslang, todo="", id="", doctype="",
+          categ="", addusers="", warningText="", role=""):
+    """Main entry point for the management of referees."""
     ln = wash_language(ln)
     # get user ID:
     try:
         uid = getUid(req)
-        uid_email = get_email(uid)
     except Error, e:
-        return errorMsg(e.value,req)
-    (auth_code, auth_message) = acc_authorize_action(req, "cfgwebsubmit",verbose=0)
+        return errorMsg(e.value, req)
+    (auth_code, auth_message) = acc_authorize_action(req, "cfgwebsubmit", verbose=0)
     if auth_code != 0:
         ## user is not authorised to use WebSubmit Admin:
         return page_not_authorized(req=req, text=auth_message)
 
     # request for deleting a user
     if todo == "deleteuser":
-        acc_delete_user_role(id,name_role=role)
+        acc_delete_user_role(id, name_role=role)
     # request for adding user(s)
     if todo == "adduser":
-        role = "referee_%s_%s" % (doctype,categ[1])
+        role = "referee_%s_%s" % (doctype, categ[1])
         roleId = acc_get_role_id(role)
         # if the role does not exists, we create it
         if roleId == 0:
-            if acc_add_role(role,"referees for document type %s category %s" % (doctype,categ[1])) == 0:
-                return errorMsg("Cannot create referee role",req)
+            if acc_add_role(role, "referees for document type %s category %s" % (doctype, categ[1])) == 0:
+                return errorMsg("Cannot create referee role", req)
             else:
                 roleId = acc_get_role_id(role)
             # if the action does not exist, we create it
             actionId = acc_get_action_id("referee")
             if actionId == 0:
-                if acc_add_action("referee","","no",("doctype","categ")) == 0:
-                    return errorMsg("Cannot create action 'referee'",req)
+                if acc_add_action("referee", "", "no", ("doctype","categ")) == 0:
+                    return errorMsg("Cannot create action 'referee'", req)
                 else:
                     actionId = acc_get_action_id("referee")
             #create arguments
-            arg1Id = acc_add_argument("doctype",doctype)
-            arg2Id = acc_add_argument("categ",categ[1])
+            arg1Id = acc_add_argument("doctype", doctype)
+            arg2Id = acc_add_argument("categ", categ[1])
             # then link the role with the action
-            if acc_add_role_action_arguments(roleId,actionId,-1,0,0,[arg1Id,arg2Id]) == 0:
-                return errorMsg("Cannot link role with action",req)
+            if acc_add_role_action_arguments(roleId, actionId, -1, 0, 0, [arg1Id, arg2Id]) == 0:
+                return errorMsg("Cannot link role with action", req)
         roleId = acc_get_role_id(role)
         # For each id in the array
-        if isinstance(addusers,types.ListType):
+        if isinstance(addusers, types.ListType):
             for adduser in addusers:
                 # First check  whether this id is not already associated with this rule
                 myRoles = acc_get_user_roles(adduser)
                 if not roleId in myRoles:
                     # Actually add the role to the user
-                    acc_add_user_role(adduser,roleId)
+                    acc_add_user_role(adduser, roleId)
                 else:
-                    warningText = "<font color=red>Sorry... This user is already a referee for this category.</font>"
+                    warningText = '<span style="color:#f00">Sorry... This user is already a referee for this category.</span>'
         else:
             # First check  whether this id is not already associated with this rule
             myRoles = acc_get_user_roles(addusers)
             if not roleId in myRoles:
                 # Actually add the role to the user
-                acc_add_user_role(addusers,roleId)
+                acc_add_user_role(addusers, roleId)
             else:
-                warningText = "<font color=red>Sorry... This user is already a referee for this category.</font>"
+                warningText = '<span style="color:#f00">Sorry... This user is already a referee for this category.</span>'
     return page(title="websubmit admin - referee selection",
-                    body=displayRefereesPage(doctype,warningText),
+                    body=displayRefereesPage(doctype, warningText),
                     description="",
                     keywords="",
                     uid=uid,
                     language=ln,
                     req=req)
 
-def displayRefereesPage(doctype,warningText):
-    t=""
+def displayRefereesPage(doctype, warningText):
+    """Output the list of refeeres as well as the controls to add/remove them"""
+    t = ""
     if doctype in ['', '*']:
         doctype = '*'
         docname = "all catalogues"
     else:
         res = run_sql("SELECT * FROM sbmDOCTYPE WHERE sdocname=%s", (doctype,))
         docname = res[0][0]
-    t+=warningText
-    t+="""
-<FORM ACTION='referees.py' METHOD='POST'>
-<INPUT TYPE='hidden' NAME='todo' VALUE=''>
-<INPUT TYPE='hidden' NAME='id' VALUE=''>
-<INPUT TYPE='hidden' NAME='doctype' VALUE='%s'>
-<INPUT TYPE='hidden' NAME='categ' VALUE=''>
-<INPUT TYPE='hidden' NAME='role' VALUE=''>
+    t += warningText
+    t += """
+<form action="referees.py" method="post">
+<input type="hidden" name="todo" value="" />
+<input type="hidden" name="id" value="" />
+<input type="hidden" name="doctype" value="%s" />
+<input type="hidden" name="categ" value="" />
+<input type="hidden" name="role" value="" />
 <!-- Role: referee -->
-<TABLE><TR><TD valign=top>""" %doctype
+<table><tr><td valign="top">""" % doctype
     # call the function to display the table containing the list of associated emails
-    t+=displayUserTable(doctype)
-    t+="""
-    </TD>
-    <TD valign=top>"""
+    t += displayUserTable(doctype)
+    t += """
+    </td>
+    <td valign="top">"""
     # call the function to display the form allowing the manager to add new users
-    t+=displayAddUser(doctype)
+    t += displayAddUser(doctype)
     end_url = "%s/admin/websubmit/websubmitadmin.py/doctypeconfigure?doctype=%s" % (weburl, doctype)
     if doctype in ['', '*']:
         end_url = "%s/admin/websubmit/websubmitadmin.py/" % weburl
-    t+= """
-    </TD></TR></TABLE>
+    t += """
+    </td></tr></table>
 <!-- End submissionuser rule -->
     <a href="%s">Finished</a>
-    </FORM>""" % end_url
+    </form>""" % end_url
     return t
 
 def displayUserTable(doctype):
-    t=""
+    """Display the list of referees for the given doctype, as well as
+    the control to remove them"""
+    t = ""
     # start displaying the table which will contain the list of email addresses.
-    t+= """
-    <table class="searchbox" summary="">
+    t += """
+    <table class="searchbox">
         <tr>
             <th class="portalboxheader" colspan="2">Referees</th>
         </tr>"""
@@ -156,73 +165,75 @@ def displayUserTable(doctype):
     for role in roles:
         role_name = role[1]
         role_id = role[0]
-        if re.match("^referee_%s_" % doctype,role_name):
+        if re.match("^referee_%s_" % doctype, role_name):
             # Try to retrieve the referee's email from the referee's database
             if acc_get_role_users(role_id) is not None:
                 referees[role_name] = acc_get_role_users(role_id)
 
     if len(referees) == 0:
-        t+= "<TR><TD align=center colspan=2><IMG SRC=\"%s/noway.gif\" height=16 width=16></TD></TR>" % images
-    i=0
+        t += '<tr><td align="center" colspan="2"><img src="%s/noway.gif" height="16px" width="16px" alt="Empty"/></td></tr>' % images
+    i = 0
     for role in referees.keys():
-        categ = re.match("referee_%s_(.*)" % doctype,role).group(1)
-        res = run_sql("SELECT lname FROM sbmCATEGORIES WHERE sname=%s and doctype=%s", (categ,doctype,))
+        categ = re.match("referee_%s_(.*)" % doctype, role).group(1)
+        res = run_sql("SELECT lname FROM sbmCATEGORIES WHERE sname=%s and doctype=%s", (categ, doctype,))
         if len(res) > 0:
             categname = "Referee(s) for category: %s" % res[0][0]
         else:
             categname = "General Referee(s)"
-        t+= "<TR><TD colspan=2><small><b>%s</b> </small></TD></TR>" % categname
+        t += '<tr><td colspan="2"><small><b>%s</b> </small></td></tr>' % categname
         for referee in referees[role]:
             if int(i/2) == i/2:
-                bgcolor="#eeeeee"
+                bgcolor = "#eeeeee"
             else:
-                bgcolor="#dddddd"
-            t+= "<TR bgcolor=%s>" % bgcolor
-            t+= "<TD align=right><small>"
-            t+= referee[1]
-            t+= "</small></TD>"
-            t+= "<TD><a href=\"\" onClick=\"if (confirm('Are you sure you want to delete this referee?')){document.forms[0].todo.value='deleteuser';document.forms[0].id.value='%s';document.forms[0].role.value='%s';document.forms[0].submit();return false;}else{return false;}\">" % (referee[0],role)
-            t+= "<IMG SRC=\"%s/iconcross.gif\" border=0></a>" % images
-            t+= "</TD>";
-            t+= "</TR>";
-            i+=1
+                bgcolor = "#dddddd"
+            t += '<tr bgcolor="%s">' % bgcolor
+            t += '<td align="right"><small>'
+            t += referee[1]
+            t += '</small></td>'
+            t += '''<td><a href="" onclick="if (confirm('Are you sure you want to delete this referee?')){document.forms[0].todo.value='deleteuser';document.forms[0].id.value='%s';document.forms[0].role.value='%s';document.forms[0].submit();return false;}else{return false;}">''' % (referee[0], role)
+            t += '<img src="%s/iconcross.gif" border="0" alt="Remove" /></a>' % images
+            t += '</td>'
+            t += '</tr>'
+            i += 1
     # close table
-    t+="</TABLE>"
+    t += "</table>"
     return t
 
 def displayAddUser(doctype):
-    t=""
+    """Display controls for adding users"""
+    t = ""
     # start displaying the table which will contain the add form
-    t+= """
+    t += """
     <table class="searchbox" summary="">
         <tr>
             <th class="portalboxheader">Add</th>
         </tr>
         <tr>
             <td>
-    User:<br>"""
+    User:<br/>"""
     users = list_registered_users()
     if len(users) < 20:
         numrows = len(users)
     else:
         numrows = 20
-    t+= "<SELECT multiple name=addusers size=%s>" % numrows
+    t += '<select multiple="multiple" name="addusers" size="%s">' % numrows
     for user in users:
         if user[1] != "":
-            t+= "<OPTION value=%s>%s" % (user[0],user[1])
-    t+= "</SELECT><br>"
-    t+= "<SELECT name=categ>"
-    t+= "<OPTION value='*'>All categories"
-    res = run_sql("SELECT lname,sname FROM sbmCATEGORIES WHERE doctype=%s ORDER BY lname", (doctype,))
+            t += '<option value="%s">%s</option>' % (user[0], user[1])
+    t += '</select><br/>'
+    t += '<select name="categ">'
+    t += '<option value="*">All categories</option>'
+    res = run_sql("select lname,sname FROM sbmCATEGORIES WHERE doctype=%s ORDER BY lname", (doctype,))
     for row in res:
-        t+= "<OPTION value=%s>%s" % (row[1],row[0])
-    t+= "</SELECT><br>"
-    t+= "<INPUT class=\"adminbutton\" type=button onClick=\"document.forms[0].todo.value='adduser';document.forms[0].submit();\" VALUE=\"ADD\">"
-    t+= "</small></TD></TR></TABLE>"
+        t += '<option value="%s">%s</option>' % (row[1], row[0])
+    t += '</select><br/>'
+    t += '''<input class="adminbutton" type="button" onclick="document.forms[0].todo.value='adduser';document.forms[0].submit();" value="ADD" />'''
+    t += '</td></tr></table>'
     return t
 
 
-def errorMsg(title,req,uid,c=cdsname,ln=cdslang):
+def errorMsg(title, req, uid, c=cdsname, ln=cdslang):
+    """Prints the error page."""
     return page(title="error",
                     body = create_error_box(req, title=title,verbose=0, ln=ln),
                     description="%s - Internal Error" % c,
