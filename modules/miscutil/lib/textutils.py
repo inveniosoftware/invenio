4cb45d92f9cea7bc04cf21f64f4caf40699c3984
Tibor Simko <tibor.simko@cern.ch>
2008-02-25 17:39:40 +0000

    Added wait_for_user() that is useful for interactive CLI programs. Slightly emended wrap_text_in_a_box().  More refactoring needed, e.g. make_conclusion() is but a special case of wrap_text_in_a_box().

--- modules/miscutil/lib/textutils.py
+++ modules/miscutil/lib/textutils.py
@@ -18,14 +18,13 @@
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-""" Functions for text handling:
-- indenting
-- ...
+"""
+Functions useful for text handling.
 """
 
 __revision__ = "$Id$"
 
-
+import sys
 from formatter import AbstractFormatter, DumbWriter
 from cStringIO import StringIO
 
@@ -50,36 +49,38 @@ def indent_text(text,
         output += tabs + line + linebreak_output
     return output
 
-def wrap_in_a_box(msg, title=''):
-    """Return a nicely formatted hello! as:
-    ******************
-    **  some title  **
-    **--------------**
-    **    hello!    **
-    ******************
+def wrap_text_in_a_box(title='', body='', char_sep=' ', max_col=72):
+    """Return a nicely formatted text box:
+
+       ******************
+       **  title       **
+       **--------------**
+       **  body        **
+       ******************
     """
     ret = ''
     out = StringIO()
-    tool = AbstractFormatter(DumbWriter(out, maxcol=72))
-    for row in msg.split('\n'):
+    tool = AbstractFormatter(DumbWriter(out, maxcol=max_col))
+    for row in body.split('\n'):
         tool.add_flowing_data(row)
         tool.end_paragraph(1)
-    msg_rows = out.getvalue().split('\n')[:-2]
+    body_rows = out.getvalue().split('\n')[:-2]
 
     out = StringIO()
-    tool = AbstractFormatter(DumbWriter(out, maxcol=72))
+    tool = AbstractFormatter(DumbWriter(out, maxcol=max_col))
     tool.add_flowing_data(title)
     title_rows = out.getvalue().split('\n')
 
-    max_len = max([len(row) for row in title_rows + msg_rows]) + 6
+    max_len = max([len(row) for row in title_rows + body_rows]) + 6
     ret += '*' * max_len + '\n'
     if title:
         for row in title_rows:
             ret += '** %s%s **\n' % (row, ' ' * (max_len - len(row) - 6))
-        ret += '**' + '-' * (max_len - 4) + '**\n'
-    for row in msg_rows:
+        ret += '**' + char_sep * (max_len - 4) + '**\n'
+    for row in body_rows:
         ret += '** %s%s **\n' % (row, ' ' * (max_len - len(row) - 6))
     ret += '*' * max_len + '\n'
+
     return ret
 
 def make_conclusion(text):
@@ -95,3 +96,14 @@ def make_conclusion(text):
     max_len = max([len(row) for row in text])
 
     return '%s\n%s\n' % ('-' * max_len, '\n'.join(text))
+
+def wait_for_user(msg):
+    """Print MSG and prompt user for confirmation."""
+    try:
+        raw_input(msg)
+    except KeyboardInterrupt:
+        print "\n\nInstallation aborted."
+        sys.exit(1)
+    except EOFError:
+        print " (continuing in batch mode)"
+        return
