0ee2f6a0155818f10f0ce7bbf1cbfab8e0f72b1a
Samuele Kaplun <samuele.kaplun@cern.ch>
2008-03-04 17:31:48 +0000

    Prettified revision name (it is used in Python 2.5 to create the egg info file.

--- modules/miscutil/lib/intbitset_setup.py
+++ modules/miscutil/lib/intbitset_setup.py
@@ -24,7 +24,7 @@ from distutils.extension import Extension
 
 setup(
     name = 'intbitset',
-    version = '$Revision$',
+    version = '$Revision$'[len('$Revision$')],
     description = """
     Defines an intbitset data object to hold unordered sets of
     unsigned integers with ultra fast set operations, implemented via
