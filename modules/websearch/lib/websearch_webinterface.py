2ce55664b1b108f7757d5116e0bdf813fb6c8765
Tibor Simko <tibor.simko@cern.ch>
2008-04-10 16:54:35 +0000

    Removed apparently forgotten raise statement that was breaking output. Fixed URL argument escaping in author pages.

--- modules/websearch/lib/websearch_webinterface.py
+++ modules/websearch/lib/websearch_webinterface.py
@@ -213,7 +213,8 @@ class WebInterfaceAuthorPages(WebInterfaceDirectory):
                                                     citedbylist, kwtuples, authors, vtuples, ln)
 
         #cited-by summary
-        out = summarize_records(pubs, 'hcs', ln, "../search/f=author&p="+self.authorname)
+        out = summarize_records(pubs, 'hcs', ln, "../search?f=author&p=" + \
+                                quote(self.authorname))
         req.write(out)
 
         simauthbox = search_engine.create_similarly_named_authors_link_box(self.authorname)
@@ -311,7 +312,6 @@ class WebInterfaceRecordPages(WebInterfaceDirectory):
                 navmenuid='search')
 
         # mod_python does not like to return [] in case when of=id:
-        raise str(**argd)
         out = search_engine.perform_request_search(req, **argd)
         if out == []:
             return str(out)
