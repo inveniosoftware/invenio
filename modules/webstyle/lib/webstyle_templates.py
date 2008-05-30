e78e080c10e4c53330493aa4ce9fe74290dbc713
Jerome Caffaro <jerome.caffaro@cern.ch>
2008-05-30 07:57:17 +0000

    Added module docstring

--- modules/webstyle/lib/webstyle_templates.py
+++ modules/webstyle/lib/webstyle_templates.py
@@ -17,7 +17,9 @@
 ## You should have received a copy of the GNU General Public License
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-
+"""
+WebStyle templates. Customize the look of pages of CDS Invenio
+"""
 __revision__ = \
     "$Id$"
 
