11f5142c670786a336d6311fc0219900ff409e13
Samuele Kaplun <samuele.kaplun@cern.ch>
2008-08-14 16:04:40 +0000

    Refactored stream_file function.

--- modules/websubmit/lib/bibdocfile.py
+++ modules/websubmit/lib/bibdocfile.py
@@ -1491,33 +1491,27 @@ class BibDocFile:
         else:
             raise InvenioWebSubmitFileError, "You are not authorized to download %s: %s" % (self.fullname, auth_message)
 
-def stream_stream(req, file_object, size=None, mtime=None, fullname=None, mime=None, encoding=None, etag=None, md5=None):
-    """This is a generic function to stream an opened stream to the user."""
-
-
 def stream_file(req, fullpath, fullname=None, mime=None, encoding=None, etag=None, md5=None, location=None):
-    """This is a generic function to stream a file to the user."""
+    """This is a generic function to stream a file to the user.
+    If fullname, mime, encoding, and location are not provided they will be
+    guessed based on req and fullpath.
+    md5 should be passed as an hexadecimal string.
+    """
     def normal_streaming(size):
         req.set_content_length(size)
         req.send_http_header()
-        try:
-            if not req.header_only:
-                req.sendfile(fullpath)
-                return ''
-        except IOError, e:
-            register_exception(req=req)
-            raise InvenioWebSubmitFileError, "Encountered exception while reading '%s': '%s'" % (fullpath, e)
+        if not req.header_only:
+            req.sendfile(fullpath)
+        return ""
 
     def single_range(size, the_range):
-        the_range = (the_range[0], min(the_range[1], size - the_range[0]))
-        assert(the_range[1] > 0)
         req.set_content_length(the_range[1])
         req.headers_out['Content-Range'] = 'bytes %d-%d/%d' % (the_range[0], the_range[0] + the_range[1] - 1, size)
         req.status = apache.HTTP_PARTIAL_CONTENT
         req.send_http_header()
         if not req.header_only:
             req.sendfile(fullpath, the_range[0], the_range[1])
-        return ''
+        return ""
 
     def multiple_ranges(size, ranges, mime):
         req.status = apache.HTTP_PARTIAL_CONTENT
@@ -1525,43 +1519,132 @@ def stream_file(req, fullpath, fullname=None, mime=None, encoding=None, etag=Non
         boundary = '%s%04d' % (time.strftime('THIS_STRING_SEPARATES_%Y%m%d%H%M%S'), random.randint(0, 9999))
         req.headers_out['boundary'] = boundary
         req.send_http_header()
-        for arange in ranges:
-            arange = (arange[0], min(arange[1], size - arange[0]))
-            assert(arange[1] > 0)
-            req.write('--%s\r\n' % boundary)
-            req.write('Content-Type: %s\r\n' % mime)
-            req.write('Content-Range: bytes %d-%d/%d\r\n' % (arange[0], arange[0] + arange[1] - 1, size))
-            req.write('\r\n')
-            req.sendfile(fullpath, arange[0], arange[1])
-            req.write('\r\n')
-        req.write('--%s--\r\n' % boundary)
+        if not req.header_only:
+            for arange in ranges:
+                req.write('--%s\r\n' % boundary, flush=0)
+                req.write('Content-Type: %s\r\n' % mime, flush=0)
+                req.write('Content-Range: bytes %d-%d/%d\r\n' % (arange[0], arange[0] + arange[1] - 1, size), flush=0)
+                req.write('\r\n', flush=0)
+                req.sendfile(fullpath, arange[0], arange[1])
+                req.write('\r\n', flush=0)
+            req.write('--%s--\r\n' % boundary)
         return ""
 
     def parse_date(date):
-        if date is None:
+        """According to <http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.3>
+        a date can come in three formats (in order of preference):
+            Sun, 06 Nov 1994 08:49:37 GMT  ; RFC 822, updated by RFC 1123
+            Sunday, 06-Nov-94 08:49:37 GMT ; RFC 850, obsoleted by RFC 1036
+            Sun Nov  6 08:49:37 1994       ; ANSI C's asctime() format
+        Moreover IE is adding some trailing information after a ';'.
+        Wrong dates should be simpled ignored.
+        This function return the time in seconds since the epoch GMT or None
+        in case of errors."""
+        if not date:
             return None
         try:
             date = date.split(';')[0].strip() # Because of IE
+            ## Sun, 06 Nov 1994 08:49:37 GMT
             return time.mktime(time.strptime(date, '%a, %d %b %Y %X %Z'))
         except:
             try:
+                ## Sun, 06 Nov 1994 08:49:37 GMT
                 return time.mktime(time.strptime(date, '%A, %d-%b-%y %H:%M:%S %Z'))
             except:
                 try:
+                    ## Sun, 06 Nov 1994 08:49:37 GMT
                     return time.mktime(date)
                 except:
                     return None
 
-    if_none_match = req.headers_in.get('If-None-Match')
-    if if_none_match is not None:
-        if_none_match = [elem.strip() for elem in if_none_match.split(',')]
-        if etag is not None and etag in if_none_match:
-            raise apache.SERVER_RETURN, apache.HTTP_NOT_MODIFIED
+    def parse_ranges(ranges):
+        """According to <http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35>
+        a (multiple) range request comes in the form:
+            bytes=20-30,40-60,70-,-80
+        with the meaning:
+            from byte to 20 to 30 inclusive (11 bytes)
+            from byte to 40 to 60 inclusive (21 bytes)
+            from byte 70 to (size - 1) inclusive (size - 70 bytes)
+            from byte size - 80 to (size - 1) inclusive (80 bytes)
+        This function will return the list of ranges in the form:
+            [[first_byte, last_byte], ...]
+        If first_byte or last_byte aren't specified they'll be set to None
+        If the list is not well formatted it will return None
+        """
+        try:
+            if ranges.startswith('bytes') and '=' in ranges:
+                ranges = ranges.split('=')[1].strip()
+            else:
+                return None
+            ret = []
+            for arange in ranges.split(','):
+                arange = arange.strip()
+                if arange.startswith('-'):
+                    ret.append([None, int(arange[1:])])
+                elif arange.endswith('-'):
+                    ret.append([int(arange[:-1]), None])
+                else:
+                    ret.append(map(int, arange.split('-')))
+            return ret
+        except:
+            return None
+
+    def parse_tags(tags):
+        """Return a list of tags starting from a comma separated list."""
+        return [tag.strip() for tag in tags.split(',')]
+
+    def fix_ranges(ranges, size):
+        """Complementary to parse_ranges it will transform all the ranges
+        into (first_byte, length), adjusting all the value based on the
+        actual size provided.
+        """
+        ret = []
+        for arange in ranges:
+            if (arange[0] is None and arange[1] > 0) or arange[0] < size:
+                if arange[0] is None:
+                    arange[0] = size - arange[1]
+                elif arange[1] is None:
+                    arange[1] = size - arange[0]
+                else:
+                    arange[1] = arange[1] - arange[0] - 1
+                arange[0] = max(0, arange[0])
+                arange[1] = min(size - arange[0], arange[1])
+                ret.append(arange)
+        return ret
+
+    def get_normalized_headers(headers):
+        """Strip and lowerize all the keys of the headers dictionary plus
+        strip, lowerize and transform known headers value into their value."""
+        ret = {
+            'if-match' : None,
+            'unless-modified-since' : None,
+            'if-modified-since' : None,
+            'range' : None,
+            'if-range' : None,
+            'if-none-match' : None,
+        }
+        for key, value in req.headers_in.iteritems():
+            key = key.strip().lower()
+            value = value.strip()
+            if key in ('unless-modified-since', 'if-modified-since'):
+                value = parse_date(value)
+            elif key == 'range':
+                value = parse_ranges(value)
+            elif key == 'if-range':
+                value = parse_date(value) or parse_tags(value)
+            elif key in ('if-match', 'if-none-match'):
+                value = parse_tags(value)
+            if value:
+                ret[key] = value
+        return ret
+
+    headers = get_normalized_headers(req.headers_in)
+    if headers['if-match']:
+        if etag is not None and etag not in headers['if-match']:
+            raise apache.SERVER_RETURN, apache.HTTP_PRECONDITION_FAILED
+
     if os.path.exists(fullpath):
         mtime = os.path.getmtime(fullpath)
-        if_modified_since = parse_date(req.headers_in.get('If-Modified-Since'))
-        if if_modified_since is not None and if_modified_since >= mtime:
-            raise apache.SERVER_RETURN, apache.HTTP_NOT_MODIFIED
         if fullname is None:
             fullname = os.path.basename(fullpath)
         if mime is None:
@@ -1569,13 +1652,13 @@ def stream_file(req, fullpath, fullname=None, mime=None, encoding=None, etag=Non
             (mime, encoding) = _mimes.guess_type(fullpath)
             if mime is None:
                 mime = "application/octet-stream"
+        if location is None:
+            location = req.uri
         req.content_type = mime
         req.encoding = encoding
         req.filename = fullname
         req.headers_out["Last-Modified"] = time.strftime('%a, %d %b %Y %X GMT', time.gmtime(mtime))
         req.headers_out["Accept-Ranges"] = "bytes"
-        if location is None:
-            location = req.uri
         req.headers_out["Content-Location"] = location
         if etag is not None:
             req.headers_out["ETag"] = etag
@@ -1583,60 +1666,38 @@ def stream_file(req, fullpath, fullname=None, mime=None, encoding=None, etag=Non
             req.headers_out["Content-MD5"] = base64.encodestring(binascii.unhexlify(md5.upper()))[:-1]
         req.headers_out["Content-Disposition"] = 'attachment; filename="%s"' % fullname.replace('"', '\\"')
         size = os.path.getsize(fullpath)
-        ranges = req.headers_in.get('Range')
-        if ranges is not None:
-            if_match = req.headers_in.get('If-Match')
-            if if_match is not None:
-                if_match = [elem.strip() for elem in if_match.split(',')]
-                if etag is None:
-                    return normal_streaming(size)
-                elif etag not in if_match:
-                    raise apache.SERVER_RETURN, apache.HTTP_PRECONDITION_FAILED
-            if_range = req.headers_in.get('If-Range')
-            if if_range is not None:
-                if_range = [elem.strip() for elem in if_range.split(',')]
-                if etag is None or etag not in if_range:
-                    return normal_streaming(size)
-            unless_modified_since = parse_date(req.headers_in.get('Unless-Modified-Since'))
-            if unless_modified_since is not None and unless_modified_since < mtime:
-                return normal_streaming(size)
+        if not size:
             try:
-                ranges = ranges[len('bytes='):].split(',')
-                parsed_ranges = []
-                for arange in ranges:
-                    arange = arange.strip()
-                    if arange.startswith('-'):
-                        arange = int(arange[1:])
-                        if size - arange < size:
-                            parsed_ranges.append((size - arange, arange))
-                    elif arange.endswith('-'):
-                        arange = int(arange[:-1])
-                        if arange < size:
-                            parsed_ranges.append((arange, size - arange))
-                    elif '-' in arange:
-                        (rfrom, rto) = arange.split('-')
-                        rfrom = int(rfrom)
-                        rto = int(rto)
-                        if rfrom < size:
-                            parsed_ranges.append((rfrom, rto - rfrom + 1))
-                if not parsed_ranges:
-                    raise apache.SERVER_RETURN, apache.HTTP_RANGE_NOT_SATISFIABLE
-                if len(parsed_ranges) == 1:
-                    return single_range(size, parsed_ranges[0])
-                else:
-                    return multiple_ranges(size, parsed_ranges, mime)
-            except apache.SERVER_RETURN:
-                raise
+                raise Exception, '%s exists but is empty' % fullpath
             except Exception:
-                if 'Content-Range' in req.headers_out:
-                    del req.headers_out['Content-Range']
-        normal_streaming(size)
+                register_exception(req, alert_admin=True)
+            raise apache.SERVER_RETURN, apache.HTTP_NOT_FOUND
+        if headers['if-modified-since'] and headers['if-modified-since'] >= mtime:
+            raise apache.SERVER_RETURN, apache.HTTP_NOT_MODIFIED
+        if headers['if-none-match']:
+            if etag is not None and etag in headers['if-none-match']:
+                raise apache.SERVER_RETURN, apache.HTTP_NOT_MODIFIED
+        if headers['unless-modified-since'] and headers['unless-modified-since'] < mtime:
+            return normal_streaming(size)
+        if headers['range']:
+            if headers['if-range']:
+                if etag is None or etag not in headers['if-range']:
+                    return normal_streaming(size)
+            ranges = fix_ranges(headers['range'], size)
+            if len(ranges) > 1:
+                return multiple_ranges(size, ranges, mime)
+            elif ranges:
+                return single_range(size, ranges[0])
+            else:
+                raise apache.SERVER_RETURN, apache.HTTP_RANGE_NOT_SATISFIABLE
+        else:
+            return normal_streaming(size)
     else:
-        raise InvenioWebSubmitFileError, "%s does not exists!" % fullpath
+        raise apache.SERVER_RETURN, apache.HTTP_NOT_FOUND
 
 def stream_restricted_icon(req):
     """Return the content of the "Restricted Icon" file."""
-    return stream_file(req, '%s/img/restricted.gif' % CFG_WEBDIR, 'restricted', 'image/gif')
+    return stream_file(req, '%s/img/restricted.gif' % CFG_WEBDIR)
 
 def list_types_from_array(bibdocs):
     """Retrieves the list of types from the given bibdoc list."""
