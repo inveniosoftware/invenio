74b048dec6f28da85ac7e424e1f3c15c0958d579
Christopher Parker <chris.parker.za@gmail.com>
2008-08-14 09:55:49 +0000

    Modified to make use of a updated way of obtaining the group_id using the group file

--- modules/websubmit/lib/functions/Create_Cplx_Approval.py
+++ modules/websubmit/lib/functions/Create_Cplx_Approval.py
@@ -32,13 +32,13 @@ import time
 
 from invenio.dbquery import run_sql
 
-def Create_Cplx_Approval(parameters, curdir, form, user_info=None):
+def Create_Cplx_Approval_test(parameters, curdir, form, user_info=None):
     global rn
     doctype = form['doctype']
     act = form['act']
     categformat = parameters['categformatDAM']
     # retrieve category
-    if re.search("<FILE:",categformat):
+    '''if re.search("<FILE:",categformat):
         filename = categformat.replace("<FILE:","")
         filename = filename.replace(">","")
         if os.path.exists("%s/%s" % (curdir,filename)):
@@ -52,19 +52,28 @@ def Create_Cplx_Approval(parameters, curdir, form, user_info=None):
         categformat = categformat.replace("<CATEG>","([^-]*)")
         category = re.match(categformat,rn).group(1)
     if category == "":
-        category = "unknown"
+        category = "unknown"'''
+
+    #Obtain the document category from the global report number (rn)
+    #Strip away the year and the document number and keep only the category
+    category = rn.split('-')
+    category = category[2]
 
     #Path of file containing group
+    group_id = ""
     if os.path.exists("%s/%s" % (curdir,'Group')):
         fp = open("%s/%s" % (curdir,'Group'),"r")
         group = fp.read()
         group = group.replace("/","_")
         group = re.sub("[\n\r]+","",group)
         group_id = run_sql ("""SELECT id FROM usergroup WHERE name = %s""", (group,))[0][0]
+    else:
+        return ""
 
     sth = run_sql("SELECT rn FROM sbmCPLXAPPROVAL WHERE  doctype=%s and categ=%s and rn=%s and type=%s and id_group=%s", (doctype,category,rn,act,group_id))
     if len(sth) == 0:
-        run_sql("INSERT INTO  sbmCPLXAPPROVAL values(%s,%s,%s,%s,'waiting',%s,'','',NOW(),NOW(),'','','','','','')", (doctype,category,rn,act,group_id,))
+        run_sql("INSERT INTO  sbmCPLXAPPROVAL values(%s,%s,%s,%s,'waiting',%s,'','',NOW(),NOW(),'','','','','','')",(doctype,category,rn,act,group_id,))
     else:
         run_sql("UPDATE sbmCPLXAPPROVAL SET dLastReq=NOW(), status='waiting', dProjectLeaderAction='' WHERE  doctype=%s and categ=%s and rn=%s and type=%s and id_group=%s", (doctype,category,rn,act,group_id))
     return ""
+
