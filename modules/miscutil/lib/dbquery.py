6f5cceebe9883b7b023922854fabebd034bce5f8
Tibor Simko <tibor.simko@cern.ch>
2009-11-10 21:34:20 +0100

    dbquery: connection safety for execute statements
    * Added more re-connection safety for run_sql() and run_sql_many() by
      enveloping cursor execute statements in a try/except clause, as it used
      to be before run_sql_many() was introduced.  This fixes recently observed
      problems of the type `MySQL server has gone away' on some configurations.
    
    * Removed unused code related to query warnings.
    
    * Other minor docstring and cosmetic changes.

--- modules/miscutil/lib/dbquery.py
+++ modules/miscutil/lib/dbquery.py
@@ -1,13 +1,3 @@
-## CDS Invenio utility to run SQL queries.  The core is taken from
-## modpython FAQ and modified to suit our needs.  The insert_id() is
-## inspired by Erik Forsberg's mod_python slides.
-
-## FIXME: note that this version of persistent connectivity to the
-## database is not thread-safe; it works allright in the prefork model
-## only (apache2-mpm-prefork).  We should rather replace it with the
-## connection pool technique when time permits.  See:
-## http://modpython.org/FAQ/faqw.py?req=show&file=faq03.003.htp
-
 ## This file is part of CDS Invenio.
 ## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008 CERN.
 ##
@@ -25,7 +15,15 @@
 ## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""CDS Invenio utility to run SQL queries."""
+"""
+CDS Invenio utilities to run SQL queries.
+
+The main API functions are:
+    - run_sql()
+    - run_sql_cached()
+    - run_sql_many()
+but see the others as well.
+"""
 
 __revision__ = "$Id$"
 
@@ -48,14 +46,6 @@ from invenio.config import CFG_ACCESS_CONTROL_LEVEL_SITE, \
     CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT
 from invenio.errorlib import register_exception
 
-## Set the following flag to True in order to transform warning printed
-## during run_sql usage into very verbose exception, useful for debugging
-## purpouses.
-CFG_RUN_SQL_WARNINGS_INTO_VERBOSE_EXCEPTIONS=False
-
-if CFG_RUN_SQL_WARNINGS_INTO_VERBOSE_EXCEPTIONS:
-    warnings.filterwarnings('error', category=Warning, module='invenio\.dbquery')
-
 if CFG_MISCUTIL_SQL_USE_SQLALCHEMY:
     try:
         import sqlalchemy.pool as pool
@@ -68,12 +58,14 @@ if CFG_MISCUTIL_SQL_USE_SQLALCHEMY:
 else:
     from MySQLdb import connect
 
-## DB config variables.  These variables are to be set in invenio.conf
-## by admins and then replaced in situ in this file by calling
-## "inveniocfg --update-dbexec".
+## DB config variables.  These variables are to be set in
+## invenio-local.conf by admins and then replaced in situ in this file
+## by calling "inveniocfg --update-dbexec".
 ## Note that they are defined here and not in config.py in order to
 ## prevent them from being exported accidentally elsewhere, as no-one
 ## should know DB credentials but this file.
+## FIXME: this is more of a blast-from-the-past that should be fixed
+## both here and in inveniocfg when the time permits.
 CFG_DATABASE_HOST = 'localhost'
 CFG_DATABASE_PORT = '3306'
 CFG_DATABASE_NAME = 'cdsinvenio'
@@ -86,7 +78,6 @@ try:
     _db_cache
 except NameError:
     _db_cache = {}
-
 
 def _db_login(relogin = 0):
     """Login to the database."""
@@ -135,29 +126,6 @@ def _db_logout():
         del _DB_CONN[get_ident()]
     except KeyError:
         pass
-
-def _db_cursor():
-    """Retrieves the db cursor - reconnects in case of need"""
-    try:
-        db = _db_login()
-        cur = db.cursor()
-        return cur
-    except OperationalError: # unexpected disconnect, bad malloc error, etc
-        # FIXME: now reconnect is always forced, we may perhaps want to ping() first?
-        try:
-            db = _db_login(relogin = 1)
-            cur = db.cursor()
-            return cur
-        except OperationalError: # again an unexpected disconnect, bad malloc error, etc
-            raise
-    except Warning, wmsg:
-        msg = "Warning in executing query:\n%s\nwith parameters:\n%s\n%s" % (sql, param, wmsg)
-        print >> sys.stderr, msg
-        try:
-            raise Warning(msg)
-        except Warning:
-            register_exception()
-            raise
 
 def run_sql_cached(sql, param=None, n=0, with_desc=0, affected_tables=['bibrec']):
     """
@@ -217,26 +185,28 @@ def run_sql_cached(sql, param=None, n=0, with_desc=0, affected_tables=['bibrec']
 def run_sql(sql, param=None, n=0, with_desc=0):
     """Run SQL on the server with PARAM and return result.
 
-        @param param: tuple of string params to insert in the query
-                      (see notes below)
-        @param n: number of tuples in result (0 for unbounded)
-        @param with_desc: if true, will return a
-                          DB API 7-tuple describing columns in query
-        @return: if SELECT, SHOW, DESCRIBE statements: tuples of data, followed
-                                                       by description if parameter
-                                                       provided
-                 if INSERT: last row id.
-                 else: SQL result as provided by database
+    @param param: tuple of string params to insert in the query (see
+        notes below)
 
-       When the site is closed for maintenance (as governed by the
-       config variable CFG_ACCESS_CONTROL_LEVEL_SITE), do not attempt
-       to run any SQL queries but return empty list immediately.
-       Useful to be able to have the website up while MySQL database
-       is down for maintenance, hot copies, table repairs, etc.
+    @param n: number of tuples in result (0 for unbounded)
 
-       In case of problems, exceptions are returned according to the
-       Python DB API 2.0.  The client code can import them from this
-       file and catch them.
+    @param with_desc: if True, will return a DB API 7-tuple describing
+        columns in query.
+
+    @return: If SELECT, SHOW, DESCRIBE statements, return tuples of
+        data, followed by description if parameter with_desc is
+        provided.  If INSERT, return last row id.  Otherwise return
+        SQL result as provided by database.
+
+    @note: When the site is closed for maintenance (as governed by the
+        config variable CFG_ACCESS_CONTROL_LEVEL_SITE), do not attempt
+        to run any SQL queries but return empty list immediately.
+        Useful to be able to have the website up while MySQL database
+        is down for maintenance, hot copies, table repairs, etc.
+
+    @note: In case of problems, exceptions are returned according to
+        the Python DB API 2.0.  The client code can import them from
+        this file and catch them.
     """
 
     if CFG_ACCESS_CONTROL_LEVEL_SITE == 3:
@@ -248,8 +218,18 @@ def run_sql(sql, param=None, n=0, with_desc=0):
     if param:
         param = tuple(param)
 
-    cur = _db_cursor()
-    rc = cur.execute(sql, param)
+    try:
+        db = _db_login()
+        cur = db.cursor()
+        rc = cur.execute(sql, param)
+    except OperationalError: # unexpected disconnect, bad malloc error, etc
+        # FIXME: now reconnect is always forced, we may perhaps want to ping() first?
+        try:
+            db = _db_login(relogin=1)
+            cur = db.cursor()
+            rc = cur.execute(sql, param)
+        except OperationalError: # again an unexpected disconnect, bad malloc error, etc
+            raise
 
     if string.upper(string.split(sql)[0]) in ("SELECT", "SHOW", "DESC", "DESCRIBE"):
         if n:
@@ -267,24 +247,38 @@ def run_sql(sql, param=None, n=0, with_desc=0):
 
 def run_sql_many(query, params, limit=CFG_MISCUTIL_SQL_RUN_SQL_MANY_LIMIT):
     """Run SQL on the server with PARAM.
-    This method does execute_many and is therefore more efficient than execute
+    This method does executemany and is therefore more efficient than execute
     but it has sense only with queries that affect state of a database
     (INSERT, UPDATE). That is why the results just count number of affected rows
 
     @param params: tuple of tuple of string params to insert in the query
-        (see notes below)
-    @param limit: query will be executed in a cycle for parameters >limit
-        (each iteration receive at most "limit" parameters)
+
+    @param limit: query will be executed in parts when number of
+         parameters is greater than limit (each iteration runs at most
+         `limit' parameters)
+
     @return: SQL result as provided by database
     """
-    cur = _db_cursor()
     i = 0
     r = None
     while i < len(params):
+        ## make partial query safely (mimicking procedure from run_sql())
+        try:
+            db = _db_login()
+            cur = db.cursor()
+            rc = cur.executemany(query, params[i:i+limit])
+        except OperationalError:
+            try:
+                db = _db_login(relogin=1)
+                cur = db.cursor()
+                rc = cur.executemany(query, params[i:i+limit])
+            except OperationalError:
+                raise
+        ## collect its result:
         if r is None:
-            r = cur.executemany(query, params[i:i+limit])
+            r = rc
         else:
-            r += cur.executemany(query, params[i:i+limit])
+            r += rc
         i += limit
     return r
 
@@ -407,9 +401,10 @@ def get_table_status_info(tablename):
 def serialize_via_marshal(obj):
     """Serialize Python object via marshal into a compressed string."""
     return compress(marshal.dumps(obj))
-def deserialize_via_marshal(string):
+
+def deserialize_via_marshal(astring):
     """Decompress and deserialize string into a Python object via marshal."""
-    return marshal.loads(decompress(string))
+    return marshal.loads(decompress(astring))
 
 try:
     import psyco
