b0b11f1245306798cd1b6459d7699ff49d1c8317
Jerome Caffaro <jerome.caffaro@cern.ch>
2009-11-30 10:10:50 +0100

    WebJournal: do not try to display empty fields

--- modules/webjournal/lib/elements/bfe_webjournal_article_author.py
+++ modules/webjournal/lib/elements/bfe_webjournal_article_author.py
@@ -55,6 +55,8 @@ def format(bfo, separator, display_email='yes',
 
     authors_list = []
     for author, email in zip(authors, emails):
+        if not author:
+            continue
         if email.strip() and display_email.lower() == 'yes' :
             authors_list.append(create_html_mailto(email,
                                                    link_label=author,
