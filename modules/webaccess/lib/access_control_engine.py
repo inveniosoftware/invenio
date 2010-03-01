7b02dd575d6c25a29e7fd5dd70127165f122282d
Samuele Kaplun <samuele.kaplun@cern.ch>
2010-03-01 13:59:16 +0100

    WebAccess: new API acc_get_authorized_emails()
    * Added new API acc_get_authorized_emails() function, to retrieve
      in a best effort way, all the emails authorized to a given
      action/arguments.

--- modules/webaccess/lib/access_control_engine.py
+++ modules/webaccess/lib/access_control_engine.py
@@ -20,14 +20,20 @@
 __revision__ = "$Id$"
 
 import cgi
+import sys
 from urllib import quote
+
+if sys.hexversion < 0x2040000:
+    # pylint: disable-msg=W0622
+    from sets import Set as set
+    # pylint: enable-msg=W0622
 
 from invenio.config import CFG_SITE_SECURE_URL
 from invenio.dbquery import run_sql_cached
-from invenio.access_control_admin import acc_find_possible_roles, acc_is_user_in_role, CFG_SUPERADMINROLE_ID
+from invenio.access_control_admin import acc_find_possible_roles, acc_is_user_in_role, CFG_SUPERADMINROLE_ID, acc_get_role_users
 from invenio.access_control_config import CFG_WEBACCESS_WARNING_MSGS, CFG_WEBACCESS_MSGS
 from invenio.webuser import collect_user_info
-from invenio.access_control_firerole import acc_firerole_suggest_apache_p, deserialize
+from invenio.access_control_firerole import acc_firerole_suggest_apache_p, deserialize, load_role_definition, acc_firerole_extract_emails
 from invenio.urlutils import make_canonical_urlargd
 
 CFG_CALLED_FROM_APACHE = 1 #1=web,0=cli
@@ -118,3 +124,25 @@ def acc_authorize_action(req, name_action, authorized_if_no_roles=False, **argum
             return (20, CFG_WEBACCESS_WARNING_MSGS[20] % cgi.escape(name_action))
     ## User is not authorized
     return (1, "%s %s %s" % (CFG_WEBACCESS_WARNING_MSGS[1], (CFG_CALLED_FROM_APACHE and "%s %s" % (CFG_WEBACCESS_MSGS[0] % quote(user_info['uri']), CFG_WEBACCESS_MSGS[1]) or ""), make_apache_message(name_action, arguments, user_info['uri'])))
+
+def acc_get_authorized_emails(name_action, **arguments):
+    """
+    Given the action and its arguments, try to retireve all the matching
+    email addresses of users authorized.
+    This is a best effort operation, because if a role is authorized and
+    happens to be defined using a FireRole rule based on regular expression
+    or on IP addresses, non every email might be returned.
+    @param name_action: the name of the action.
+    @type name_action: string
+    @param arguments: the arguments to the action.
+    @return: the list of authorized emails.
+    @rtype: set of string
+    """
+    authorized_emails = set()
+    roles = acc_find_possible_roles(name_action, always_add_superadmin=False, **arguments)
+    for id_role in roles:
+        for dummy1, email, dummy2 in acc_get_role_users(id_role):
+            authorized_emails.add(email.lower().strip())
+        firerole = load_role_definition(id_role)
+        authorized_emails.union(acc_firerole_extract_emails(firerole))
+    return authorized_emails
