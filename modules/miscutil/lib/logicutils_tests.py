2a3d069242f44407675386c490c2e7fb97f03cbc
Tibor Simko <tibor.simko@cern.ch>
2010-09-15 10:42:58 +0200

    logicutils: fix unit test suite import
    * Fix test suite import that was left unrenamed during merge changes.
      (Whoops.)
    
    * Added copyright notice.

--- modules/miscutil/lib/logicutils_tests.py
+++ modules/miscutil/lib/logicutils_tests.py
@@ -1,9 +1,27 @@
 # -*- coding: utf-8 -*-
+##
+## This file is part of CDS Invenio.
+## Copyright (C) 2010 CERN.
+##
+## CDS Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## CDS Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with CDS Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
 """Unit tests for logic library."""
 
 import unittest
 
-from invenio.logic import expr, Expr, to_cnf, pl_true
+from invenio.logicutils import expr, Expr, to_cnf, pl_true
 from invenio.testutils import make_test_suite, run_test_suite
 
 class exprExprOpsTest(unittest.TestCase):
