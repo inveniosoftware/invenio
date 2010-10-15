6b78cdbf46abc2b1fee1cced9c2333e441526971
Jan Iwaszkiewicz <jan.iwaszkiewicz@cern.ch>
2010-10-15 16:39:23 +0200

    BibFormat: full-text snippet selection improved
    * grep only for exact matches or in case of absense of those, match words
      beginning with the stem of a pattern
    * avoid highlighting substrings which are parts of non-pattern words
    * while cutting out the snippet, use re for matching patterns insread of
      the previous method
    * ',' or '-' are properly recognized as word boundary
    * "disappearing snippets" problem is fixed (it occured for multi-word search
      patterns) (see #272)
    * in the input strings, the parenthesis are removed except in case there is
      no space between them, e.g. U(1)
    * the highlight method is now also using the new highlight_matches method
      and "escapes" search terms

--- modules/bibformat/lib/bibformat_utils.py
+++ modules/bibformat/lib/bibformat_utils.py
@@ -40,7 +40,23 @@ from invenio.urlutils import string_to_numeric_char_reference
 from invenio.textutils import encode_for_xml
 from invenio.shellutils import run_shell_command
 
-def highlight(text, keywords=None, prefix_tag='<strong>', suffix_tag="</strong>"):
+def highlight_matches(text, compiled_pattern, \
+                      prefix_tag='<strong>', suffix_tag="</strong>"):
+    """
+    Highlight words in 'text' matching the 'compiled_pattern'
+    """
+
+    #Add 'prefix_tag' and 'suffix_tag' before and after 'match'
+    #FIXME decide if non english accentuated char should be desaccentuaded
+    def replace_highlight(match):
+        """ replace match.group() by prefix_tag + match.group() + suffix_tag"""
+        return prefix_tag + match.group() + suffix_tag
+
+    #Replace and return keywords with prefix+keyword+suffix
+    return compiled_pattern.sub(replace_highlight, text)
+
+def highlight(text, keywords=None, \
+              prefix_tag='<strong>', suffix_tag="</strong>"):
     """
     Returns text with all words highlighted with given tags (this
     function places 'prefix_tag' and 'suffix_tag' before and after
@@ -56,17 +72,16 @@ def highlight(text, keywords=None, prefix_tag='<strong>', suffix_tag="</strong>"
     if not keywords:
         return text
 
-    #FIXME decide if non english accentuated char should be desaccentuaded
-    def replace_highlight(match):
-        """ replace match.group() by prefix_tag + match.group() + suffix_tag"""
-        return prefix_tag + match.group() + suffix_tag
-
+    escaped_keywords = []
+    for k in keywords:
+        escaped_keywords.append(re.escape(k))
     #Build a pattern of the kind keyword1 | keyword2 | keyword3
-    pattern = '|'.join(keywords)
+    pattern = '|'.join(escaped_keywords)
     compiled_pattern = re.compile(pattern, re.IGNORECASE)
 
     #Replace and return keywords with prefix+keyword+suffix
-    return compiled_pattern.sub(replace_highlight, text)
+    return highlight_matches(text, compiled_pattern, \
+                             prefix_tag, suffix_tag)
 
 def get_contextual_content(text, keywords, max_lines=2):
     """
@@ -579,7 +594,8 @@ def get_pdf_snippets(recID, patterns,
             # no hit, so check stemmed versions:
             from invenio.bibindex_engine_stemmer import stem
             stemmed_patterns = [stem(p, 'en') for p in patterns]
-            out = get_text_snippets(text_path, stemmed_patterns, nb_words_around, max_snippets)
+            out = get_text_snippets(text_path, stemmed_patterns,
+                                    nb_words_around, max_snippets, False)
         if out:
             out_courtesy = ""
             if text_path_courtesy:
@@ -590,13 +606,15 @@ def get_pdf_snippets(recID, patterns,
     else:
         return ""
 
-def get_text_snippets(textfile_path, patterns, nb_words_around, max_snippets):
+def get_text_snippets(textfile_path, patterns, nb_words_around, max_snippets, \
+                      right_boundary = True):
     """
     Extract text snippets around 'patterns' from file found at 'textfile_path'
     The snippets are meant to look like in the results of the popular search
     engine: using " ... " between snippets.
     For empty patterns it returns ""
     The idea is to first produce big snippets with grep and narrow them
+    @param right_boundary: match the right word boundary
     TODO: - distinguish the beginning of sentences and try to make the snippets
           start there
     """
@@ -604,6 +622,21 @@ def get_text_snippets(textfile_path, patterns, nb_words_around, max_snippets):
     if len(patterns) == 0:
         return ""
 
+    # split multiword patterns
+    # escape the parenthesis unless there is no space between then (e.g. U(1))
+    escaped_keywords = []
+    for p in patterns:
+        for w in str(p).split():
+            #if there are both '(' and ')' in one word we leave them
+            if w.count('(') or w.count(')'):
+                if re.match("\w*\(\w*\)\w*", w):
+                    w1 = w.replace('(', '\(')
+                    escaped_keywords.append(w1.replace(')', '\)'))
+                else:
+                    w1 = w.replace('(', '')
+                    escaped_keywords.append(w1.replace(')', ''))
+            else:
+                escaped_keywords.append(w)
     # the max number of words that can still be added to the snippet
     words_left = max_snippets * (nb_words_around * 2 + 1)
     # Assuming that there will be at least one word per line we can produce the
@@ -611,19 +644,23 @@ def get_text_snippets(textfile_path, patterns, nb_words_around, max_snippets):
     # FIXME: the ligature replacement should be done at the textification time;
     # then the sed expression can go away here.
     cmd = "sed \'s/ﬀ/ff/g; s/ﬁ/fi/g; s/ﬂ/fl/g; s/ﬃ/ffi/g; s/ﬄ/ffl/g\' \
-           %s | grep -i -A%s -B%s -m%s"
+           %s | grep -i -E -A%s -B%s -m%s"
     cmdargs = [textfile_path, str(nb_words_around), str(nb_words_around), str(max_snippets)]
-    for p in patterns:
-        cmd += " -e %s"
+    for p in escaped_keywords:
+        cmd += " -e \"(\\b|\\s)\"%s"
+        if right_boundary:
+            cmd += "\"(\\b|\\s)\""
         cmdargs.append(p)
     (dummy1, output, dummy2) = run_shell_command(cmd, cmdargs)
 
+    #print cmd, cmdargs, output
     result = []
     big_snippets = output.split("--")
 
     # cut the snippets to match the nb_words_around parameter precisely:
     for s in big_snippets:
-        small_snippet = cut_out_snippet(s, patterns, nb_words_around, words_left)
+        small_snippet = cut_out_snippet(s, escaped_keywords, nb_words_around, \
+        words_left, right_boundary)
         #count words
         words_left -= len(small_snippet.split())
         #if words_left <= 0:
@@ -636,24 +673,25 @@ def get_text_snippets(textfile_path, patterns, nb_words_around, max_snippets):
         if snippet:
             if out:
                 out += "<br>"
-            out += "..." + highlight(snippet, patterns) + "..."
+            out += "..." + snippet + "..."
     return out
 
-def cut_out_snippet(text, patterns, nb_words_around, max_words):
+def cut_out_snippet(text, patterns, nb_words_around, max_words, right_boundary = True):
     # the snippet can include many occurances of the patterns if they are not
     # further appart than 2 * nb_words_around
 
-    def starts_with_any(word, patterns):
-        # Check whether the word's beginning matches any of the patterns.
-        # The second argument is an array of patterns to match.
+    def matches_any(w1):
+        if compiled_pattern.search(' ' + w1 + ' '):
+            return True
+        else:
+            return False
 
-        ret = False
-        lower_case = word.lower()
-        for p in patterns:
-            if lower_case.startswith(str(p).lower()):
-                ret = True
-                break
-        return ret
+    #Build a pattern of the kind keyword1 | keyword2 | keyword3
+    if right_boundary:
+        pattern = '(\\b|\\s)(' + '|'.join(patterns) + ')(\\b|\\s)'
+    else:
+        pattern = '(\\b|\\s)(' + '|'.join(patterns) + ')'
+    compiled_pattern = re.compile(pattern, re.IGNORECASE | re.UNICODE)
 
     # make the nb_words_around smaller if required by max_words
     # to make sure that at least one pattern is included
@@ -668,7 +706,7 @@ def cut_out_snippet(text, patterns, nb_words_around, max_words):
     last_written_word = -1
     i = 0
     while i < len(words):
-        if starts_with_any(words[i], patterns):
+        if matches_any(words[i]):
             # add part before first or following occurance of a word
             j = max(last_written_word + 1, i - nb_words_around)
             while j < i:
@@ -681,7 +719,7 @@ def cut_out_snippet(text, patterns, nb_words_around, max_words):
             # write the suffix. If pattern found, break
             j = 1
             while j <= nb_words_around and i + j < len(words):
-                if starts_with_any(words[i+j], patterns):
+                if matches_any(words[i+j]):
                     break
                 else:
                     snippet += (" " + words[i+j])
@@ -691,6 +729,7 @@ def cut_out_snippet(text, patterns, nb_words_around, max_words):
         else:
             i += 1
 
+    snippet = highlight_matches(snippet, compiled_pattern)
     # apply max_words param if needed
     snippet_words = snippet.split()
     length = len(snippet_words)
