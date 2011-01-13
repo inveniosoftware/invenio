faffe97352c9460625610e457aded93590fb5208
Jerome Caffaro <jerome.caffaro@cern.ch>
2011-01-13 10:12:15 +0100

    OAI Harvest: fix msg string

--- modules/bibharvest/lib/oai_harvest_admin.py
+++ modules/bibharvest/lib/oai_harvest_admin.py
@@ -1129,7 +1129,7 @@ def perform_request_viewhprecord(hpupdate_id, ln = CFG_SITE_LANG, confirm=0):
         hp_label = bibharvest_templates.tmpl_print_brs(ln, 2) + "Holdingpen version of record"\
             + bibharvest_templates.tmpl_print_brs(ln, 1)
     except:
-        return _("Error when formatting the Holding Pen entry. Probably it's content is broken")
+        return _("Error when formatting the Holding Pen entry. Probably its content is broken")
     submit_link = create_html_link(urlbase=oai_harvest_admin_url + \
                                        "/accepthprecord",
                                    urlargd={'ln': ln,
