f2210d15f2c8402c40b08b69b477afdde7762c3a
Javier Martin <javier.martin.montull@cern.ch>
2011-01-17 14:50:41 +0100

    BibEditMulti: remove sysno column when displaying MARC
    * When displaying search results in MARC format, do not
      add a sysno column

--- modules/bibedit/lib/bibeditmulti_engine.py
+++ modules/bibedit/lib/bibeditmulti_engine.py
@@ -397,19 +397,13 @@ def _create_marc(records_xml):
 
     records = bibrecord.create_records(records_xml)
     for (record, status_code, list_of_errors) in records:
-        # The system number is in field 970a
-        # By this reason it should exist in the MARC XML
-        # otherwise it will be None in the output ALEPH marc
-        sysno_options = {"text-marc":0}
-        sysno = xmlmarc2textmarc.get_sysno_from_record(record,
-                                                       sysno_options)
 
-        if sysno == None:
-            sysno = ""
+        sysno = ""
 
         options = {"aleph-marc":0, "correct-mode":1, "append-mode":0,
                    "delete-mode":0, "insert-mode":0, "replace-mode":0,
                    "text-marc":1}
+
         aleph_record = xmlmarc2textmarc.create_marc_record(record,
                                                            sysno,
                                                            options)
