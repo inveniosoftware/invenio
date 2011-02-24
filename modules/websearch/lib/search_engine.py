9cdc08fe7cefd7e68cd262d45adec10d04f6ff4d
valkyrie <vasavage@gmail.com>
2011-02-24 15:45:17 -0800

    WebSearch: fix search term suggestion "oddity"
    * added quoting to suggested "terms" that contain more than one word
      in order to prevent concatenation of several same terms which also
      results in several failed searches (fixes #500)

--- modules/websearch/lib/search_engine.py
+++ modules/websearch/lib/search_engine.py
@@ -2727,6 +2727,8 @@ def create_nearest_terms_box(urlargd, p, f, t='w', n=5, ln=CFG_SITE_LANG, intro_
                         break
                 else:
                     if string.find(argd_px, f+':'+p) > -1:
+                        if string.find(term.strip(), ' ') > -1:
+                            term = '"' + term + '"'
                         argd[px] = string.replace(argd_px, f+':'+p, f+':'+term)
                         break
                     elif string.find(argd_px, f+':"'+p+'"') > -1:
