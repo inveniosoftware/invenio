4c69265348a2466fa5bc3876ab83e578c1ae785e
Jerome Caffaro <jerome.caffaro@cern.ch>
2011-03-08 09:20:50 +0100

    BibHarvest: fix MARCXML creation in OAI updater
    * Fix creation of MARCXML to correct records in OAI Repository Updater.
      Added corresponding test cases.
    
    * Add <collection> tags around created output.
    
    * Move some tests from "unit tests" file to "regression tests" one.

--- modules/bibharvest/lib/oai_repository_regression_tests.py
+++ modules/bibharvest/lib/oai_repository_regression_tests.py
@@ -23,8 +23,13 @@ __revision__ = "$Id$"
 
 import unittest
 import time
+import re
 
-from invenio.config import CFG_SITE_URL, CFG_OAI_SLEEP
+from invenio.config import CFG_SITE_URL, \
+     CFG_OAI_SLEEP, \
+     CFG_OAI_LOAD, \
+     CFG_OAI_ID_FIELD
+from invenio import oai_repository_server, search_engine, oai_repository_updater
 from invenio.testutils import make_test_suite, run_test_suite, \
                               test_web_page_content, merge_error_messages
 
@@ -61,7 +66,124 @@ class OAIRepositoryWebPagesAvailabilityTest(unittest.TestCase):
             self.fail(merge_error_messages(error_messages))
         return
 
-TEST_SUITE = make_test_suite(OAIRepositoryWebPagesAvailabilityTest)
+class TestSelectiveHarvesting(unittest.TestCase):
+    """Test set, from and until parameters used to do selective harvesting."""
+
+    def test_set(self):
+        """oairepository - testing selective harvesting with 'set' parameter"""
+        self.assertNotEqual([], oai_repository_server.oaigetsysnolist(set="cern:experiment"))
+        self.assert_("Multifractal analysis of minimum bias events" in \
+                     ''.join([oai_repository_server.print_record(recID) for recID in \
+                              oai_repository_server.oaigetsysnolist(set="cern:experiment")]))
+        self.assert_("Multifractal analysis of minimum bias events" not in \
+                     ''.join([oai_repository_server.print_record(recID) for recID in \
+                              oai_repository_server.oaigetsysnolist(set="cern:theory")]))
+        self.assertEqual([], oai_repository_server.oaigetsysnolist(set="nonExistingSet"))
+
+    def test_from_and_until(self):
+        """oairepository - testing selective harvesting with 'from' and 'until' parameters"""
+
+        # List available records, get datestamps and play with them
+        identifiers = oai_repository_server.oailistidentifiers("")
+        datestamps = re.findall('<identifier>(?P<id>.*)</identifier>\s*<datestamp>(?P<date>.*)</datestamp>', identifiers)
+
+        sample_datestamp = datestamps[0][1] # Take one datestamp
+        sample_oai_id = datestamps[0][0] # Take corresponding oai id
+        sample_id = search_engine.perform_request_search(p=sample_oai_id,
+                                                         f=CFG_OAI_ID_FIELD)[0] # Find corresponding system number id
+
+        # There must be some datestamps
+        self.assertNotEqual([], datestamps)
+
+        # We must be able to retrieve an id with the date we have just found
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp))
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp))
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp, \
+                                                                 fromdate=sample_datestamp))
+
+        # Same, with short format date. Eg 2007-12-13
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp.split('T')[0]))
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp.split('T')[0]))
+        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp.split('T')[0], \
+                                                                 untildate=sample_datestamp.split('T')[0]))
+
+        # At later date (year after) we should not find our id again
+        sample_datestamp_year = int(sample_datestamp[0:4])
+        sample_datestamp_rest = sample_datestamp[4:]
+        later_datestamp = str(sample_datestamp_year + 1) + sample_datestamp_rest
+        self.assert_(sample_id not in oai_repository_server.oaigetsysnolist(fromdate=later_datestamp))
+
+        # At earlier date (year before) we should not find our id again
+        earlier_datestamp = str(sample_datestamp_year - 1) + sample_datestamp_rest
+        self.assert_(sample_id not in oai_repository_server.oaigetsysnolist(untildate=earlier_datestamp))
+
+        # From earliest date to latest date must include all oai records
+        dates = [(time.mktime(time.strptime(date[1], "%Y-%m-%dT%H:%M:%SZ")), date[1]) for date in datestamps]
+        dates = dict(dates)
+        sorted_times = dates.keys()
+        sorted_times.sort()
+        earliest_datestamp = dates[sorted_times[0]]
+        latest_datestamp = dates[sorted_times[-1]]
+        self.assertEqual(len(oai_repository_server.oaigetsysnolist()), \
+                         len(oai_repository_server.oaigetsysnolist(fromdate=earliest_datestamp, \
+                                                            untildate=latest_datestamp)))
+
+
+class TestPerformance(unittest.TestCase):
+    """Test performance of the repository """
+
+    def setUp(self):
+        """Setting up some variables"""
+        # Determine how many records are served
+        self.number_of_records = oai_repository_server.oaigetsysnolist("", "", "")
+        if CFG_OAI_LOAD < self.number_of_records:
+            self.number_of_records = CFG_OAI_LOAD
+
+    def test_response_speed_oai(self):
+        """oairepository - speed of response for oai_dc output"""
+        allowed_seconds_per_record_oai = 0.02
+
+        # Test oai ListRecords performance
+        t0 = time.time()
+        oai_repository_server.oailistrecords('metadataPrefix=oai_dc&verb=ListRecords')
+        t = time.time() - t0
+        if t > self.number_of_records * allowed_seconds_per_record_oai:
+            self.fail("""Response for ListRecords with metadataPrefix=oai_dc took too much time:
+%s seconds.
+Limit: %s seconds""" % (t, self.number_of_records * allowed_seconds_per_record_oai))
+
+    def test_response_speed_marcxml(self):
+        """oairepository - speed of response for marcxml output"""
+        allowed_seconds_per_record_marcxml = 0.05
+
+        # Test marcxml ListRecords performance
+        t0 = time.time()
+        oai_repository_server.oailistrecords('metadataPrefix=marcxml&verb=ListRecords')
+        t = time.time() - t0
+        if t > self.number_of_records * allowed_seconds_per_record_marcxml:
+            self.fail("""Response for ListRecords with metadataPrefix=marcxml took too much time:\n
+%s seconds.
+Limit: %s seconds""" % (t, self.number_of_records * allowed_seconds_per_record_marcxml))
+
+
+class TestOAIRepositoryUpdater(unittest.TestCase):
+    """Test functions in OAI_repository_updater"""
+
+    def test_marcxml_filtering(self):
+        """oairepository - test MARCXML filtering"""
+        self.assertEqual(oai_repository_updater.marcxml_filter_out_tags(98, ['088__a']),
+                         '  <datafield tag="088" ind1=" " ind2=" ">\n    <subfield code="9">SCAN-9709037</subfield>\n  </datafield>  <datafield tag="088" ind1=" " ind2=" ">\n  </datafield>')
+
+        self.assertEqual(oai_repository_updater.marcxml_filter_out_tags(98, ['088__c']),
+                         '  <datafield tag="088" ind1=" " ind2=" ">\n    <subfield code="9">SCAN-9709037</subfield>\n  </datafield>  <datafield tag="088" ind1=" " ind2=" ">\n    <subfield code="a">UCRL-8417</subfield>\n  </datafield>')
+
+        self.assertEqual(oai_repository_updater.marcxml_filter_out_tags(98, ['0248_p']),
+                         '  <datafield tag="024" ind1="8" ind2=" ">\n    <subfield code="a">oai:cds.cern.ch:SCAN-9709037</subfield>\n  </datafield>  <datafield tag="024" ind1="8" ind2=" ">\n  </datafield>')
+
+TEST_SUITE = make_test_suite(OAIRepositoryWebPagesAvailabilityTest,
+                             TestSelectiveHarvesting,
+                             TestPerformance,
+                             TestOAIRepositoryUpdater)
 
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE, warn_user=True)
--- modules/bibharvest/lib/oai_repository_tests.py
+++ modules/bibharvest/lib/oai_repository_tests.py
@@ -24,14 +24,9 @@ __revision__ = "$Id$"
 
 import unittest
 import re
-import time
 
-from invenio import oai_repository_server, search_engine
+from invenio import oai_repository_server
 from invenio.testutils import make_test_suite, run_test_suite
-
-from invenio.config import \
-     CFG_OAI_LOAD, \
-     CFG_OAI_ID_FIELD
 
 class TestVerbs(unittest.TestCase):
     """Test for OAI verb functionality."""
@@ -45,67 +40,6 @@ class TestVerbs(unittest.TestCase):
         self.assertNotEqual(None, re.search("ListSets", oai_repository_server.oailistsets("")))
         self.assertNotEqual(None, re.search("GetRecord", oai_repository_server.oaigetrecord("")))
 
-class TestSelectiveHarvesting(unittest.TestCase):
-    """Test set, from and until parameters used to do selective harvesting."""
-
-    def test_set(self):
-        """oairepository - testing selective harvesting with 'set' parameter"""
-        self.assertNotEqual([], oai_repository_server.oaigetsysnolist(set="cern:experiment"))
-        self.assert_("Multifractal analysis of minimum bias events" in \
-                     ''.join([oai_repository_server.print_record(recID) for recID in \
-                              oai_repository_server.oaigetsysnolist(set="cern:experiment")]))
-        self.assert_("Multifractal analysis of minimum bias events" not in \
-                     ''.join([oai_repository_server.print_record(recID) for recID in \
-                              oai_repository_server.oaigetsysnolist(set="cern:theory")]))
-        self.assertEqual([], oai_repository_server.oaigetsysnolist(set="nonExistingSet"))
-
-    def test_from_and_until(self):
-        """oairepository - testing selective harvesting with 'from' and 'until' parameters"""
-
-        # List available records, get datestamps and play with them
-        identifiers = oai_repository_server.oailistidentifiers("")
-        datestamps = re.findall('<identifier>(?P<id>.*)</identifier>\s*<datestamp>(?P<date>.*)</datestamp>', identifiers)
-
-        sample_datestamp = datestamps[0][1] # Take one datestamp
-        sample_oai_id = datestamps[0][0] # Take corresponding oai id
-        sample_id = search_engine.perform_request_search(p=sample_oai_id,
-                                                         f=CFG_OAI_ID_FIELD)[0] # Find corresponding system number id
-
-        # There must be some datestamps
-        self.assertNotEqual([], datestamps)
-
-        # We must be able to retrieve an id with the date we have just found
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp))
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp))
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp, \
-                                                                 fromdate=sample_datestamp))
-
-        # Same, with short format date. Eg 2007-12-13
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp.split('T')[0]))
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(untildate=sample_datestamp.split('T')[0]))
-        self.assert_(sample_id in oai_repository_server.oaigetsysnolist(fromdate=sample_datestamp.split('T')[0], \
-                                                                 untildate=sample_datestamp.split('T')[0]))
-
-        # At later date (year after) we should not find our id again
-        sample_datestamp_year = int(sample_datestamp[0:4])
-        sample_datestamp_rest = sample_datestamp[4:]
-        later_datestamp = str(sample_datestamp_year + 1) + sample_datestamp_rest
-        self.assert_(sample_id not in oai_repository_server.oaigetsysnolist(fromdate=later_datestamp))
-
-        # At earlier date (year before) we should not find our id again
-        earlier_datestamp = str(sample_datestamp_year - 1) + sample_datestamp_rest
-        self.assert_(sample_id not in oai_repository_server.oaigetsysnolist(untildate=earlier_datestamp))
-
-        # From earliest date to latest date must include all oai records
-        dates = [(time.mktime(time.strptime(date[1], "%Y-%m-%dT%H:%M:%SZ")), date[1]) for date in datestamps]
-        dates = dict(dates)
-        sorted_times = dates.keys()
-        sorted_times.sort()
-        earliest_datestamp = dates[sorted_times[0]]
-        latest_datestamp = dates[sorted_times[-1]]
-        self.assertEqual(len(oai_repository_server.oaigetsysnolist()), \
-                         len(oai_repository_server.oaigetsysnolist(fromdate=earliest_datestamp, \
-                                                            untildate=latest_datestamp)))
 
 class TestErrorCodes(unittest.TestCase):
     """Test for handling OAI error codes."""
@@ -148,47 +82,11 @@ class TestEncodings(unittest.TestCase):
         self.assertEqual("%20", oai_repository_server.escape_space(" "))
         self.assertEqual("%25%20%3F%23%3D%26%2F%3A%3B%2B", oai_repository_server.encode_for_url("% ?#=&/:;+"))
 
-class TestPerformance(unittest.TestCase):
-    """Test performance of the repository """
 
-    def setUp(self):
-        """Setting up some variables"""
-        # Determine how many records are served
-        self.number_of_records = oai_repository_server.oaigetsysnolist("", "", "")
-        if CFG_OAI_LOAD < self.number_of_records:
-            self.number_of_records = CFG_OAI_LOAD
-
-    def test_response_speed_oai(self):
-        """oairepository - speed of response for oai_dc output"""
-        allowed_seconds_per_record_oai = 0.02
-
-        # Test oai ListRecords performance
-        t0 = time.time()
-        oai_repository_server.oailistrecords('metadataPrefix=oai_dc&verb=ListRecords')
-        t = time.time() - t0
-        if t > self.number_of_records * allowed_seconds_per_record_oai:
-            self.fail("""Response for ListRecords with metadataPrefix=oai_dc took too much time:
-%s seconds.
-Limit: %s seconds""" % (t, self.number_of_records * allowed_seconds_per_record_oai))
-
-    def test_response_speed_marcxml(self):
-        """oairepository - speed of response for marcxml output"""
-        allowed_seconds_per_record_marcxml = 0.05
-
-        # Test marcxml ListRecords performance
-        t0 = time.time()
-        oai_repository_server.oailistrecords('metadataPrefix=marcxml&verb=ListRecords')
-        t = time.time() - t0
-        if t > self.number_of_records * allowed_seconds_per_record_marcxml:
-            self.fail("""Response for ListRecords with metadataPrefix=marcxml took too much time:\n
-%s seconds.
-Limit: %s seconds""" % (t, self.number_of_records * allowed_seconds_per_record_marcxml))
 
 TEST_SUITE = make_test_suite(TestVerbs,
                              TestErrorCodes,
-                             TestEncodings,
-                             TestSelectiveHarvesting,
-                             TestPerformance,)
+                             TestEncodings,)
 
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE)
