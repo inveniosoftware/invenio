dda6b62b1f37ed364e8f1e19ab1537fdd1e808a2
Roman Chyla <roman.chyla@cern.ch>
2011-04-15 10:52:06 +0200

    remote debugger: initial release
    * A new module for (remote) debugging of Apache WSGI applications.
      (closes #570)

---
+++ modules/miscutil/lib/remote_debugger.py
@@ -0,0 +1,290 @@
+## This file is part of Invenio.
+## Copyright (C) 2011 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+Module for debugging mod_python && mod_wsgi applications that run inside
+the Apache webserver (or any other webserver). This is a utility module
+that makes remote debugging possible and easy.
+"""
+
+
+
+# Debug mode is activated by passing debug=[debugger_id] in the url, when
+# you try to load a webpage using such url, the execution will stop (if
+# breakpoints are set, or automatically depending on the debugger you are
+# using). This module is only a helper utility, the actual debugging is
+# done by others.
+#
+# Each debugger has its own number:
+#
+# local winpdb: debug=1
+# remote winpdb: debug=2
+# remote pydev: debug=3
+#
+# If the debug parameter is not present, the code is executed normally
+# (without stopping).
+#
+#
+# Each debugger has its own parameters that can be set via url parameters,
+# you can also create your your own debugging functions, and assign a new
+# number to them. Please see get_debugger() function for more information on
+# how to create a new call, and see the individual debuggers for information
+# what parameters they accept.
+
+# ----------------------------- CONFIGURATION -----------------------------------------
+
+from invenio.remote_debugger_config import CFG_REMOTE_DEBUGGER_ENABLED, \
+	CFG_REMOTE_DEBUGGER_IMPORT, CFG_REMOTE_DEBUGGER_WINPDB_PASSWORD, \
+	CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_IP, CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_PORT, \
+	CFG_REMOTE_DEBUGGER_PYDEV_PATHS, CFG_REMOTE_DEBUGGER_WSGI_RELOAD, \
+	CFG_PYDEV_DEBUG
+
+# -------------------------------------------------------------------------------------
+# --------------------------- no config past this point -------------------------------
+# -------------------------------------------------------------------------------------
+from invenio import config
+import os
+import glob
+import traceback
+import sys
+from cStringIO import StringIO
+
+
+def start_file_changes_monitor():
+    from invenio import remote_debugger_wsgi_reload as monitor
+    monitor.start(interval=1.0)
+    for pattern in CFG_REMOTE_DEBUGGER_WSGI_RELOAD:
+        for f in glob.glob(os.path.join(config.CFG_PREFIX, pattern)):
+            monitor.track(f)
+
+# -------------------------------------------------------------------------------------
+# -----------------------------     DEBUGGER PART LOADING    --------------------------
+# -------------------------------------------------------------------------------------
+
+
+
+normcase = os.path.normcase
+
+# raise exception so that this module is not loaded (this modules is always imported
+# in try...except manner)
+if not CFG_REMOTE_DEBUGGER_ENABLED:
+    raise Exception('Remote debugger is disabled')
+
+
+
+# import modules that are configured for this debugger, at least for Eclipse, this
+# MUST HAPPEN before other stuff gets loaded
+for path, name in CFG_REMOTE_DEBUGGER_IMPORT.items():
+    try:
+        if '.' in path:
+            globals()[name] = __import__(path, fromlist=path.split('.'))
+        else:
+            globals()[name] = __import__(path)
+    except Exception:
+        traceback.print_exc()
+        sys.stderr.write("Error in remote_debugger, import of the %s failed" % path)
+
+
+def error_msg(debugger_args):
+    """Error has been caught and we were given chance to report it"""
+
+    debug_no, params = parse_args(debugger_args)
+
+    if debug_no == '3':
+        exc_info =  sys.exc_info()
+        if exc_info[0]:
+            exception_data = StringIO()
+            traceback.print_exception(exc_info[0], exc_info[1], exc_info[2], None, exception_data)
+            exception_data = exception_data.getvalue()
+            if exception_data.endswith('\n'):
+                    exception_data = exception_data[:-1]
+            #pydev is truncating data (no help printing in loop)
+            sys.stderr.write('\n\n...')
+            sys.stderr.write(exception_data[-600:])
+            sys.stderr.write('\n\n')
+
+
+
+def start(debugger_args):
+    """
+    Switch into a debugger mode manualy - to be called fromt the command line scripts mostly
+    @var debugger_args: string, eg. "3|ip:192.168.31.1|port:9999"
+    """
+    if not isinstance(debugger_args, list) or not isinstance(debugger_args, tuple):
+        debugger_args = [str(debugger_args)]
+
+    debug_starter = get_debugger(debugger_args)
+    if debug_starter is None:
+        raise Exception("Requested debugger not found or not initalized properly: %s" % debugger_args)
+    debug_starter()
+
+
+def get_debugger(arg):
+    """
+    Returns function that will initialize the debugger
+    @var arg: arg passed from url parameter debug=xxx
+    @return: function call
+    """
+
+    debug_no, params = parse_args(arg)
+
+    if debug_no == '1':
+        func = start_embedded_winpdb_debugger
+    elif debug_no == '2':
+        func = start_remote_winpdb_debugger
+    elif debug_no == '3':
+        func = start_remote_pydev_debugger
+    else:
+        return None
+
+    # we could determine the function signature and check arguments
+    # func.func_code.co_varnames[:func.func_code.co_argcount]
+    # but I don't do that intentionally (to raise error if something wrong is
+    # sumbmitted)
+
+
+    #raise(str(params))
+
+    return lambda: func(**params)
+
+def parse_args(arg):
+    """Parses arguments supplied through url param debug=xcxv
+    @return: tuple of debugger_no, additional_params
+    """
+    debug_no = ''
+    params = {}
+    # parse the passed-in arg
+    if '|' in arg[0]:
+        # it will raise error if something wrong happens
+        a = arg[0].split('|')
+        debug_no = a[0]
+        for k, v in map(lambda x: x.split(':'), a[1:]):
+            try:
+                v = int(v)
+            except:
+                if v == 'False':
+                    v = False
+                elif v == 'True':
+                    v = True
+            params[k] = v
+    else:
+        debug_no = arg[0]
+
+    return (debug_no, params)
+
+
+def start_embedded_winpdb_debugger(passwd=None):
+    """
+    Winpdb debugger, rpdb2 must be enabled in the
+    CFG_REMOTE_DEBUGGER_IMPORT
+
+    Change the call to suit your needs
+    """
+    p = passwd or CFG_REMOTE_DEBUGGER_WINPDB_PASSWORD
+    rpdb2.start_embedded_debugger(p)
+
+
+
+def start_remote_winpdb_debugger(passwd=None):
+    """
+    Winpdb remote debugger, change the call to suit your needs
+    """
+    p = passwd or CFG_REMOTE_DEBUGGER_WINPDB_PASSWORD
+    rpdb2.start_embedded_debugger(p, fAllowRemote=True)
+
+
+
+def start_remote_pydev_debugger(ip=None, port=None, suspend=False, stderr=True, stdout=True, path=None):
+    """
+    remote eclipse/pydev debugger, pydev and putils module should be available
+    in the CFG_REMOTE_DEBUGGER_IMPORT
+
+    If you want to change behaviour of the debugger interactively, you can
+    pass arguments encoded in the url, example:
+    http://someurl/collection/X?debug=3|ip:192.168.31.1|port:9999|stderr:0
+
+    @keyword ip: (str) the machine where the Pydev debugger is listening for incoming connections
+    @keyword port: (str) the port of the remote machine
+    @keyword suspend: (bool) whether to stop execution right after the debugger was activated
+    @keyword stderr: (bool) redirect the stderr to the remote debugging machine console
+    @keyword stdout: (bool) redirect the stdout to the remote debugging machine console
+    @keyword path: (str) list of mappings of <source> -> <target> paths separated by '#'
+
+    """
+
+    # to see the translation
+    if CFG_PYDEV_DEBUG:
+        sys.stderr.write("We set the pydev to be verbose")
+        putils.DEBUG_CLIENT_SERVER_TRANSLATION = True
+        if hasattr(pydevd, "MYDEBUG"):
+            pydevd.MYDEBUG = False
+
+    i = ip or CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_IP
+    p = port or CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_PORT
+
+    _pydev_paths = None
+    if hasattr(putils, 'PATHS_FROM_ECLIPSE_TO_PYTHON'): #never versions of Pydev
+        _pydev_paths = getattr(putils, 'PATHS_FROM_ECLIPSE_TO_PYTHON')
+    elif hasattr(putils, 'PATHS_FROM_CLIENT_TO_SERVER'): # pydev 1.5
+        _pydev_paths = getattr(putils, 'PATHS_FROM_CLIENT_TO_SERVER')
+
+    # Eclipse needs to know how to map the file from the remote server
+    if CFG_REMOTE_DEBUGGER_PYDEV_PATHS:
+        xpaths = map(lambda x: (normcase(x[0]), normcase(x[1])), CFG_REMOTE_DEBUGGER_PYDEV_PATHS)
+        for couple in xpaths:
+            if couple not in _pydev_paths:
+                _pydev_paths.append(couple)
+
+    # paths set through the url parameter
+    if path:
+        elements = path.split('#')
+        if len(elements) % 2 == 1:
+            elements.pop(-1)
+        i = 0
+        xpaths = []
+        while len(elements):
+            xpaths.append((normcase(elements.pop(0)), normcase(elements.pop(0))))
+        for couple in xpaths:
+            if couple not in _pydev_paths:
+                _pydev_paths.append(couple)
+
+
+
+    # the first argument is the IP of the (remote) machine where Eclipse Pydev
+    # is listening, we send suspend=False to not bother with stopping the code executing when
+    # pydev is initialized, set your own breakpoints inside Eclipse to stop execution
+
+
+    # this is HACK!!! we basically try to reconnect to another IP as requested on url param
+    # I dont know if it does not break pydev internals at some point
+    if (ip is not None) and hasattr(pydevd, 'oldxxxip') and pydevd.oldxxxip != ip:
+        pydevd.connected = False
+
+
+    pydevd.settrace(i,
+                    stdoutToServer=stdout,
+                    stderrToServer=stderr,
+                    port=p,
+                    suspend=suspend)
+
+    pydevd.oldxxxip = ip
+
+    if CFG_PYDEV_DEBUG:
+        sys.stderr.write("These are the mapping paths\n")
+        sys.stderr.write(str(_pydev_paths) + "\n")
+
+
---
+++ modules/miscutil/lib/remote_debugger_config.py
@@ -0,0 +1,124 @@
+## This file is part of Invenio.
+## Copyright (C) 2011 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+# global switch - if 0, remote_debugger is not loaded at all and
+# no remote debugging is available, if you run the debugger for the Invenio
+# site, make sure also your config contains the same directive
+
+CFG_REMOTE_DEBUGGER_ENABLED = 0 # by default, we don't want to enable debugger
+
+# if CFG_DEVEL_SITE is defined, then we do want to enable debugger:
+try:
+    from invenio.config import CFG_DEBUG_SITE
+    if CFG_DEBUG_SITE:
+        CFG_REMOTE_DEBUGGER_ENABLED = 1
+except ImportError:
+    pass
+
+# Modules that should be imported during initialization
+# the structure is: 'module_path' : 'name'
+#
+# So, 'pydev.pydevd': 'pydev' means: "import pydev.pydevd as pydev"
+# WARNING! The name of the module is quite important, as the functions
+# are calling them without checking if they were imported or not. That is
+# not a bug, that is a feature! You shall know what you are doing when
+# changing default names.
+#
+# You can uncomment some lines if you know you are not going to use these
+# modules for debugging.
+
+CFG_REMOTE_DEBUGGER_IMPORT = {
+                            'rpdb2' : 'rpdb2', #windpb debugging
+                            'pydev.pydevd' : 'pydevd', # eclipse/pydev
+                            'pydev.pydevd_file_utils': 'putils', # eclipse/pydev
+
+                             }
+
+# -----------------------------------------------------------------------------
+# configuration options for winpdb debugging
+
+CFG_REMOTE_DEBUGGER_WINPDB_PASSWORD = 'Change1Me'
+
+
+# -----------------------------------------------------------------------------
+# configuration options for Eclipse/Pydev
+
+# Remote debuggin with Eclipse Pydev - Apache does not need to be configured
+# as a single-worker. But you do the following:
+#
+# 1. find pydevd.py inside your Eclipse/Pydev installation
+#    (on my computer it is in: c:\dev\eclipse342\plugins\org.python.pydev.debug_1.5.0.1251989166\pysrc)
+# 2. copy the pysrc folder into the remote machine (eg. inside: /usr/lib/python2.5/site-packages
+# 3. rename the pysrc into pydev
+# 4. put __init__.py inside pydev (if you don't do that, pydev is not recognized as a package)
+#
+# Then, in your Eclipse, change perspective to the debug mode, start Pydev remote
+# debug server, set some breakpoint and reload a webpage with url param debug=3
+# eg. http://invenio-vm/?debug=3
+#
+# I repeat, you must be in the Debug perspective to catch the breakpoints!
+#
+# This is where your (local) Eclipse is listening for communication, this IP address
+# can be anything that Invenio can access (ie. the machine that is running Invenio
+# must have access to the IP)
+
+CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_IP = '127.0.0.1' #'192.168.0.1'
+CFG_REMOTE_DEBUGGER_PYDEV_REMOTE_PORT = 5678
+
+
+# When you hit a breakpoing, Eclipse needs to know which file to display. For instance:
+# you are developing inside a virtual machine (Linux) where Invenio code lives at
+# /opt/cds-invenio/lib/python/invenio. Your environment is Windows (yes, why not? ;-))
+# and you have access to the invenio folder through samba as:
+#
+# \\invenio-vm\root\opt\cds-invenio\lib\python\invenio
+#
+# (or perhaps you don't have samba but you have a local copy of the codebase somewhere else)
+#
+# You must set a mapping: local Eclipse path <--> remote Linux path
+# in this way:
+#
+# CFG_REMOTE_DEBUGGER_PYDEV_PATHS = [('\\\\invenio-vm\\root\\opt\\cds-invenio\\lib\\python\\invenio',
+#                                     '/opt/cds-invenio/lib/python/invenio')]
+#
+# what I (rca) do is to map a whole drive through samba:
+# CFG_REMOTE_DEBUGGER_PYDEV_PATHS = [('\\\\invenio-vm\\root\\',
+#                                     '/')]
+# and I also recommend using (back)slashes at the end, ie. /opt/ and not /opt
+
+CFG_REMOTE_DEBUGGER_PYDEV_PATHS = [
+                                   #('/opt/cds-invenio/lib/python/invenio/', '/usr/local/lib/python2.5/site-packages/invenio/'),
+                                   #('U:\\opt\\', '/opt/'),
+                                   #('\\\\Invenio-ubu\\root\\', '/'),
+                                   #('U:\\usr\\', '/usr/'),
+
+                                   #('\\\\invenio-ubu\\root\\usr\\', '/usr/'),
+                                   #('\\\\invenio-ubu\\root\\opt\\', '/opt/')
+                                   ]
+
+
+
+# Shall we monitor changes and restart daemon threads when you change source code?
+# Put here list of glob patters (files) to monitor. The paths are relative to the
+# Invenio root dir.
+
+CFG_REMOTE_DEBUGGER_WSGI_RELOAD = ['lib/python/invenio/*.py']
+
+
+# For debugging of a debugger ;)
+
+CFG_PYDEV_DEBUG = False
