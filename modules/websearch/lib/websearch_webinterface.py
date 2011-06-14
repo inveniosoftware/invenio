6f46fd705dbe346cc1359a59931b1e991e77dd5e
Tibor Simko <tibor.simko@cern.ch>
2011-06-14 18:15:13 +0200

    WebSearch: fix collection display normalisation
    * When displaying collection page for a collection that does not
      exist, the original name should be preserved for the error message
      display.  This fixes problems introduced as part of the collection
      name normalisation call 1824cc422cba28b387e1be5f5a8d364251827f9b.

--- modules/websearch/lib/websearch_webinterface.py
+++ modules/websearch/lib/websearch_webinterface.py
@@ -1291,8 +1291,7 @@ def display_collection(req, c, aas, verbose, ln):
     req.content_type = "text/html"
     req.send_http_header()
     # deduce collection id:
-    c = get_coll_normalised_name(c)
-    colID = get_colID(c)
+    colID = get_colID(get_coll_normalised_name(c))
     if type(colID) is not int:
         page_body = '<p>' + (_("Sorry, collection %s does not seem to exist.") % ('<strong>' + str(c) + '</strong>')) + '</p>'
         page_body = '<p>' + (_("You may want to start browsing from %s.") % ('<a href="' + CFG_SITE_URL + '?ln=' + ln + '">' + get_coll_i18nname(CFG_SITE_NAME, ln) + '</a>')) + '</p>'
