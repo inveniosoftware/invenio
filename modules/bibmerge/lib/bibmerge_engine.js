inveniosoftware/invenio

commit c4445c5896324f68e495b2f6fa245e30888e7d7f

kwalitee: check for <option> missing 'value's
* Check that <option> elements use a value=".." attribute and
  do not solely rely on the label of the option, as this label
  happens to be translated by some browsers/plugins.

* Fixes throughout code where 'value' attribute was missing.

modules/bibmerge/lib/bibmerge_engine.js
@@ -602,12 +602,12 @@ function buildSelectList(mode) {
   if (mode=='forSearch') {
     _list = gSearchResults;
     for (var i=0, n=_list[0].length; i<n; i++)
-      _optionsHtml += "<option title=\""+ _list[2][i] +"\">"+ _list[1][i] +"</option>\n";
+      _optionsHtml += "<option value=\""+ _list[1][i] +\"" title=\""+ _list[2][i] +"\">"+ _list[1][i] +"</option>\n";
   }
   else {
     _list = gRevisions;
     for (var i=0, n=_list[0].length; i<n; i++)
-      _optionsHtml += "<option>"+ _list[1][i] +"</option>\n";
+      _optionsHtml += "<option value=\""+ _list[1][i] +"\">"+ _list[1][i] +"</option>\n";
   }
   $('#bibMergeSelectListRow select').html(_optionsHtml);
 }

modules/bibedit/lib/bibeditmulti_templates.py
@@ -388,7 +388,7 @@ div .boxleft_2 {
         <td />
         <td>
         <select class="fieldActionType" onchange="onFieldActionTypeChange(this);">
-        <option>%(text_select_action)s</option>
+        <option value="%(text_select_action)s">%(text_select_action)s</option>
         <option value="0">%(text_add_field)s</option>
         <option value="1">%(text_delete_field)s</option>
         <option value="2">%(text_update_field)s</option>

modules/bibformat/lib/bibformat_templates.py
@@ -1890,7 +1890,7 @@ class Template:
         </html>
         ''' % {'docs': ', '.join(["'"+x+"'" for x in docs_list]).replace('\n','\\n'),
                'codes': ', '.join(["'"+x+"'" for x in codes_list]).replace('\n','\\n'),
-               'names': '\n'.join(['<option>'+x+'</option>' for x in names_list])}
+               'names': '\n'.join(['<option value="'+x+'">'+x+'</option>' for x in names_list])}
 
         return out
 

modules/bibknowledge/lib/bibknowledge_templates.py
@@ -257,7 +257,7 @@ class Template:
                          user gives its postal code (270__a): \
                          Set field to 100__a, expression to 270__a:*%*.")+"<br/><br/>"
         #create a pretty select box
-        selectbox = "<select name=\"collection\"><option>"+_("Any collection")+"</option>"
+        selectbox = "<select name=\"collection\"><option value=\""+ _("Any collection") +"\">"+_("Any collection")+"</option>"
         for mycoll in collections:
             selectbox += "<option value=\""+mycoll+"\""
             if mycoll == collection:

modules/bibupload/lib/batchuploader_templates.py
@@ -362,8 +362,8 @@ class Template:
         <span class="italics">%(txt2)s: /afs/cern.ch/user/j/user/public/foo/</span></div>
         <div><span class="mandatory_field""> * </span> %(txt3)s:
         <select name="matching">
-            <option>reportnumber</option>
-            <option>recid</option>
+            <option value="reportnumber">reportnumber</option>
+            <option value="recid">recid</option>
         </select>
         </div>
         <div><span class="mandatory_field""> * </span> %(txt4)s:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="radio" name="mode" value="append" "checked" id="appendcheckbox"/><label for="appendcheckbox">append</label>

modules/miscutil/demo/democfgdata.sql
@@ -1133,7 +1133,7 @@ INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_AU',NULL,'100__a','T',NULL,6,60,NULL,N
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_SUPERV',NULL,'','T',NULL,6,60,NULL,NULL,NULL,'2008-03-02','2008-03-02','<br />Thesis Supervisor(s)<br />(one per line):<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_ABS',NULL,'520__a','T',NULL,12,80,NULL,NULL,NULL,'2008-03-02','2008-03-02','<br />Abstract:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_NUMP',NULL,'300__a','I',5,NULL,NULL,NULL,NULL,NULL,'2008-03-02','2008-03-06','<br />Number of Pages:&nbsp;',NULL,0);
-INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOTHE_LANG\">\r\n        <OPTION>Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-02','2008-03-02','<br /><br />Select the Language:&nbsp;',NULL,0);
+INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOTHE_LANG\">\r\n        <option value=\"Select:\">Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-02','2008-03-02','<br /><br />Select the Language:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_PUBL',NULL,'','I',35,NULL,NULL,NULL,NULL,NULL,'2008-03-02','2008-03-02','<br />Thesis Publisher (or University):&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_PLDEF',NULL,'','I',20,NULL,NULL,NULL,NULL,NULL,'2008-03-02','2008-03-02','<br /><br />Place of Thesis Defence:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOTHE_DIPL',NULL,'','S',NULL,NULL,NULL,NULL,NULL,'<select name=\"DEMOTHE_DIPL\">\r\n <option value=\"\">Select:</option>\r\n <option value=\"Diploma\">Diploma</option>\r\n <option value=\"MSc\">MSc</option>\r\n <option value=\"PhD\">PhD</option>\r\n</select>','2008-03-02','2008-03-02','<br /><br />Diploma Awarded:<br />',NULL,0);
@@ -1153,7 +1153,7 @@ INSERT INTO sbmFIELDDESC VALUES ('DEMOART_DATE',NULL,'269__c','I',10,NULL,NULL,N
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_END',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'<div align=\"center\">\r\n<INPUT TYPE=\"button\" class=\"adminbutton\" name=\"endS\" width=\"400\" height=\"50\" value=\"Finish Submission\" onclick=\"finish();\">\r\n</div>','2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_FILE',NULL,'','F',60,NULL,NULL,NULL,NULL,NULL,'2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_KW',NULL,'6531_a','T',NULL,4,50,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br /><br />Keywords:<br /><i>(one keyword/key-phrase per line)</i><br />',NULL,0);
-INSERT INTO sbmFIELDDESC VALUES ('DEMOART_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOART_LANG\">\r\n        <OPTION>Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-07','2008-03-07','<br /><br />Select the Language:&nbsp;',NULL,0);
+INSERT INTO sbmFIELDDESC VALUES ('DEMOART_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOART_LANG\">\r\n        <option value=\"Select:\">Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-07','2008-03-07','<br /><br />Select the Language:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_NOTE',NULL,'500__a','T',NULL,6,60,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br /><br />Additional Comments or Notes:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_NUMP',NULL,'300__a','I',5,NULL,NULL,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br />Number of Pages:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOART_REP',NULL,'088__a','T',NULL,4,30,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br />Other Report Numbers <i>(one per line)</i>:',NULL,0);
@@ -1167,14 +1167,14 @@ INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_DATE',NULL,'269__c','I',10,NULL,NULL,N
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_END',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'<div align=\"center\">\r\n<INPUT TYPE=\"button\" class=\"adminbutton\" name=\"endS\" width=\"400\" height=\"50\" value=\"Finish Submission\" onclick=\"finish();\">\r\n</div>','2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_FILE',NULL,'','F',60,NULL,NULL,NULL,NULL,NULL,'2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_KW',NULL,'6531_a','T',NULL,4,50,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br /><br />Keywords:<br /><i>(one keyword/key-phrase per line)</i><br />',NULL,0);
-INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOBOO_LANG\">\r\n        <OPTION>Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-07','2008-03-07','<br /><br />Select the Language:&nbsp;',NULL,0);
+INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOBOO_LANG\">\r\n        <option value=\"Select:\">Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-07','2008-03-07','<br /><br />Select the Language:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_NOTE',NULL,'500__a','T',NULL,6,60,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br /><br />Additional Comments or Notes:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_NUMP',NULL,'300__a','I',5,NULL,NULL,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br />Number of Pages:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_REP',NULL,'088__a','T',NULL,4,30,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br />Other Report Numbers <i>(one per line)</i>:',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_RN',NULL,'037__a','I',35,NULL,NULL,NULL,'DEMO-BOOK-<YYYY>-???',NULL,'2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_TITLE',NULL,'245__a','T',NULL,5,60,NULL,NULL,NULL,'2008-03-07','2008-03-07','<br />Title:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_COMNT',NULL,'','T',NULL,6,60,NULL,NULL,NULL,'2008-03-07','2008-03-07',NULL,NULL,0);
-INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_DECSN',NULL,'','S',NULL,NULL,NULL,NULL,NULL,'<select name=\"DEMOBOO_DECSN\">\r\n<option>Select:</option>\r\n<option value=\"approve\">Approve</option>\r\n<option value=\"reject\">Reject</option>\r\n</select>\r\n','2008-03-07','2008-03-07',NULL,NULL,0);
+INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_DECSN',NULL,'','S',NULL,NULL,NULL,NULL,NULL,'<select name=\"DEMOBOO_DECSN\">\r\n<option value=\"Select:\">Select:</option>\r\n<option value=\"approve\">Approve</option>\r\n<option value=\"reject\">Reject</option>\r\n</select>\r\n','2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOBOO_REGB',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'<div align=\"center\">\r\n<INPUT TYPE=\"button\" class=\"adminbutton\" name=\"endS\" width=\"400\" height=\"50\" value=\"Register Decision\" onclick=\"finish();\">\r\n</div>','2008-03-07','2008-03-07',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_ABS',NULL,'520__a','T',NULL,20,80,NULL,NULL,NULL,'2008-03-12','2008-03-12','<br />Abstract:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_AU',NULL,'100__a','T',NULL,6,60,NULL,NULL,NULL,'2008-03-12','2008-03-12','<br />Authors: <i>(one per line)</i><br />',NULL,0);
@@ -1182,7 +1182,7 @@ INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_CHANGE',NULL,'','S',NULL,NULL,NULL,NUL
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_CONT',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'<div align=\"center\">\r\n<input type=\"button\" class=\"adminbutton\" width=\"400\" height=\"50\" name=\"endS\" value=\"Continue\" onclick=\"finish();\" />\r\n</div>','2008-03-12','2008-03-12',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_DUMMY',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'</td></tr></table><br /><br /></td></tr></table>','2008-03-12','2008-03-12',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_END',NULL,'','D',NULL,NULL,NULL,NULL,NULL,'<div align=\"center\">\r\n<INPUT TYPE=\"button\" class=\"adminbutton\" name=\"endS\" width=\"400\" height=\"50\" value=\"Finish Submission\" onclick=\"finish();\">\r\n</div>','2008-03-12','2008-03-12',NULL,NULL,0);
-INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOPOE_LANG\">\r\n        <OPTION>Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-12','2008-03-12','<br /><br />Select the Language:&nbsp;',NULL,0);
+INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_LANG',NULL,'041__a','S',NULL,NULL,NULL,NULL,NULL,'<SELECT name=\"DEMOPOE_LANG\">\r\n        <option value=\"Select:\">Select:</option>\r\n        <option value=\"eng\">English</option>\r\n        <option value=\"fre\">French</option>\r\n        <option value=\"ger\">German</option>\r\n        <option value=\"dut\">Dutch</option>\r\n        <option value=\"ita\">Italian</option>\r\n        <option value=\"spa\">Spanish</option>\r\n        <option value=\"por\">Portuguese</option>\r\n        <option value=\"gre\">Greek</option>\r\n        <option value=\"slo\">Slovak</option>\r\n        <option value=\"cze\">Czech</option>\r\n        <option value=\"hun\">Hungarian</option>\r\n        <option value=\"pol\">Polish</option>\r\n        <option value=\"nor\">Norwegian</option>\r\n        <option value=\"swe\">Swedish</option>\r\n        <option value=\"fin\">Finnish</option>\r\n        <option value=\"rus\">Russian</option>\r\n</SELECT>','2008-03-12','2008-03-12','<br /><br />Select the Language:&nbsp;',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_RN',NULL,'037__a','I',35,NULL,NULL,NULL,'DEMO-POETRY-<YYYY>-???',NULL,'2008-03-12','2008-03-12',NULL,NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_TITLE',NULL,'245__a','T',NULL,5,60,NULL,NULL,NULL,'2008-03-12','2008-03-12','<br />Title:<br />',NULL,0);
 INSERT INTO sbmFIELDDESC VALUES ('DEMOPOE_YEAR',NULL,'909C0y','I',4,NULL,NULL,4,NULL,NULL,'2008-03-12','2008-03-12','<br /><br />Year:&nbsp;',NULL,0);

modules/miscutil/lib/htmlutils.py
@@ -512,10 +512,10 @@ def create_html_select(options, selected=None, attrs=None, **other_attrs):
 
         >>> print create_html_select(["foo", "bar"], selected="bar", name="baz")
         <select name="baz">
-          <option selected="selected">
+          <option selected="selected" value="bar">
             bar
           </option>
-          <option>
+          <option value="foo">
             foo
           </option>
         </select>
@@ -532,7 +532,7 @@ def create_html_select(options, selected=None, attrs=None, **other_attrs):
     @param options: this can either be a sequence of strings or a map of
         C{key->value}. In the former case, the C{select} tag will contain
         a list of C{option} tags (in alphabetical order), where the
-        C{value} attribute is not specified. In the latter case, the
+        C{value} attribute is set to C{value}. In the latter case, the
         C{value} attribute will be set to the C{key}, while the body
         of the C{option} will be set to C{value}.
     @type options: sequence or map
@@ -548,6 +548,10 @@ def create_html_select(options, selected=None, attrs=None, **other_attrs):
     @rtype: string
 
     @note: the values and keys will be escaped for HTML.
+
+    @note: it is important that parameter C{value} is always
+        specified, in case some browser plugin play with the
+        markup, for eg. when translating the page.
     """
     body = []
     try:
@@ -560,7 +564,7 @@ def create_html_select(options, selected=None, attrs=None, **other_attrs):
         options.sort()
         for value in options:
             option_attrs = value == selected and {"selected": "selected"} or {}
-            body.append(create_html_tag("option", body=value, escape_body=True, attrs=option_attrs))
+            body.append(create_html_tag("option", body=value, escape_body=True, value=value, attrs=option_attrs))
     return create_html_tag("select", body='\n'.join(body), attrs=attrs, **other_attrs)
 
 class _LinkGetter(HTMLParser):

modules/miscutil/lib/htmlutils_tests.py
@@ -24,7 +24,8 @@ __revision__ = "$Id$"
 import unittest
 
 from invenio.htmlutils import HTMLWasher, nmtoken_from_string, \
-     remove_html_markup, CFG_TIDY_INSTALLED, \
+     remove_html_markup, create_html_select, \
+     CFG_TIDY_INSTALLED, \
      CFG_BEAUTIFULSOUP_INSTALLED, tidy_html
 from invenio.testutils import make_test_suite, run_test_suite
 
@@ -220,11 +221,21 @@ class HTMLMarkupRemovalTest(unittest.TestCase):
         self.assertEqual(remove_html_markup(test_input, 'X'),
                          test_expected)
 
+class HTMLCreation(unittest.TestCase):
+    """Test functions related to creation of HTML markup."""
+
+    def test_create_html_select(self):
+        """htmlutils - create HTML <select> list """
+        self.assertEqual(create_html_select(["foo", '"bar"'], selected="bar", name="baz"),
+                         '<select name="baz">\n  <option value="&quot;bar&quot;">\n    "bar"\n  </option>\n  <option value="foo">\n    foo\n  </option>\n</select>')
+
+
 TEST_SUITE = make_test_suite(XSSEscapingTest,
                              CharactersEscapingTest,
                              HTMLWashingTest,
                              HTMLMarkupRemovalTest,
-                             HTMLTidyingTest)
+                             HTMLTidyingTest,
+                             HTMLCreation)
 
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE)

commit 100bffd9915a1f269647d4ebe36759ac0475a911

BibMerge: fix record selection bug
* Fixes syntax error related to the selection option change introduced in
  c4445c5896324f68e495b2f6fa245e30888e7d7f that was preventing BibMerge
  interface's record selection box from working.

modules/bibmerge/lib/bibmerge_engine.js
@@ -602,12 +602,12 @@ function buildSelectList(mode) {
   if (mode=='forSearch') {
     _list = gSearchResults;
     for (var i=0, n=_list[0].length; i<n; i++)
-      _optionsHtml += "<option value=\""+ _list[1][i] +\"" title=\""+ _list[2][i] +"\">"+ _list[1][i] +"</option>\n";
+      _optionsHtml += "<option value=\"" + _list[1][i] + "\" title=\"" + _list[2][i] + "\">"+ _list[1][i] + "</option>\n";
   }
   else {
     _list = gRevisions;
     for (var i=0, n=_list[0].length; i<n; i++)
-      _optionsHtml += "<option value=\""+ _list[1][i] +"\">"+ _list[1][i] +"</option>\n";
+      _optionsHtml += "<option value=\"" + _list[1][i] + "\">" + _list[1][i] + "</option>\n";
   }
   $('#bibMergeSelectListRow select').html(_optionsHtml);
 }