efaf9a605a662bed024bdb8b658dfdb4b101aa4b
Javier Martin <javier.martin.montull@cern.ch>
2011-09-27 18:31:46 +0200

    BibEdit: use regex to check record locking
    * Optionally, use regex to check if bibupload tasks present
      in the bibsched queue may prevent record opening in BibEdit.
      The regexp technique is faster then the default bibrecord
      technique to check for possible record locking needs, but
      it may also be imprecise. The queue checking behaviour can
      be configured by altering CFG_BIBEDIT_QUEUE_CHECK_METHOD
      in 'invenio-local.conf'.  By default, the precise technique
      is used.
    
    Co-authored-by: Tibor Simko <tibor.simko@cern.ch>

--- modules/bibedit/lib/bibedit_utils.py
+++ modules/bibedit/lib/bibedit_utils.py
@@ -48,7 +48,8 @@ from invenio.bibrecord import create_record, create_records, \
 from invenio.bibtask import task_low_level_submission
 from invenio.config import CFG_BIBEDIT_LOCKLEVEL, \
     CFG_BIBEDIT_TIMEOUT, CFG_BIBUPLOAD_EXTERNAL_OAIID_TAG as OAIID_TAG, \
-    CFG_BIBUPLOAD_EXTERNAL_SYSNO_TAG as SYSNO_TAG, CFG_TMPSHAREDDIR
+    CFG_BIBUPLOAD_EXTERNAL_SYSNO_TAG as SYSNO_TAG, CFG_TMPSHAREDDIR, \
+    CFG_BIBEDIT_QUEUE_CHECK_METHOD
 from invenio.dateutils import convert_datetext_to_dategui
 from invenio.bibedit_dblayer import get_bibupload_task_opts, \
     get_marcxml_of_record_revision, get_record_revisions, \
@@ -607,14 +608,30 @@ def _record_in_files_p(recid, filenames):
     # For each record in each file, compare ids and abort if match is found
     for filename in filenames:
         try:
-            file_ = open(filename)
-            records = create_records(file_.read(), 0, 0)
-            for i in range(0, len(records)):
-                record, all_good = records[i][:2]
-                if record and all_good:
-                    if _record_has_id_p(record, recid, rec_oaiid, rec_sysno):
+            if CFG_BIBEDIT_QUEUE_CHECK_METHOD == 'regexp':
+                # check via regexp: this is fast, but may not be precise
+                re_match_001 = re.compile('<controlfield tag="001">%s</controlfield>' % (recid))
+                re_match_oaiid = re.compile('<datafield tag="%s" ind1=" " ind2=" ">(\s*<subfield code="a">\s*|\s*<subfield code="9">\s*.*\s*</subfield>\s*<subfield code="a">\s*)%s' % (OAIID_TAG[0:3],rec_oaiid))
+                re_match_sysno = re.compile('<datafield tag="%s" ind1=" " ind2=" ">(\s*<subfield code="a">\s*|\s*<subfield code="9">\s*.*\s*</subfield>\s*<subfield code="a">\s*)%s' % (SYSNO_TAG[0:3],rec_sysno))
+                file_content = open(filename).read()
+                if re_match_001.search(file_content):
+                    return True
+                if rec_oaiid_tag:
+                    if re_match_oaiid.search(file_content):
                         return True
-            file_.close()
+                if rec_sysno_tag:
+                    if re_match_sysno.search(file_content):
+                        return True
+            else:
+                # by default, check via bibrecord: this is accurate, but may be slow
+                file_ = open(filename)
+                records = create_records(file_.read(), 0, 0)
+                for i in range(0, len(records)):
+                    record, all_good = records[i][:2]
+                    if record and all_good:
+                        if _record_has_id_p(record, recid, rec_oaiid, rec_sysno):
+                            return True
+                file_.close()
         except IOError:
             continue
     return False
