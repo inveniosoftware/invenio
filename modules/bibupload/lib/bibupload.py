inveniosoftware/invenio

commit 61ecf087c4cd3e0d8491e5be3ab2d9f28277d458

BibUpload: remove selected formats upon uploading
* Introduces new configuration variable CFG_BIBUPLOAD_DELETE_FORMATS
  that defines a list of formats to be deleted upon record ingestion
  in bibupload.  Useful to delete pre-cached HTML brief and detailed
  formats right at the time of the record metadata change; the record
  will then be formatted on-the-fly until the next bibreformat run
  updates the cache.  (closes #870)

modules/bibupload/lib/bibupload.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -44,6 +44,7 @@ from invenio.config import CFG_OAI_ID_FIELD, \
      CFG_BIBUPLOAD_STRONG_TAGS, \
      CFG_BIBUPLOAD_CONTROLLED_PROVENANCE_TAGS, \
      CFG_BIBUPLOAD_SERIALIZE_RECORD_STRUCTURE, \
+     CFG_BIBUPLOAD_DELETE_FORMATS, \
      CFG_SITE_URL, CFG_SITE_RECORD, \
      CFG_OAI_PROVENANCE_ALTERED_SUBFIELD
 
@@ -341,6 +342,13 @@ def bibupload(record, opt_tag=None, opt_mode=None,
                         msg = "   Failed: error during update_bibfmt_format 'recstruct'"
                         write_message(msg, verbose=1, stream=sys.stderr)
                         return (1, int(rec_id), msg)
+                # delete some formats like HB upon record change:
+                for format_to_delete in CFG_BIBUPLOAD_DELETE_FORMATS:
+                    try:
+                        delete_bibfmt_format(rec_id, format_to_delete, pretend=pretend)
+                    except:
+                        # OK, some formats like HB could not have been deleted, no big deal
+                        pass
                 # archive MARCXML format of this record for version history purposes:
                 error = archive_marcxml_for_history(rec_id, pretend=pretend)
                 if error == 1:
@@ -1613,6 +1621,14 @@ def update_bibfmt_format(id_bibrec, format_value, format_name, modification_date
         else:
             write_message("   -Insert the format %s in bibfmt : DONE" % format_name , verbose=2)
             return 0
+
+def delete_bibfmt_format(id_bibrec, format_name, pretend=False):
+    """
+    Delete format FORMAT_NAME from bibfmt table for record ID_BIBREC.
+    """
+    if not pretend:
+        run_sql("DELETE FROM bibfmt WHERE id_bibrec=%s and format=%s", (id_bibrec, format_name))
+    return 0
 
 def archive_marcxml_for_history(recID, pretend=False):
     """

config/invenio.conf
@@ -1,5 +1,5 @@
 ## This file is part of Invenio.
-## Copyright (C) 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2008, 2009, 2010, 2011, 2012 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -1405,6 +1405,15 @@ CFG_BIBUPLOAD_FFT_ALLOWED_EXTERNAL_URLS = [
 ## or to fill the cache for all records that have not been cached yet.
 CFG_BIBUPLOAD_SERIALIZE_RECORD_STRUCTURE = 1
 
+## CFG_BIBUPLOAD_DELETE_FORMATS -- which formats do we want bibupload
+## to delete when a record is ingested?  Enter comma-separated list of
+## formats.  For example, 'hb,hd' will delete pre-formatted HTML brief
+## and defailed formats from cache, so that search engine will
+## generate them on-the-fly.  Useful to always present latest data of
+## records upon record display, until the periodical bibreformat job
+## runs next and updates the cache.
+CFG_BIBUPLOAD_DELETE_FORMATS = hb
+
 ## CFG_BATCHUPLOADER_FILENAME_MATCHING_POLICY -- a comma-separated list
 ## indicating which fields match the file names of the documents to be
 ## uploaded.
@@ -1558,10 +1567,10 @@ CFG_BIBMATCH_QUERY_TEMPLATES = {
 ##                      a successful match will cause validation to immediately exit as a success.
 ##
 ## You can add your own rulesets in the dictionary below. The 'default' ruleset is always applied,
-## and should therefore NOT be removed, but can be changed. The tag-rules can also be overwritten 
+## and should therefore NOT be removed, but can be changed. The tag-rules can also be overwritten
 ## by other rulesets.
 ##
-## WARNING: Beware that the validation quality is only as good as given rules, so matching results 
+## WARNING: Beware that the validation quality is only as good as given rules, so matching results
 ## are never guaranteed to be accurate, as it is very content-specific.
 CFG_BIBMATCH_MATCH_VALIDATION_RULESETS = [('default', [{ 'tags' : '245__%,242__%',
                                                          'threshold' : 0.8,

modules/miscutil/lib/inveniocfg.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2008, 2009, 2010, 2011, 2012 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -153,6 +153,7 @@ def convert_conf_option(option_name, option_value):
                        'CFG_BIBSCHED_GC_TASKS_TO_ARCHIVE',
                        'CFG_BIBUPLOAD_FFT_ALLOWED_LOCAL_PATHS',
                        'CFG_BIBUPLOAD_CONTROLLED_PROVENANCE_TAGS',
+                       'CFG_BIBUPLOAD_DELETE_FORMATS',
                        'CFG_WEBSEARCH_ENABLED_SEARCH_INTERFACES',
                        'CFG_WEBSTYLE_HTTP_STATUS_ALERT_LIST',
                        'CFG_WEBSEARCH_RSS_I18N_COLLECTIONS',

commit 16963a109af09e6f2af42fc6232f71e3514b209a

BibUpload: fix FMT uploading
* Fixes problem with uploading of FMT tags introduced in
  61ecf087c4cd3e0d8491e5be3ab2d9f28277d458.  (closes #870)

modules/bibupload/lib/bibupload.py
@@ -263,9 +263,9 @@ def bibupload(record, opt_tag=None, opt_mode=None,
             return (1, -1, msg)
 
         # Have a look if we have FMT tags
+        we_have_fmt_tags_p = extract_tag_from_record(record, 'FMT') is not None
         write_message("Stage 1: Start (Insert of FMT tags if exist).", verbose=2)
-        if opt_stage_to_start_from <= 1 and \
-            extract_tag_from_record(record, 'FMT') is not None:
+        if opt_stage_to_start_from <= 1 and we_have_fmt_tags_p:
             record = insert_fmt_tags(record, rec_id, opt_mode, pretend=pretend)
             if record is None:
                 msg = "   Stage 1 failed: Error while inserting FMT tags"
@@ -342,13 +342,14 @@ def bibupload(record, opt_tag=None, opt_mode=None,
                         msg = "   Failed: error during update_bibfmt_format 'recstruct'"
                         write_message(msg, verbose=1, stream=sys.stderr)
                         return (1, int(rec_id), msg)
-                # delete some formats like HB upon record change:
-                for format_to_delete in CFG_BIBUPLOAD_DELETE_FORMATS:
-                    try:
-                        delete_bibfmt_format(rec_id, format_to_delete, pretend=pretend)
-                    except:
-                        # OK, some formats like HB could not have been deleted, no big deal
-                        pass
+                if not we_have_fmt_tags_p:
+                    # delete some formats like HB upon record change:
+                    for format_to_delete in CFG_BIBUPLOAD_DELETE_FORMATS:
+                        try:
+                            delete_bibfmt_format(rec_id, format_to_delete, pretend=pretend)
+                        except:
+                            # OK, some formats like HB could not have been deleted, no big deal
+                            pass
                 # archive MARCXML format of this record for version history purposes:
                 error = archive_marcxml_for_history(rec_id, pretend=pretend)
                 if error == 1: