7b36792cd288918ae6e287e2549bb129d34e4c99
Nikolaos Kasioumis <nikolaos.kasioumis@cern.ch>
2012-02-02 18:25:22 +0100

    htmlutils: improve remove_html_markup() function
    * Adds an optional boolean argument to the remove_html_makrup()
      function, which controls whether unescaped characters like
      '&amp;', '&lt;', '&gt;' and '&quot;' are also removed.
      (closes #895)
    
    * Adds a new function to htmlutils called unescape() as the opposite
      of the cgi.escape() function.

--- modules/miscutil/lib/htmlutils.py
+++ modules/miscutil/lib/htmlutils.py
@@ -52,6 +52,7 @@ CFG_HTML_BUFFER_ALLOWED_ATTRIBUTE_WHITELIST = ('href', 'name', 'class')
 
 ## precompile some often-used regexp for speed reasons:
 RE_HTML = re.compile("(?s)<[^>]*>|&#?\w+;")
+RE_HTML_WITHOUT_ESCAPED_CHARS = re.compile("(?s)<[^>]*>")
 
 def nmtoken_from_string(text):
     """
@@ -480,7 +481,7 @@ def get_html_text_editor(name, id=None, content='', textual_content=None, width=
 
     return editor
 
-def remove_html_markup(text, replacechar=' '):
+def remove_html_markup(text, replacechar=' ', remove_escaped_chars_p=True):
     """
     Remove HTML markup from text.
 
@@ -489,10 +490,29 @@ def remove_html_markup(text, replacechar=' '):
     @param replacechar: By which character should we replace HTML markup.
         Usually, a single space or an empty string are nice values.
     @type replacechar: string
+    @param remove_escaped_chars_p: If True, also remove escaped characters
+        like '&amp;', '&lt;', '&gt;' and '&quot;'.
+    @type remove_escaped_chars_p: boolean
     @return: Input text with HTML markup removed.
     @rtype: string
     """
+    if not remove_escaped_chars_p:
+        return RE_HTML_WITHOUT_ESCAPED_CHARS.sub(replacechar, text)
     return RE_HTML.sub(replacechar, text)
+
+def unescape(s, quote=False):
+    """
+    The opposite of the cgi.escape function.
+    Replace escaped characters '&amp;', '&lt;' and '&gt;' with the corresponding
+    regular characters. If the optional flag quote is true, the escaped quotation
+    mark character ('&quot;') is also translated.
+    """
+    s = s.replace('&lt;', '<')
+    s = s.replace('&gt;', '>')
+    if quote:
+        s = s.replace('&quot;', '"')
+    s = s.replace('&amp;', '&')
+    return s
 
 
 class EscapedString(str):
