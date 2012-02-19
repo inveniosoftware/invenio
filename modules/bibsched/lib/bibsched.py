inveniosoftware/invenio

commit 3c4e2f41da5aa274ea1ef2c77d8a2583ee5d2d13

BibSched: add support for task sequences
* Adds support for task sequences via sequence id.
  (closes #796)

modules/bibsched/lib/bibsched.py
@@ -409,7 +409,7 @@ class Manager:
         msg += ' sleeptime: %s\n\n' % self.currentrow[4]
         msg += '    status: %s\n\n' % self.currentrow[5]
         msg += '  progress: %s\n\n' % self.currentrow[6]
-        arguments = marshal.loads(self.currentrow[7])
+        arguments = marshal.loads(self.currentrow[8])
         if type(arguments) is dict:
             # FIXME: REMOVE AFTER MAJOR RELEASE 1.0
             msg += '   options : %s\n\n' % arguments
@@ -867,7 +867,7 @@ class Manager:
                     table = "hstTASK"
                     order = "runtime DESC"
                     where = ''
-                self.rows = run_sql("""SELECT id,proc,user,runtime,sleeptime,status,progress,arguments,priority,host FROM %s WHERE status NOT LIKE '%%_DELETED' %s ORDER BY %s""" % (table, where, order))
+                self.rows = run_sql("""SELECT id,proc,user,runtime,sleeptime,status,progress,arguments,priority,host,sequenceid FROM %s WHERE status NOT LIKE '%%_DELETED' %s ORDER BY %s""" % (table, where, order))
                 ring = 0
                 self.repaint()
             ring += 1
@@ -928,14 +928,14 @@ class BibSched:
         n_active = []
         if "bibupload" not in self.allowed_task_types:
             self.node_relevant_bibupload_tasks = ()
-        for id,proc,runtime,status,priority,host in self.node_relevant_waiting_tasks:
+        for id, proc, runtime, status, priority, host, sequenceid in self.node_relevant_waiting_tasks:
             procname = proc.split(':')[0]
             if procname in self.allowed_task_types:
-                n_waiting.append((id, proc, runtime, status, priority, host))
-        for id,proc,runtime,status,priority,host in self.node_relevant_active_tasks:
+                n_waiting.append((id, proc, runtime, status, priority, host, sequenceid))
+        for id, proc, runtime, status, priority, host, sequenceid in self.node_relevant_active_tasks:
             procname = proc.split(':')[0]
             if procname in self.allowed_task_types:
-                n_active.append((id, proc, runtime, status, priority, host))
+                n_active.append((id, proc, runtime, status, priority, host, sequenceid))
         self.node_relevant_active_tasks = tuple(n_active)
         self.node_relevant_waiting_tasks = tuple(n_waiting)
 
@@ -980,23 +980,24 @@ class BibSched:
         higher = []
         lower = []
         ### !!! We allready have this in node_relevant_active_tasks
-        for other_task_id, task_proc, runtime, status, task_priority, task_host in self.node_relevant_active_tasks:
+        for other_task_id, task_proc, runtime, status, task_priority, task_host, sequenceid in self.node_relevant_active_tasks:
+#        for other_task_id, task_proc, runtime, status, task_priority, task_host in self.node_relevant_active_tasks:
 #        for other_task_id, task_proc, task_priority, status in self.get_running_tasks():
             if task_id == other_task_id:
                 continue
             if task_priority < priority and task_host == self.hostname:
-                lower.append((other_task_id, task_proc, task_priority, status))
+                lower.append((other_task_id, task_proc, task_priority, status, sequenceid))
             elif task_host == self.hostname:
-                higher.append((other_task_id, task_proc, task_priority, status))
+                higher.append((other_task_id, task_proc, task_priority, status, sequenceid))
         return lower, higher
 
-    def handle_task(self, task_id, proc, runtime, status, priority, host):
+    def handle_task(self, task_id, proc, runtime, status, priority, host, sequenceid):
         """Perform needed action of the row representing a task.
         Return True when task_status need to be refreshed"""
         #Log('%s id: %s, proc: %s, runtime: %s, status: %s, priority: %s' % (task_status, task_id, proc, runtime, status, priority))
         #Log("task_id: %s, proc: %s, runtime: %s, status: %s, priority: %s" % (task_id, proc, runtime, status, priority))
 
-        if (task_id, proc, runtime, status, priority, host) in self.node_relevant_waiting_tasks:
+        if (task_id, proc, runtime, status, priority, host, sequenceid) in self.node_relevant_waiting_tasks:
 #        elif task_id in self.task_status['WAITING'] or task_id in self.task_status['SLEEPING']:
             #Log("Trying to run %s" % task_id)
 
@@ -1006,7 +1007,7 @@ class BibSched:
             lower, higher = self.split_active_tasks_by_priority(task_id, priority)
             #Log('lower: %s' % lower)
             #Log('higher: %s' % higher)
-            for other_task_id, other_proc, other_runtime, other_status, other_priority, other_host  in self.active_tasks_all_nodes:
+            for other_task_id, other_proc, other_runtime, other_status, other_priority, other_host, other_sequenceid in self.active_tasks_all_nodes:
                 if not self.is_task_safe_to_execute(proc, other_proc):
                     ### !!! WE NEED TO CHECK FOR TASKS THAT CAN ONLY BE EXECUTED ON ONE MACHINE AT ONE TIME
                     ### !!! FOR EXAMPLE BIBUPLOADS WHICH NEED TO BE EXECUTED SEQUENTIALLY AND NEVER CONCURRENTLY
@@ -1014,6 +1015,12 @@ class BibSched:
                     ## cannot run at the same time of the given task.
                     ## We give up
                     #Log("Cannot run because task_id: %s, proc: %s is the queue and incompatible" % (other_task_id, other_proc))
+                    return False
+
+            for other_task_id, other_proc, other_dummy, other_status, other_sequenceid in higher + lower:
+                if sequenceid == other_sequenceid and task_id > other_task_id:
+                    Log('Same sequence id processes.')
+                    ## If there is a task with same sequence number then do not run the current task
                     return False
 
             ## No higher priority task have issue with the given task.
@@ -1100,10 +1107,10 @@ class BibSched:
             if max_bibupload_priority:
                 run_sql("UPDATE schTASK SET priority=%s WHERE status='WAITING' AND proc='bibupload' AND runtime<=NOW()", ( max_bibupload_priority[0][0], ))
             ## The bibupload tasks are sorted by id, which means by the order they were scheduled
-            self.node_relevant_bibupload_tasks = run_sql("SELECT id,proc,runtime,status,priority,host FROM schTASK WHERE status='WAITING' AND proc='bibupload' AND runtime<=NOW() ORDER BY id ASC LIMIT 1", n=1)
+            self.node_relevant_bibupload_tasks = run_sql("SELECT id,proc,runtime,status,priority,host,sequenceid FROM schTASK WHERE status='WAITING' AND proc='bibupload' AND runtime<=NOW() ORDER BY id ASC LIMIT 1", n=1)
             ## The other tasks are sorted by priority
-            self.node_relevant_waiting_tasks = run_sql("SELECT id,proc,runtime,status,priority,host FROM schTASK WHERE (status='WAITING' AND runtime<=NOW()) OR status='SLEEPING' ORDER BY priority DESC, runtime ASC, id ASC")
-            self.node_relevant_active_tasks = run_sql("SELECT id,proc,runtime,status,priority,host FROM schTASK WHERE status IN ('RUNNING','CONTINUING','SCHEDULED','ABOUT TO STOP','ABOUT TO SLEEP')")
+            self.node_relevant_waiting_tasks = run_sql("SELECT id,proc,runtime,status,priority,host,sequenceid FROM schTASK WHERE (status='WAITING' AND runtime<=NOW()) OR status='SLEEPING' ORDER BY priority DESC, runtime ASC, id ASC")
+            self.node_relevant_active_tasks = run_sql("SELECT id,proc,runtime,status,priority,host,sequenceid FROM schTASK WHERE status IN ('RUNNING','CONTINUING','SCHEDULED','ABOUT TO STOP','ABOUT TO SLEEP')")
             self.active_tasks_all_nodes = tuple(self.node_relevant_active_tasks)
             ## Remove tasks that can not be executed on this host
             self.filter_for_allowed_tasks()

Makefile.am
@@ -535,6 +535,7 @@ update-v1.0.0-rc0-tables: # from v1.0.0-rc0 to next release
 	echo "UPDATE sbmALLFUNCDESCR SET function='Move_CKEditor_Files_to_Storage', description='Transfer files attached to the record with the CKEditor' WHERE function='Move_FCKeditor_Files_to_Storage';" | ${prefix}/bin/dbexec
 	echo "UPDATE sbmFUNCTIONS SET function='Move_CKEditor_Files_to_Storage' WHERE function='Move_FCKeditor_Files_to_Storage';" | ${prefix}/bin/dbexec
 	echo "ALTER TABLE schTASK CHANGE proc proc varchar(255) NOT NULL;" | ${prefix}/bin/dbexec
+	echo "ALTER TABLE schTASK ADD sequenceid int(8) NULL default NULL;" | ${prefix}/bin/dbexec
 	echo "ALTER TABLE session CHANGE session_object session_object longblob;" | ${prefix}/bin/dbexec
 	echo "ALTER TABLE session CHANGE session_expiry session_expiry datetime NOT NULL default '0000-00-00 00:00:00';" | ${prefix}/bin/dbexec
 	echo "ALTER TABLE oaiREPOSITORY CHANGE setSpec setSpec varchar(255) NOT NULL default 'GLOBAL_SET';" | ${prefix}/bin/dbexec

modules/bibsched/lib/bibtask.py
@@ -87,6 +87,7 @@ _TASK_PARAMS = {
         'priority': 0,
         'runtime_limit': None,
         'profile': [],
+        'sequence-id':None,
         }
 
 # Global _OPTIONS dictionary.
@@ -280,6 +281,7 @@ def task_init(
         "runtime_limit" : None,
         "profile" : [],
         "post-process": [],
+        "sequence-id": None,
     }
     to_be_submitted = True
     if len(sys.argv) == 2 and sys.argv[1].isdigit():
@@ -409,7 +411,7 @@ def _task_build_params(
     # set user-defined options:
     try:
         (short_params, long_params) = specific_params
-        opts, args = getopt.gnu_getopt(argv[1:], "hVv:u:s:t:P:N:L:" +
+        opts, args = getopt.gnu_getopt(argv[1:], "hVv:u:s:t:P:N:L:I:" +
             short_params, [
                 "help",
                 "version",
@@ -422,6 +424,7 @@ def _task_build_params(
                 "limit=",
                 "profile=",
                 "post-process="
+                "sequence-id="
             ] + long_params)
     except getopt.GetoptError, err:
         _usage(1, err, help_specific_usage=help_specific_usage, description=description)
@@ -452,6 +455,8 @@ def _task_build_params(
                 _TASK_PARAMS["profile"] += opt[1].split(',')
             elif opt[0] in ("--post-process", ):
                 _TASK_PARAMS["post-process"] += [opt[1]];
+            elif opt[0] in ("-S","--sequence-id"):
+                _TASK_PARAMS["sequence-id"] = opt[1]
             elif not callable(task_submit_elaborate_specific_parameter_fnc) or \
                 not task_submit_elaborate_specific_parameter_fnc(opt[0],
                     opt[1], opts, args):
@@ -680,10 +685,10 @@ def _task_submit(argv, authorization_action, authorization_msg):
     write_message("storing task options %s\n" % argv, verbose=9)
     verbose_argv = 'Will execute: %s' % ' '.join([escape_shell_arg(str(arg)) for arg in argv])
     _TASK_PARAMS['task_id'] = run_sql("""INSERT INTO schTASK (proc,user,
-                                           runtime,sleeptime,status,progress,arguments,priority)
-                                         VALUES (%s,%s,%s,%s,'WAITING',%s,%s, %s)""",
+                                           runtime,sleeptime,status,progress,arguments,priority,sequenceid)
+                                         VALUES (%s,%s,%s,%s,'WAITING',%s,%s,%s,%s)""",
         (task_name, _TASK_PARAMS['user'], _TASK_PARAMS["runtime"],
-         _TASK_PARAMS["sleeptime"], verbose_argv, marshal.dumps(argv), _TASK_PARAMS['priority']))
+         _TASK_PARAMS["sleeptime"], verbose_argv, marshal.dumps(argv), _TASK_PARAMS['priority'], _TASK_PARAMS['sequence-id']))
 
     ## update task number:
     write_message("Task #%d submitted." % _TASK_PARAMS['task_id'])
@@ -842,6 +847,7 @@ def _usage(exitcode=1, msg="", help_specific_usage="", description=""):
     sys.stderr.write("  -s, --sleeptime=SLEEP\tSleeping frequency after"
         " which to repeat the task.\n"
         "\t\t\tExamples: 30m, 2h, 1d. [default=no]\n")
+    sys.stderr.write("  -I, --sequence-id=SEQUENCE-ID\tSequence Id of the current process\n")
     sys.stderr.write("  -L  --limit=LIMIT\tTime limit when it is"
         " allowed to execute the task.\n"
         "\t\t\tExamples: 22:00-03:00, Sunday 01:00-05:00.\n"

modules/miscutil/sql/tabcreate.sql
@@ -3517,6 +3517,7 @@ CREATE TABLE IF NOT EXISTS schTASK (
   status varchar(50),
   progress varchar(255),
   priority tinyint(4) NOT NULL default 0,
+  sequenceid int(8) NULL default NULL,
   PRIMARY KEY  (id),
   KEY status (status),
   KEY runtime (runtime),

commit e76ae9990fe43638b8e0ad9e5cbfd7e6dda2c047

BibSched: fix task option display in monitor
* Fixes bug with task option display in bibsched monitor (key O)
  that was introduced in 3c4e2f41da5aa274ea1ef2c77d8a2583ee5d2d13.
  (addresses #796)

modules/bibsched/lib/bibsched.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -409,7 +409,7 @@ class Manager:
         msg += ' sleeptime: %s\n\n' % self.currentrow[4]
         msg += '    status: %s\n\n' % self.currentrow[5]
         msg += '  progress: %s\n\n' % self.currentrow[6]
-        arguments = marshal.loads(self.currentrow[8])
+        arguments = marshal.loads(self.currentrow[7])
         if type(arguments) is dict:
             # FIXME: REMOVE AFTER MAJOR RELEASE 1.0
             msg += '   options : %s\n\n' % arguments