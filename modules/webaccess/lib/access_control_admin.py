261a97c57c7a38d88ba317e0b57e19b4c44cd5ec
Nikolaos Kasioumis <nikolaos.kasioumis@cern.ch>
2012-07-03 20:07:11 +0200

    WebAccess: use DB slave for more selected queries
    * Adds the run_on_slave=True argument to frequently used SQL SELECT queries
      on tables 'accACTION', 'accARGUMENT', 'accMAILCOOKIE, 'accROLE' and
      'accROLE_accACTION_accARGUMENT'.

--- modules/webaccess/lib/access_control_admin.py
+++ modules/webaccess/lib/access_control_admin.py
@@ -249,10 +249,10 @@ def acc_is_role(name_action, **arguments):
 
     # first check if an action exists with this name
     id_action = acc_get_action_id(name_action)
-    arole = run_sql("SELECT id_accROLE FROM accROLE_accACTION_accARGUMENT WHERE id_accACTION=%s AND argumentlistid <= 0 LIMIT 1", (id_action, ), 1)
+    arole = run_sql("SELECT id_accROLE FROM accROLE_accACTION_accARGUMENT WHERE id_accACTION=%s AND argumentlistid <= 0 LIMIT 1", (id_action, ), 1, run_on_slave=True)
     if arole:
         return True
-    other_roles_to_check = run_sql("SELECT id_accROLE, keyword, value, argumentlistid FROM  accROLE_accACTION_accARGUMENT JOIN accARGUMENT ON id_accARGUMENT=id WHERE id_accACTION=%s AND argumentlistid > 0", (id_action, ))
+    other_roles_to_check = run_sql("SELECT id_accROLE, keyword, value, argumentlistid FROM  accROLE_accACTION_accARGUMENT JOIN accARGUMENT ON id_accARGUMENT=id WHERE id_accACTION=%s AND argumentlistid > 0", (id_action, ), run_on_slave=True)
     other_roles_to_check_dict = {}
     for id_accROLE, keyword, value, argumentlistid in other_roles_to_check:
         try:
@@ -1229,10 +1229,10 @@ def acc_get_user_roles_from_user_info(user_info):
         roles = intbitset(run_sql("""SELECT ur.id_accROLE
             FROM user_accROLE ur
             WHERE ur.id_user = %s AND ur.expiration >= NOW()
-            ORDER BY ur.id_accROLE""", (uid, )))
+            ORDER BY ur.id_accROLE""", (uid, ), run_on_slave=True))
 
     potential_implicit_roles = run_sql("""SELECT id, firerole_def_ser FROM accROLE
-        WHERE firerole_def_ser IS NOT NULL""")
+        WHERE firerole_def_ser IS NOT NULL""", run_on_slave=True)
 
     for role_id, firerole_def_ser in potential_implicit_roles:
         if role_id not in roles:
@@ -1294,7 +1294,7 @@ def acc_find_user_role_actions(user_info):
                    ur.id_accROLE = raa.id_accROLE AND
                    raa.id_accACTION = a.id AND
                    raa.id_accROLE = r.id """
-        res1 = run_sql(query, (uid, ))
+        res1 = run_sql(query, (uid, ), run_on_slave=True)
 
     res2 = []
     for res in res1:
@@ -1307,7 +1307,7 @@ def acc_find_user_role_actions(user_info):
             WHERE raa.id_accACTION = a.id AND
             raa.id_accROLE = r.id """
 
-        res3 = run_sql(query)
+        res3 = run_sql(query, run_on_slave=True)
         res4 = []
         for role_name, action_name, role_definition in res3:
             if acc_firerole_check_user(user_info,
