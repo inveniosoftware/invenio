134bedbc309ed586a75c3089b03179c8159e61a2
Jiri Kuncar <jiri.kuncar@cern.ch>
2012-10-16 18:17:33 +0200

    WebSearch: Python 2.6 dict problem fix

--- modules/websearch/lib/websearch_model.py
+++ modules/websearch/lib/websearch_model.py
@@ -302,8 +302,8 @@ class Collection(db.Model):
         """
         default = {'':g._('any field')}
         found = dict((o.field.code, o.field.name_ln) for o in self._search_within)
-        default.update(found or {(f.name.replace(' ',''), f.name_ln) for f in Field.query.filter(
-                    Field.name.in_(CFG_WEBSEARCH_SEARCH_WITHIN)).all()})
+        default.update(found or dict((f.name.replace(' ',''), f.name_ln) for f in Field.query.filter(
+                    Field.name.in_(CFG_WEBSEARCH_SEARCH_WITHIN)).all()))
         return default
 
     @property
