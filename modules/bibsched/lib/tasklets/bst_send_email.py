2b1571bfa80ce667d96a33ac8bd41434519ecd9b
Jan Aage Lavik <jan.age.lavik@cern.ch>
2012-10-30 17:27:02 +0100

    mailutils: extend send_email with BCC option
    * Adds new parameter bccaddr to send_email() that allows clients
      to specify addresses to be put in BCC.
    
      This acts as a workaround for the silent behaviour of mailutils
      that multiple recipients in the To field are put in BCC instead.
    
      This means that you can now specify one receiver in To: and the
      rest in BCC for cases where one would want to do this.
    
    * Also updates bst_send_mail with the new API behaviour.
    
    * Does some code de-duplication efforts and pep8 updates.

--- modules/bibsched/lib/tasklets/bst_send_email.py
+++ modules/bibsched/lib/tasklets/bst_send_email.py
@@ -23,17 +23,19 @@ Invenio Bibliographic Tasklet send_email wrapper.
 
 from invenio.mailutils import send_email
 
+
 def bst_send_email(fromaddr,
-               toaddr,
-               subject="",
-               content="",
-               header=None,
-               footer=None,
-               copy_to_admin=0,
-               attempt_times=1,
-               attempt_sleeptime=10,
-               replytoaddr="",
-               ):
+                   toaddr,
+                   subject="",
+                   content="",
+                   header=None,
+                   footer=None,
+                   copy_to_admin=0,
+                   attempt_times=1,
+                   attempt_sleeptime=10,
+                   replytoaddr="",
+                   bccaddr="",
+                  ):
     """
     Send a forged email to TOADDR from FROMADDR with message created from subjet, content and possibly
     header and footer.
@@ -57,6 +59,8 @@ def bst_send_email(fromaddr,
     @type attempt_sleeptime: int
     @param replytoaddr: comma-separated list of emails to add as reply-to header
     @type replytoaddr: string
+    @param bccaddr: comma-separated list of emails to add as bcc header
+    @type bccaddr: string
 
     If sending fails, try to send it ATTEMPT_TIMES, and wait for
     ATTEMPT_SLEEPTIME seconds in between tries.
@@ -64,5 +68,15 @@ def bst_send_email(fromaddr,
     copy_to_admin = int(copy_to_admin)
     attempt_times = int(attempt_times)
     attempt_sleeptime = int(attempt_sleeptime)
-    return send_email(fromaddr=fromaddr, toaddr=toaddr, subject=subject, content=content, header=header, footer=footer, copy_to_admin=copy_to_admin, attempt_times=attempt_times, attempt_sleeptime=attempt_sleeptime, replytoaddr=replytoaddr)
-
+    return send_email(fromaddr=fromaddr,
+                      toaddr=toaddr,
+                      subject=subject,
+                      content=content,
+                      header=header,
+                      footer=footer,
+                      copy_to_admin=copy_to_admin,
+                      attempt_times=attempt_times,
+                      attempt_sleeptime=attempt_sleeptime,
+                      replytoaddr=replytoaddr,
+                      bccaddr=bccaddr,
+                     )
