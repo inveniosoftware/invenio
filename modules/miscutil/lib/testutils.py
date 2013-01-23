dbf592bd0c5836a50aadcd8985339377d4da74f6
Tibor Simko <tibor.simko@cern.ch>
2013-01-23 17:07:15 +0100

    testutils: fix for importing CFG_DATABASE values

--- modules/miscutil/lib/testutils.py
+++ modules/miscutil/lib/testutils.py
@@ -1,5 +1,5 @@
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
+## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -113,6 +113,13 @@ def make_test_suite(*test_cases):
                                for case in test_cases])
 
 from invenio.config import *
+from invenio.dbquery import CFG_DATABASE_HOST, \
+    CFG_DATABASE_PORT, \
+    CFG_DATABASE_NAME, \
+    CFG_DATABASE_USER, \
+    CFG_DATABASE_PASS, \
+    CFG_DATABASE_TYPE, \
+    CFG_DATABASE_SLAVE
 from invenio.webinterface_handler_flask import create_invenio_flask_app
 from invenio.sqlalchemyutils import db
 from flask.ext.testing import TestCase, Twill
