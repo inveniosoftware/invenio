a4fda317ee2b0663a33a6b865ddae7751cf60aff
Konstantinos Ntemagkos <konstantinos.ntemagkos@cern.ch>
2013-07-15 19:00:37 +0200

    WebAPIKey: initial Flask port
    * Moves `web_api_key` logic to the model.
    
    * Refactors functions to work with sqlalchemy and flask request.
    
    * Refactors `web_api_key` to work with model functions.
    
    * Updates `web_api_key_regression_tests`.
    
    Signed-off-by: Konstantinos Ntemagkos <konstantinos.ntemagkos@cern.ch>
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- modules/miscutil/lib/web_api_key.py
+++ modules/miscutil/lib/web_api_key.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2010, 2011 CERN.
+## Copyright (C) 2006, 2007, 2008, 2010, 2011, 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -18,28 +18,10 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-    Invenio utilities to perform a REST like authentication
+Invenio utilities to perform a REST like authentication.
 """
-import hmac
-import time
-from cgi import parse_qsl
-from urllib import urlencode
-import re
-
-try:
-    from uuid import uuid4
-except ImportError:
-    import random
-    def uuid4():
-        return "%x" % random.getrandbits(16*8)
-
-from invenio.dbquery import run_sql, IntegrityError
-from invenio.config import CFG_WEB_API_KEY_ALLOWED_URL
 from invenio.access_control_config import CFG_WEB_API_KEY_STATUS
-from invenio.hashutils import sha1
-
-_CFG_WEB_API_KEY_ALLOWED_URL = [(re.compile(_url), _authorized_time, _need_timestamp)
-        for _url, _authorized_time, _need_timestamp in CFG_WEB_API_KEY_ALLOWED_URL]
+from invenio.web_api_key_model import WebAPIKey
 
 
 def create_new_web_api_key(uid, key_description=None):
@@ -52,15 +34,8 @@ def create_new_web_api_key(uid, key_description=None):
     @param key_description: User's description for the REST API key
     @type key_description: string
     """
-    key_id = str(uuid4())
-    key_secrect = str(uuid4())
-    while True:
-        try:
-            run_sql("INSERT INTO webapikey (id,secret,id_user,description) VALUES(%s,%s,%s,%s)",
-                    (key_id, key_secrect, uid, key_description))
-            break
-        except IntegrityError:
-            key_id = str(uuid4())
+    WebAPIKey.create_new(uid, key_description)
+
 
 def show_web_api_keys(uid, diff_status=CFG_WEB_API_KEY_STATUS['REMOVED']):
     """
@@ -73,12 +48,9 @@ def show_web_api_keys(uid, diff_status=CFG_WEB_API_KEY_STATUS['REMOVED']):
     admin part)
     @type diff_statusparam: string
 
-    @return: Tuples with the id, description and status of the user's REST API
-    keys
     """
-    keys_info = run_sql("SELECT id, description, status FROM webapikey WHERE id_user = %s AND status <> %s",
-                        (uid, diff_status))
-    return keys_info
+    return WebAPIKey.show_keys(uid, diff_status)
+
 
 def mark_web_api_key_as_removed(key_id):
     """
@@ -89,7 +61,8 @@ def mark_web_api_key_as_removed(key_id):
     @param key_id: The id of the REST key that will be "removed"
     @type key_id: string
     """
-    run_sql("UPDATE webapikey SET status=%s WHERE id=%s", (CFG_WEB_API_KEY_STATUS['REMOVED'], key_id, ))
+    WebAPIKey.mark_as(key_id, CFG_WEB_API_KEY_STATUS['REMOVED'])
+
 
 def get_available_web_api_keys(uid):
     """
@@ -99,84 +72,32 @@ def get_available_web_api_keys(uid):
     @param uid: The user id
     @type uid: int
 
-    @return: Tuples of REST API public keys
+    @return: WebAPIKey objects
     """
-    keys = run_sql("SELECT id FROM webapikey WHERE id_user=%s AND status <> %s AND status <> %s",
-                  (uid, CFG_WEB_API_KEY_STATUS['REMOVED'], CFG_WEB_API_KEY_STATUS['REVOKED']))
-    return keys
+    return WebAPIKey.get_available(uid)
 
-def acc_get_uid_from_request(path, args):
+
+def acc_get_uid_from_request():
     """
     Looks in the data base for the secret that matches with the API key in the
     request. If the REST API key is found and if the signature is correct
     returns the user's id.
 
-    @param path: uri of the request until the "?" (i.e.: req.uri)
-    @type path: string
-    @param args: All the params of the request (i.e.: req.args)
-    @type args: string
-
     @return: If everything goes well it returns the user's uid, it not -1
     """
-    from invenio.webstat import register_customevent
 
-    params = parse_qsl(args)
-    api_key = signature = timestamp = None
+    return WebAPIKey.acc_get_uid_from_request()
 
-    for param in params:
-        if param[0] == 'apikey':
-            api_key = param[1]
-        elif param[0] == 'signature':
-            signature = param[1]
-            params.remove(param) #Get rid of the signature
-        elif param [0] == 'timestamp':
-            timestamp = param[1]
-    #Check if the url is well built
-    if api_key == None or signature == None:
-        return -1
 
-    url_req = "%s?%s" % (path, urlencode(params))
-
-    authorized_time = None
-    need_timestamp = False
-    for url, authorized_time, need_timestamp in _CFG_WEB_API_KEY_ALLOWED_URL:
-        if url.match(url_req) is not None:
-            break
-
-    if need_timestamp and timestamp == None:
-        return -1
-
-    if authorized_time is None:
-        return -1
-
-    if authorized_time != 0 and need_timestamp:
-        time_lapse = time.time() - float(timestamp)
-        if time_lapse > authorized_time or time_lapse < 0:
-            return -1
-
-    key = run_sql("SELECT id_user, secret FROM webapikey WHERE id=%s AND status <> %s AND status <> %s",
-                  (api_key, CFG_WEB_API_KEY_STATUS['REMOVED'], CFG_WEB_API_KEY_STATUS['REVOKED']))
-    if len(key) == 0 or not key:
-        return -1
-    else:
-        uid = key[0][0]
-        secret_key = key[0][1]
-        server_signature = hmac.new(secret_key, url_req, sha1).hexdigest()
-        if signature == server_signature:
-            #If the signature is fine, log the key activity and return the UID
-            register_customevent("apikeyusage", [uid, api_key, path, url_req])
-            return uid
-        else:
-            return -1
-
-def build_web_request(path, params, uid=-1, api_key=None, timestamp=True):
+def build_web_request(path, params=None, uid=-1, api_key=None, timestamp=True):
     """
     Build a new request that uses REST authentication.
     1. Add your REST API key to the params
     2. Add the current timestamp to the params, if needed
     3. Sort the query string params
     4. Merge path and the sorted query string to a single string
-    5. Create a HMAC-SHA1 signature of this string using your secret key as the key
+    5. Create a HMAC-SHA1 signature of this string
+       using your secret key as the key
     6. Append the hex-encoded signature to your query string
 
     @note: If the api_key parameter is None, then this method performs a search
@@ -198,37 +119,5 @@ def build_web_request(path, params, uid=-1, api_key=None, timestamp=True):
 
     @return: Signed request string or, in case of error, ''
     """
-    if not isinstance(params, dict):
-        if len(params) != 0 and params[0] == '?':
-            params = params.replace('?','')
-        params = parse_qsl(params)
 
-    items = (hasattr(params, 'items') and [params.items()] or [list(params)])[0]
-
-    if api_key:
-        items.append(('apikey', api_key))
-    elif uid > 0:
-        keys = run_sql("SELECT id FROM webapikey WHERE id_user=%s AND status <> %s AND status <> %s",
-                  (uid, CFG_WEB_API_KEY_STATUS['REMOVED'], CFG_WEB_API_KEY_STATUS['REVOKED']))
-        if keys is not None and len(keys) != 0:
-            api_key = keys[0][0]
-            items.append(('apikey', api_key))
-        else:
-            return ''
-    else:
-        return ''
-
-    if timestamp:
-        items.append(('timestamp', str(int(time.time()))))
-
-    items = sorted(items, key=lambda x: x[0].lower())
-    url = '%s?%s' % (path, urlencode(items))
-
-    secret_key = run_sql("SELECT secret FROM webapikey WHERE id=%s", (api_key,))
-    if len(secret_key) == 0 or not secret_key:
-        return ''
-    signature = hmac.new(secret_key[0][0], url, sha1).hexdigest()
-    items.append(('signature', signature))
-    if not items:
-        return path
-    return '%s?%s' % (path, urlencode(items))
+    return WebAPIKey.build_web_request(path, params, uid, api_key, timestamp)
--- modules/miscutil/lib/web_api_key_regression_tests.py
+++ modules/miscutil/lib/web_api_key_regression_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2010, 2011 CERN.
+## Copyright (C) 2006, 2007, 2008, 2010, 2011, 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,29 +16,22 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-from invenio import web_api_key
 
 """Unit tests for REST like authentication API."""
 
-try:
-    import hashlib
-except:
-    pass
-import unittest
 import re
 import hmac
 import urllib
 import time
-import string
+import sys
 
-from invenio.testutils import make_test_suite, run_test_suite
-from invenio.dbquery import run_sql
+from flask import current_app
 
-web_api_key.CFG_WEB_API_KEY_ALLOWED_URL = [('/search\?*', 0, True),
-                                        ('/bad\?*', -1, True)] #Just for testing
+from invenio.access_control_config import CFG_WEB_API_KEY_STATUS
+from invenio.hashutils import sha1
+from invenio.testutils import InvenioTestCase, make_test_suite, \
+    run_test_suite
 
-web_api_key._CFG_WEB_API_KEY_ALLOWED_URL = [(re.compile(_url), _authorized_time, _need_timestamp)
-        for _url, _authorized_time, _need_timestamp in web_api_key.CFG_WEB_API_KEY_ALLOWED_URL]
 
 def build_web_request(path, params, api_key=None, secret_key=None):
     items = (hasattr(params, 'items') and [params.items()] or [list(params)])[0]
@@ -48,73 +41,97 @@ def build_web_request(path, params, api_key=None, secret_key=None):
         items.append(('timestamp', str(int(time.time()))))
         items = sorted(items, key=lambda x: x[0].lower())
         url = '%s?%s' % (path, urllib.urlencode(items))
-        signature = hmac.new(secret_key, url, hashlib.sha1).hexdigest()
+        signature = hmac.new(secret_key, url, sha1).hexdigest()
         items.append(('signature', signature))
     if not items:
         return path
     return '%s?%s' % (path, urllib.urlencode(items))
 
-class APIKeyTest(unittest.TestCase):
+
+class APIKeyTest(InvenioTestCase):
     """ Test functions related to the REST authentication API """
     def setUp(self):
-        self.id_admin = run_sql('SELECT id FROM user WHERE nickname="admin"')[0][0]
+        from invenio import web_api_key
+        from invenio.websession_model import User
+        from invenio.web_api_key_model import WebAPIKey
+        self.model = WebAPIKey
+        self.web_api_key = web_api_key
+        self.web_api_key.CFG_WEB_API_KEY_ALLOWED_URL = [('/search\?*', 0, True),
+                                                        ('/bad\?*', -1, True)]  # Just for testing
+
+        self.web_api_key._CFG_WEB_API_KEY_ALLOWED_URL = \
+            [(re.compile(_url), _authorized_time,
+             _need_timestamp) for _url, _authorized_time, _need_timestamp in
+             self.web_api_key.CFG_WEB_API_KEY_ALLOWED_URL]
+        self.model.query.delete()
+        self.id_admin = User.query.filter_by(nickname='admin').one().id
+
+    def tearDown(self):
+        self.model.query.delete()
 
     def test_create_remove_show_key(self):
         """apikey - create/list/delete REST key"""
-        self.assertEqual(0, len(web_api_key.show_web_api_keys(uid=self.id_admin)))
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key I")
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key II")
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key III")
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key IV")
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key V")
-        self.assertEqual(5, len(web_api_key.show_web_api_keys(uid=self.id_admin)))
-        self.assertEqual(5, len(web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
-        keys_info = web_api_key.show_web_api_keys(uid=self.id_admin)
-        web_api_key.mark_web_api_key_as_removed(keys_info[0][0])
-        self.assertEqual(4, len(web_api_key.show_web_api_keys(uid=self.id_admin)))
-        self.assertEqual(5, len(web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
+        self.assertEqual(0, len(self.web_api_key.show_web_api_keys(uid=self.id_admin)))
 
-        run_sql("UPDATE webapikey SET status='WARNING' WHERE id=%s", (keys_info[1][0],))
-        run_sql("UPDATE webapikey SET status='REVOKED' WHERE id=%s", (keys_info[2][0],))
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key I")
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key II")
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key III")
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key IV")
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key V")
 
-        self.assertEqual(4, len(web_api_key.show_web_api_keys(uid=self.id_admin)))
-        self.assertEqual(5, len(web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
+        self.assertEqual(5, len(self.web_api_key.show_web_api_keys(uid=self.id_admin)))
+        self.assertEqual(5, len(self.web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
 
-        run_sql("DELETE FROM webapikey")
+        keys_info = self.web_api_key.show_web_api_keys(uid=self.id_admin)
+        self.web_api_key.mark_web_api_key_as_removed(keys_info[0].id)
+
+        self.assertEqual(4, len(self.web_api_key.show_web_api_keys(uid=self.id_admin)))
+        self.assertEqual(5, len(self.web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
+
+        self.model.mark_as(keys_info[1].id, CFG_WEB_API_KEY_STATUS['WARNING'])
+        self.model.mark_as(keys_info[2].id, CFG_WEB_API_KEY_STATUS['REVOKED'])
+
+        self.assertEqual(4, len(self.web_api_key.show_web_api_keys(uid=self.id_admin)))
+        self.assertEqual(5, len(self.web_api_key.show_web_api_keys(uid=self.id_admin, diff_status='')))
 
     def test_acc_get_uid_from_request(self):
         """webapikey - Login user from request using REST key"""
         path = '/search'
         params = 'ln=es&sc=1&c=Articles & Preprints&action_search=Buscar&p=ellis'
 
-        self.assertEqual(0, len(web_api_key.show_web_api_keys(uid=self.id_admin)))
-        web_api_key.create_new_web_api_key(self.id_admin, "Test key I")
+        self.assertEqual(0, len(self.web_api_key.show_web_api_keys(uid=self.id_admin)))
+        self.web_api_key.create_new_web_api_key(self.id_admin, "Test key I")
 
-        key_info = run_sql("SELECT id FROM webapikey WHERE id_user=%s", (self.id_admin,))
-        url = web_api_key.build_web_request(path, params, api_key=key_info[0][0])
-        url = string.split(url, '?')
-        uid = web_api_key.acc_get_uid_from_request(url[0], url[1])
+        key_info = self.model.query.filter_by(id_user=self.id_admin).all()
+        url = self.web_api_key.build_web_request(path, params,
+                                                 api_key=key_info[0].id)
+        with current_app.test_client() as client:
+            client.get(url, follow_redirects=True)
+            uid = self.web_api_key.acc_get_uid_from_request()
         self.assertEqual(uid, self.id_admin)
 
-        url = web_api_key.build_web_request(path, params, api_key=key_info[0][0])
-        url += "123" # corrupt the key
-        url = string.split(url, '?')
-        uid = web_api_key.acc_get_uid_from_request(url[0], url[1])
+        url = self.web_api_key.build_web_request(path, params, api_key=key_info[0].id)
+        url += "123"  # corrupt the key
+        with current_app.test_client() as client:
+            client.get(url, follow_redirects=True)
+            uid = self.web_api_key.acc_get_uid_from_request()
         self.assertEqual(uid, -1)
 
         path = '/bad'
-        uid = web_api_key.acc_get_uid_from_request(path, "")
-        self.assertEqual(uid, -1)
-        params = {'nocache': 'yes', 'limit': 123}
-        url = web_api_key.build_web_request(path, params, api_key=key_info[0][0])
-        url = string.split(url, '?')
-        uid = web_api_key.acc_get_uid_from_request(url[0], url[1])
+        self.client.get(path, follow_redirects=True)
+        uid = self.web_api_key.acc_get_uid_from_request()
         self.assertEqual(uid, -1)
 
-        run_sql("DELETE FROM webapikey")
+        params = {'nocache': 'yes', 'limit': 123}
+        with current_app.test_client() as client:
+            url = self.web_api_key.build_web_request(path, params,
+                                                     api_key=key_info[0].id)
+            client.get(path, follow_redirects=True)
+        uid = self.web_api_key.acc_get_uid_from_request()
+        self.assertEqual(uid, -1)
+
 
 TEST_SUITE = make_test_suite(APIKeyTest)
 
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE)
-    run_sql("DELETE FROM webapikey")
