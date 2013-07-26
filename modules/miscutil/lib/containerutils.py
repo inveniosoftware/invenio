8734adf47f0bd140ee44ef31be21cdd2d5933558
Patrick Glauner <patrick.oliver.glauner@cern.ch>
2013-07-26 20:53:10 +0200

    containerutils: new Python-2.4 defaultdict
    * Allows to use defaultdict with Python 2.4 as it comes
      with 2.5 and higher.
    
    Signed-off-by: Patrick Glauner <patrick.oliver.glauner@cern.ch>
    Tested-by: Tibor Simko <tibor.simko@cern.ch>

--- modules/miscutil/lib/containerutils.py
+++ modules/miscutil/lib/containerutils.py
@@ -50,3 +50,49 @@ def get_substructure(data, path):
         return get_substructure(data[path[0]], path[1:])
     except (TypeError, IndexError, KeyError):
         return None
+
+try:
+    from collections import defaultdict
+except:
+    class defaultdict(dict):
+        """
+        Python 2.4 backport of defaultdict
+        GPL licensed code taken from NLTK - http://code.google.com/p/nltk/
+        collections.defaultdict
+        originally contributed by Yoav Goldberg <yoav.goldberg@gmail.com>
+        new version by Jason Kirtland from Python cookbook.
+        <http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/523034>
+        """
+        def __init__(self, default_factory=None, *a, **kw):
+            if (default_factory is not None and
+                 not hasattr(default_factory, '__call__')):
+                raise TypeError('first argument must be callable')
+            dict.__init__(self, *a, **kw)
+            self.default_factory = default_factory
+        def __getitem__(self, key):
+            try:
+                return dict.__getitem__(self, key)
+            except KeyError:
+                return self.__missing__(key)
+        def __missing__(self, key):
+            if self.default_factory is None:
+                raise KeyError(key)
+            self[key] = value = self.default_factory()
+            return value
+        def __reduce__(self):
+            if self.default_factory is None:
+                args = tuple()
+            else:
+                args = self.default_factory,
+            return type(self), args, None, None, self.items()
+        def copy(self):
+            return self.__copy__()
+        def __copy__(self):
+            return type(self)(self.default_factory, self)
+        def __deepcopy__(self, memo):
+            import copy
+            return type(self)(self.default_factory,
+                              copy.deepcopy(self.items()))
+        def __repr__(self):
+            return 'defaultdict(%s, %s)' % (self.default_factory,
+                                             dict.__repr__(self))
