17ffc71e4f5e1a872a2e92ee9cf1f4c7e3b9dbda
Jiri Kuncar <jiri.kuncar@cern.ch>
2013-09-24 15:41:07 +0200

    WebSearch: summarizer regression tests fix
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- modules/websearch/lib/search_engine_summarizer_regression_tests.py
+++ modules/websearch/lib/search_engine_summarizer_regression_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2011, 2012 CERN.
+## Copyright (C) 2011, 2012, 2013 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -19,13 +19,11 @@
 
 """Regression tests for the search engine summarizer."""
 
-import unittest
-
-from invenio.testutils import make_test_suite, run_test_suite
+from invenio.testutils import make_test_suite, run_test_suite, InvenioTestCase
 from invenio.intbitset import intbitset
 
 
-class WebSearchSummarizerTests(unittest.TestCase):
+class WebSearchSummarizerTests(InvenioTestCase):
     """Test utility functions for search engine summarizer."""
 
     def test_basic(self):
