5dc963f8556bcbd0ec847a830eb9be8c1000513a
Alessio Deiana <alessio.deiana@cern.ch>
2013-10-04 14:31:09 +0200

    BibSched: invalid sql in monitor history tab
    * The monitor history tab would fail with invalid sql syntax
      because of an extra WHERE in the query.

--- modules/bibsched/lib/bibsched_monitor.py
+++ modules/bibsched/lib/bibsched_monitor.py
@@ -1027,12 +1027,12 @@ order to let this task run. The current priority is %s. New value:"
             selected_id = selected_row[0]
         if self.display == 1:
             table = "schTASK"
-            where = "status IN ('DONE', 'ACK DONE', 'ACK DONE WITH ERRORS', 'ACK ERROR', 'ACK ERRORS REPORTED')"
+            where = "WHERE status IN ('DONE', 'ACK DONE', 'ACK DONE WITH ERRORS', 'ACK ERROR', 'ACK ERRORS REPORTED')"
             order = "runtime DESC"
             limit = "LIMIT %s" % CFG_BIBSCHED_MAX_ARCHIVED_ROWS_DISPLAY
         elif self.display == 2:
             table = "schTASK"
-            where = "status IN ('RUNNING', 'CONTINUING', 'SCHEDULED', 'ABOUT TO STOP', 'ABOUT TO SLEEP', 'SLEEPING', 'WAITING', 'ERRORS REPORTED', 'DONE WITH ERRORS', 'ERROR', 'CERROR', 'KILLED', 'STOPPED')"
+            where = "WHERE status IN ('RUNNING', 'CONTINUING', 'SCHEDULED', 'ABOUT TO STOP', 'ABOUT TO SLEEP', 'SLEEPING', 'WAITING', 'ERRORS REPORTED', 'DONE WITH ERRORS', 'ERROR', 'CERROR', 'KILLED', 'STOPPED')"
             order = "runtime ASC"
             limit = ""
         else:
@@ -1044,7 +1044,7 @@ order to let this task run. The current priority is %s. New value:"
                                status, progress, arguments, priority, host,
                                sequenceid
                                FROM %s
-                               WHERE %s
+                               %s
                                ORDER BY %s
                                %s""" % (table, where, order, limit))
 
