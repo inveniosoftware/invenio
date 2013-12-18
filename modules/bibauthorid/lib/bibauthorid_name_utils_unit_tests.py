kaplun/ops

BibAuthorID: improvements and bug fixes
* Fixed broken links for claiming multiple papers
* Improved the context of the email sent when an rt ticket is created
* The deamon which precomputes caches is no longer taking account empty authors
* Fixed menu bar to work even when an author does not have a canonical name
* Fixed backend name utilities: splitting and capitalization
* Fixed internal publication list on author page
* Fixed merge profiles link handling
* Fixed person search engine issues
* Fixed profiles merging algorithm

modules/bibauthorid/lib/bibauthorid_name_utils_unit_tests.py
@@ -79,7 +79,9 @@ class Test_split_name_parts(unittest.TestCase):
          'test, mix initials morder': ['Test',['M', 'I', 'M'],['Mix', 'Initials', 'Morder'],[0, 1, 2]],
          'test, s. s. b.': ['Test', ['S', 'S', 'B'], [], []],
          'test, s. still be': ['Test', ['S', 'S', 'B'], ['Still', 'Be'], [1, 2]],
-         'with .[+)* ] just but without comma test': ['Test',['W', '[', '*', ']', 'J', 'B', 'W', 'C'],['With', '[+', 'Just', 'But', 'Without', 'Comma'],[0, 1, 4, 5, 6, 7]]
+         'with .[+)* ] just but without comma test': ['Test',['W', '[', '*', ']', 'J', 'B', 'W', 'C'],['With', '[+', 'Just', 'But', 'Without', 'Comma'],[0, 1, 4, 5, 6, 7]],
+         'test-dash': ['Test-Dash', [], [], []],
+         'test-dash,': ['Test-Dash', [], [], []]
          }
 
     def test_split_name_parss(self):
@@ -181,6 +183,9 @@ class Test_soft_name_comparison(unittest.TestCase):
         'Diff, Con Nome': ['Erent, Con Nome', [(ge, 0.0), (le, 0.4)]],
         'Diff, Con Nomee': ['Erent, Che Noun', [(ge, 0.0), (le, 0.2)]],
         'Diff, Name': ['Erent, Completely', [(ge, 0.0), (le, 0.1)]],
+        'Test-dash': ['Test-dash', [(ge, 0.5), (le, 1.)]],
+        'Test, noname': ['Test,', [(ge, 0.5), (le, 1.)]],
+        'Test, nonamo': ['Test', [(ge, 0.5), (le, 1.)]],
         }
 
     def test_value_ranges(self):

inveniosoftware/invenio

modules/bibauthorid/lib/bibauthorid_name_utils_unit_tests.py
@@ -79,7 +79,9 @@ class Test_split_name_parts(unittest.TestCase):
          'test, mix initials morder': ['Test',['M', 'I', 'M'],['Mix', 'Initials', 'Morder'],[0, 1, 2]],
          'test, s. s. b.': ['Test', ['S', 'S', 'B'], [], []],
          'test, s. still be': ['Test', ['S', 'S', 'B'], ['Still', 'Be'], [1, 2]],
-         'with .[+)* ] just but without comma test': ['Test',['W', '[', '*', ']', 'J', 'B', 'W', 'C'],['With', '[+', 'Just', 'But', 'Without', 'Comma'],[0, 1, 4, 5, 6, 7]]
+         'with .[+)* ] just but without comma test': ['Test',['W', '[', '*', ']', 'J', 'B', 'W', 'C'],['With', '[+', 'Just', 'But', 'Without', 'Comma'],[0, 1, 4, 5, 6, 7]],
+         'test-dash': ['Test-Dash', [], [], []],
+         'test-dash,': ['Test-Dash', [], [], []]
          }
 
     def test_split_name_parss(self):
@@ -181,6 +183,9 @@ class Test_soft_name_comparison(unittest.TestCase):
         'Diff, Con Nome': ['Erent, Con Nome', [(ge, 0.0), (le, 0.4)]],
         'Diff, Con Nomee': ['Erent, Che Noun', [(ge, 0.0), (le, 0.2)]],
         'Diff, Name': ['Erent, Completely', [(ge, 0.0), (le, 0.1)]],
+        'Test-dash': ['Test-dash', [(ge, 0.5), (le, 1.)]],
+        'Test, noname': ['Test,', [(ge, 0.5), (le, 1.)]],
+        'Test, noname': ['Test', [(ge, 0.5), (le, 1.)]],
         }
 
     def test_value_ranges(self):