kaplun/ops

WebSession: user preference to enable profiling
* Adds a user preference that is available if you have 'profiling' permissions.
  With this preference, you can enable permanently enable profiling for your
  user. Useful for debugging random slowness.

modules/websession/lib/webaccount.py
@@ -275,7 +275,8 @@ def perform_delete(ln):
     # TODO
     return websession_templates.tmpl_account_delete(ln = ln)
 
-def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
+def perform_set(email, ln, can_config_bibcatalog=False,
+                can_config_profiling=False, verbose=0):
     """Perform_set(email,password): edit your account parameters, email and
     password.
     If can_config_bibcatalog is True, show the bibcatalog dialog (if configured).
@@ -285,13 +286,13 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
         res = run_sql("SELECT id, nickname FROM user WHERE email=%s", (email,))
         uid = res[0][0]
         nickname = res[0][1]
-    except:
+    except IndexError:
         uid = 0
         nickname = ""
 
     CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS_LOCAL = CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS
     prefs = get_user_preferences(uid)
-    if CFG_EXTERNAL_AUTHENTICATION.has_key(prefs['login_method']) and CFG_EXTERNAL_AUTHENTICATION[prefs['login_method']] is not None:
+    if prefs['login_method'] in CFG_EXTERNAL_AUTHENTICATION and CFG_EXTERNAL_AUTHENTICATION[prefs['login_method']] is not None:
         CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS_LOCAL = 3
 
     out = websession_templates.tmpl_user_preferences(
@@ -305,7 +306,7 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
         try:
             uid = run_sql("SELECT id FROM user where email=%s", (email,))
             uid = uid[0][0]
-        except:
+        except IndexError:
             uid = 0
         current_login_method = prefs['login_method']
         methods = CFG_EXTERNAL_AUTHENTICATION.keys()
@@ -358,8 +359,12 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
     if CFG_BIBCATALOG_SYSTEM and can_config_bibcatalog:
         bibcatalog_username = prefs.get('bibcatalog_username', "")
         bibcatalog_password = prefs.get('bibcatalog_password', "")
-        out += websession_templates.tmpl_user_bibcatalog_auth(bibcatalog_username, \
+        out += websession_templates.tmpl_user_bibcatalog_auth(bibcatalog_username,
                                                           bibcatalog_password, ln=ln)
+
+    if can_config_profiling:
+        out += websession_templates.tmpl_user_profiliing_settings(ln=ln,
+                                                                  enable_profiling=prefs.get('enable_profiling'))
 
     if verbose >= 9:
         for key, value in prefs.items():

modules/websession/lib/websession_templates.py
@@ -343,7 +343,7 @@ class Template:
                 <tr>
                   <td><input class="formbutton" type="submit" value="%(update_settings)s" /></td>
                 </tr>
-              </table>
+              </table></form>
         """ % {
           'sitesecureurl' : CFG_SITE_SECURE_URL,
           'bibcatalog_username' : bibcatalog_username,
@@ -377,6 +377,32 @@ class Template:
             <tr><td></td><td><input class="formbutton" type="submit" value="%(update_settings)s" /></td></tr>
         </table></form>""" % {
             'select_lang' : _('Select desired language of the web interface.'),
+            'update_settings' : _('Update settings')
+        }
+        return out
+
+    def tmpl_user_profiliing_settings(self, ln, enable_profiling):
+        _ = gettext_set_language(ln)
+        out = """
+            <form method="post" action="%(sitesecureurl)s/youraccount/change" name="edit_profiling_settings">
+              <p><big><strong class="headline">%(edit_settings)s</strong></big></p>
+              <table>
+                <tr><td align="right"><select name="profiling">
+        """ % {
+          'sitesecureurl' : CFG_SITE_SECURE_URL,
+          'edit_settings' : _("Edit profiling settings"),
+        }
+        out += """<option %(selected)s value="0">%(desc)s</option>""" % {
+            'selected' : 'selected="selected"' if enable_profiling is False else '',
+            'desc' : _("Disabled")
+        }
+        out += """<option %(selected)s value="1">%(desc)s</option>""" % {
+            'selected' : 'selected="selected"' if enable_profiling is True else '',
+            'desc' : _("Enabled")
+        }
+        out += """</select></td><td valign="top"></td></tr>
+            <tr><td></td><td><input class="formbutton" type="submit" value="%(update_settings)s" /></td></tr>
+        </table></form>""" % {
             'update_settings' : _('Update settings')
         }
         return out

inveniosoftware/invenio

Signed-off-by: Alessio Deiana <alessio.deiana@cern.ch>
Tested-by: Tibor Simko <tibor.simko@cern.ch>

modules/websession/lib/webaccount.py
@@ -1,5 +1,5 @@
 ## This file is part of Invenio.
-## Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
+## Copyright (C) 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -275,7 +275,8 @@ def perform_delete(ln):
     # TODO
     return websession_templates.tmpl_account_delete(ln = ln)
 
-def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
+def perform_set(email, ln, can_config_bibcatalog=False,
+                can_config_profiling=False, verbose=0):
     """Perform_set(email,password): edit your account parameters, email and
     password.
     If can_config_bibcatalog is True, show the bibcatalog dialog (if configured).
@@ -285,13 +286,13 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
         res = run_sql("SELECT id, nickname FROM user WHERE email=%s", (email,))
         uid = res[0][0]
         nickname = res[0][1]
-    except:
+    except IndexError:
         uid = 0
         nickname = ""
 
     CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS_LOCAL = CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS
     prefs = get_user_preferences(uid)
-    if CFG_EXTERNAL_AUTHENTICATION.has_key(prefs['login_method']) and CFG_EXTERNAL_AUTHENTICATION[prefs['login_method']] is not None:
+    if prefs['login_method'] in CFG_EXTERNAL_AUTHENTICATION and CFG_EXTERNAL_AUTHENTICATION[prefs['login_method']] is not None:
         CFG_ACCESS_CONTROL_LEVEL_ACCOUNTS_LOCAL = 3
 
     out = websession_templates.tmpl_user_preferences(
@@ -305,7 +306,7 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
         try:
             uid = run_sql("SELECT id FROM user where email=%s", (email,))
             uid = uid[0][0]
-        except:
+        except IndexError:
             uid = 0
         current_login_method = prefs['login_method']
         methods = CFG_EXTERNAL_AUTHENTICATION.keys()
@@ -358,8 +359,12 @@ def perform_set(email, ln, can_config_bibcatalog = False, verbose = 0):
     if CFG_BIBCATALOG_SYSTEM and can_config_bibcatalog:
         bibcatalog_username = prefs.get('bibcatalog_username', "")
         bibcatalog_password = prefs.get('bibcatalog_password', "")
-        out += websession_templates.tmpl_user_bibcatalog_auth(bibcatalog_username, \
+        out += websession_templates.tmpl_user_bibcatalog_auth(bibcatalog_username,
                                                           bibcatalog_password, ln=ln)
+
+    if can_config_profiling:
+        out += websession_templates.tmpl_user_profiling_settings(ln=ln,
+                                                                 enable_profiling=prefs.get('enable_profiling'))
 
     if verbose >= 9:
         for key, value in prefs.items():

modules/websession/lib/websession_templates.py
@@ -1,5 +1,5 @@
 ## This file is part of Invenio.
-## Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
+## Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -343,7 +343,7 @@ class Template:
                 <tr>
                   <td><input class="formbutton" type="submit" value="%(update_settings)s" /></td>
                 </tr>
-              </table>
+              </table></form>
         """ % {
           'sitesecureurl' : CFG_SITE_SECURE_URL,
           'bibcatalog_username' : bibcatalog_username,
@@ -377,6 +377,32 @@ class Template:
             <tr><td></td><td><input class="formbutton" type="submit" value="%(update_settings)s" /></td></tr>
         </table></form>""" % {
             'select_lang' : _('Select desired language of the web interface.'),
+            'update_settings' : _('Update settings')
+        }
+        return out
+
+    def tmpl_user_profiling_settings(self, ln, enable_profiling):
+        _ = gettext_set_language(ln)
+        out = """
+            <form method="post" action="%(sitesecureurl)s/youraccount/change" name="edit_profiling_settings">
+              <p><big><strong class="headline">%(edit_settings)s</strong></big></p>
+              <table>
+                <tr><td align="right"><select name="profiling">
+        """ % {
+          'sitesecureurl' : CFG_SITE_SECURE_URL,
+          'edit_settings' : _("Edit profiling settings"),
+        }
+        out += """<option %(selected)s value="0">%(desc)s</option>""" % {
+            'selected' : 'selected="selected"' if enable_profiling is False else '',
+            'desc' : _("Disabled")
+        }
+        out += """<option %(selected)s value="1">%(desc)s</option>""" % {
+            'selected' : 'selected="selected"' if enable_profiling is True else '',
+            'desc' : _("Enabled")
+        }
+        out += """</select></td><td valign="top"></td></tr>
+            <tr><td></td><td><input class="formbutton" type="submit" value="%(update_settings)s" /></td></tr>
+        </table></form>""" % {
             'update_settings' : _('Update settings')
         }
         return out