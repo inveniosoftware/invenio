c81ea53716d8f61ce585b1ae36679e72a9e7da3c
Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
2014-01-30 16:30:54 +0100

    comments: fix for JavaScript in record tab
    * Moves modal binding to separate function in invenio.js;
    * Adds macros for injecting CSS and JS.
    
    Signed-off-by: Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
    
    NOTE: beware, patch amended by Tibor to use new file names

--- invenio/base/static/js/invenio.js
+++ invenio/base/static/js/invenio.js
@@ -162,21 +162,24 @@
     });
   });
 
-  $(document).ready(function() {
-      // Support for AJAX loaded modal window.
-      // Focuses on first input textbox after it loads the window.
-      $('[data-toggle="modal"]').click(function(e) {
-          e.preventDefault();
-          var href = $(this).attr('href');
-          if (href.indexOf('#') === 0) {
-              $(href).modal('open');
-          } else {
-              $.get(href, function(data) {
-                  $('<div class="modal" >' + data + '</div>').modal();
-              }).success(function() { $('input:text:visible:first').focus(); });
-          }
-      });
-  });
+  function bindModals() {
+    // Support for AJAX loaded modal window.
+    // Focuses on first input textbox after it loads the window.
+    $('[data-toggle="modal"]').click(function(e) {
+        e.preventDefault();
+        var href = $(this).attr('href');
+        if (href.indexOf('#') === 0) {
+            $(href).modal('open');
+        } else {
+            $.get(href, function(data) {
+                $('<div class="modal" >' + data + '</div>').modal();
+            }).success(function() { $('input:text:visible:first').focus(); });
+        }
+    });
+  }
+
+  $(document).ready(bindModals);
+  window.bindModals = bindModals;
 
   $(document).on('hidden.bs.modal', function() {
       // delete any existing modal elements instead of just hiding them
