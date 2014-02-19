3419682b0d2c25d542bad0674e91d126a8ff878d
Georgios Kokosioulis <giokokos@gmail.com>
2014-02-19 17:56:37 +0100

    global: grunt improvemnts and bootstrap upgrade
    * Runs tasks in parallel.
    
    * Copies files that actually changed.
    
    * Adds licence headers and path option for custom path.
    
    * Upgrades bootstrap 3.1.1, jquery 2.1.0.
    
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- grunt/concurrent.js
+++ grunt/concurrent.js
@@ -17,31 +17,7 @@
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
  */
 
-'use strict';
-
-/*jshint laxcomma:true */  // FIXME?
-
-/* global module */
-/* global require */
-
-module.exports = function (grunt) {
-    var globalConfig = {
-        bower_path: 'bower_components',
-        installation_path: '../../var/invenio.base-instance/static' // default path
-    };
-
-    // target for custom path
-    if (grunt.option('path')) {
-        globalConfig.installation_path = grunt.option('target', grunt.option('path'))
-    }
-
-    // show elapsed time at the end
-    require('time-grunt')(grunt);
-
-    // load all grunt tasks
-    require('load-grunt-config')(grunt, {
-        config: {
-            globalConfig: globalConfig
-        }
-    });
+module.exports = {
+    first: ['newer:copy'],
+    second: ['newer:uglify']
 };
---
+++ grunt/copy.js
@@ -0,0 +1,236 @@
+/*
+ * This file is part of Invenio.
+ * Copyright (C) 2014 CERN.
+ *
+ * Invenio is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Invenio is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Invenio; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+module.exports = {
+    css: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>',
+        src: ['bootstrap/dist/css/bootstrap*'
+               ,'jquery-tokeninput/styles/token-input-facebook.css'
+               ,'jquery-tokeninput/styles/token-input.css'
+               ,'jquery.bookmark/jquery.bookmark.css'
+               ,'datatables-colvis/media/css/ColVis.css'
+               ,'DataTables-Plugins/integration/bootstrap/3/dataTables.bootstrap.css'],
+        dest: '<%= globalConfig.installation_path %>/css/'
+    },
+    img: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['jquery.bookmark/bookmarks.png'
+               ,'uploadify/uploadify*'
+               ,'!uploadify/uploadify.php'
+               ,'datatables-colvis/media/images/button.png'
+               ,'DataTables-Plugins/integration/bootstrap/3/images/*.png'],
+        dest: '<%= globalConfig.installation_path %>/img/'
+    },
+    js: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['bootstrap/dist/js/bootstrap.js'
+             ,'bootstrap/dist/js/bootstrap.min.js'
+             ,'jquery/dist/jquery.min.js'
+             ,'jquery/dist/jquery.min.map'
+             ,'jquery-tokeninput/src/jquery.tokeninput.js'
+             ,'jquery.bookmark/jquery.bookmark.min.js'
+             ,'DataTables/media/js/jquery.dataTables.js'
+             ,'jquery-flot/excanvas.min.js'
+             ,'jquery-flot/jquery.flot.js'
+             ,'jquery-flot/jquery.flot.selection.js'
+             ,'jquery.hotkeys/jquery.hotkeys.js'
+             ,'uploadify/jquery.uploadify.min.js'
+             ,'json2/json2.js'
+             ,'datatables-colvis/media/js/ColVis.js'
+             ,'DataTables-Plugins/integration/bootstrap/3/dataTables.bootstrap.js'],
+        dest: '<%= globalConfig.installation_path %>/js/'
+    },
+    fonts: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['bootstrap/dist/fonts/glyphicons-halflings-regular.*'],
+        dest: '<%= globalConfig.installation_path %>/fonts/'
+    },
+    typeahead: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['typeahead.js/dist/typeahead.js'],
+        dest: '<%= globalConfig.installation_path %>/js/'
+    },
+    typeaheadJSbootstrap: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['typeahead.js-bootstrap/index.css'],
+        dest: '<%= globalConfig.installation_path %>/css/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0),
+                                  'typeahead.js-bootstrap.css');
+            return dest + res;
+        }
+    },
+    hogan: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['hogan/web/builds/2.0.0/hogan-2.0.0.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            return dest + src.substring(0, src.indexOf('-')) + '.js';
+        }
+    },
+    jqueryUI: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['jquery.ui/jquery-1.8.2.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            return dest + src.substring(0, src.indexOf('-')) + '-ui.js';
+        }
+    },
+    jqueryTimePicker: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['jquery.ui.timepicker/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0),
+                                  'jquery-ui-timepicker-addon.js');
+            return dest + res;
+        }
+    },
+    MultiFile: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['jquery.multifile/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0),
+                                  'jquery.MultiFile.pack.js');
+            return dest + res;
+        }
+    },
+    ajaxPager: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['jquery.ajaxpager/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0),
+                                  'jquery.ajaxPager.js');
+            return dest + res;
+        }
+    },
+    form: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['form/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0),
+                                  'jquery.form.js');
+            return dest + res;
+        }
+    },
+    prism: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['prism/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0), 'prism.js');
+            return dest + res;
+        }
+    },
+    swfobject: {
+        expand: true,
+        flatten: true,
+        cwd: '<%= globalConfig.bower_path %>/',
+        src: ['swfobject/index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            var res = src.replace(src.substring(0), 'swfobject.js');
+            return dest + res;
+        }
+    },
+    MathJax: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/MathJax/',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/MathJax/'
+    },
+    ckeditor: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/ckeditor/',
+        src: ['**', '!**_samples/**', '!**_source/**', '!**php**',
+              '!**_**', '!**pack**', '!**ckeditor.asp'],
+        dest: '<%= globalConfig.installation_path %>/ckeditor/'
+    },
+    jqueryTreeview: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/jquery.treeview/',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/js/jquery-treeview/'
+    },
+    jqueryTableSorter: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/jquery.tablesorter/',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/js/tablesorter/'
+    },
+    themesUI: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/jquery.ui/themes/',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/img/jquery-ui/themes'
+    },
+    imagesUI: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/jquery.ui/themes/base/images/',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/img/images/'
+    },
+    jeditable: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/jquery_jeditable/',
+        src: ['index.js'],
+        dest: '<%= globalConfig.installation_path %>/js/',
+        rename: function(dest, src) {
+            if (src === 'index.js') {
+                return dest + 'jquery.jeditable.mini.js';
+            }
+            return dest + src;
+        }
+    },
+    plupload: {
+        expand: true,
+        cwd: '<%= globalConfig.bower_path %>/plupload/js',
+        src: ['**'],
+        dest: '<%= globalConfig.installation_path %>/plupload/'
+    }
+};
