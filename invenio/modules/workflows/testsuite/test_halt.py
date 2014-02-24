870752d3c03becfa8116925dee6ddab774b4b755
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-02-24 11:08:44 +0100

    workflows: engine branch halt testcase
    * Adds test case for halting workflow execution in workflows
      with branches.
    
    * Adds temporary fix to be superseeded by larger refactoring
      of workflows.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

---
+++ invenio/modules/workflows/testsuite/test_halt.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2013 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Unit tests for workflows views."""
+
+from __future__ import absolute_import
+
+from invenio.testsuite import InvenioTestCase, make_test_suite, \
+    run_test_suite
+import logging
+
+
+class WorkflowTestBranch(InvenioTestCase):
+    def test_halt(self):
+        from invenio.modules.workflows.loader import workflows
+        from invenio.modules.workflows.api import start
+        from invenio.modules.workflows.config import CFG_OBJECT_VERSION, \
+            CFG_WORKFLOW_STATUS
+        from invenio.modules.workflows.models import BibWorkflowObjectLog
+
+        halt_engine = lambda obj, eng: eng.halt("Test")
+
+        class HaltTest(object):
+            workflow = [halt_engine]
+
+        workflows['halttest'] = HaltTest
+
+        data = [{'somekey', 'somevalue'}, ]
+        eng = start('halttest', data)
+        idx, obj = list(eng.getObjects())[0]
+
+        assert obj.version == CFG_OBJECT_VERSION.HALTED
+        assert eng.status == CFG_WORKFLOW_STATUS.FINISHED
+        assert BibWorkflowObjectLog.get(
+            id_object=obj.id, log_type=logging.ERROR).count() == 0
+
+    def test_halt_in_branch(self):
+        from workflow.patterns import IF_ELSE
+        from invenio.modules.workflows.loader import workflows
+        from invenio.modules.workflows.api import start
+        from invenio.modules.workflows.config import CFG_OBJECT_VERSION, \
+            CFG_WORKFLOW_STATUS
+        from invenio.modules.workflows.models import BibWorkflowObjectLog
+
+        always_true = lambda obj, eng: True
+        halt_engine = lambda obj, eng: eng.halt("Test")
+
+        class BranchTest(object):
+            workflow = [
+                IF_ELSE(always_true, [halt_engine], [halt_engine])
+            ]
+
+        workflows['branchtest'] = BranchTest
+
+        data = [{'somekey', 'somevalue'}, ]
+        eng = start('branchtest', data)
+        idx, obj = list(eng.getObjects())[0]
+
+        assert obj.version == CFG_OBJECT_VERSION.HALTED
+        assert eng.status == CFG_WORKFLOW_STATUS.FINISHED
+        assert BibWorkflowObjectLog.get(
+            id_object=obj.id, log_type=logging.ERROR).count() == 0
+
+
+TEST_SUITE = make_test_suite(WorkflowTestBranch)
+
+if __name__ == "__main__":
+    run_test_suite(TEST_SUITE)
