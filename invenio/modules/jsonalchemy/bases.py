6edb13c171a30e6728452b1a01cf37b3a7c43ea0
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-03-07 11:58:35 +0100

    jsonalchemy: versionable extension
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Co-authored-by: Esteban J. G. Gabancho <esteban.gabancho@cern.ch>

---
+++ invenio/modules/jsonalchemy/bases.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+    invenio.models.jsonalchemy.bases
+    --------------------------------
+
+    General model extensions.
+"""
+
+
+class Versionable(object):
+    """
+    Versionable behavior for JSONAlchemy models.
+    """
+
+    def update(self):
+        """
+        Creates new revision of the object and saves link to the old one.
+        """
+        self['old_version'] = self['_id']
+        self.set_default_value('_id')  # pylint: disable=E1101
+        return super(Versionable, self).update()
