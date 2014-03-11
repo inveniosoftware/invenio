96e0247410d8623997d3a5b92cf90e29deb0e6ad
Guillaume Lastecoueres <PX9e@gmx.fr>
2014-03-11 15:10:49 +0100

    workflows: update generic_harvesting_workflow
    * Updates the generic_harvesting_workflow.
    
    Signed-off-by: Guillaume Lastecoueres <guillaume.lastecoueres@cern.ch>

--- invenio/modules/workflows/workflows/generic_harvesting_workflow.py
+++ invenio/modules/workflows/workflows/generic_harvesting_workflow.py
@@ -23,45 +23,56 @@ from ..tasks.marcxml_tasks import (get_repositories_list,
                                    harvest_records,
                                    get_files_list,
                                    get_eng_uuid_harvested,
-                                   get_records_from_file
-                                   )
+                                   get_records_from_file,
+                                   get_obj_extra_data_key, filtering_oai_pmh_identifier, update_last_update)
 
 from ..tasks.workflows_tasks import (start_workflow,
                                      wait_for_workflows_to_complete,
                                      workflows_reviews,
-                                     get_nb_workflow_created
-                                     )
+                                     get_nb_workflow_created,
+                                     num_workflow_running_greater, wait_for_a_workflow_to_complete)
 
 from ..tasks.logic_tasks import (foreach,
-                                 end_for
-                                 )
-
-from ..tasks.bibsched_tasks import write_something_bibsched
-
-from invenio.base.config import CFG_TMPSHAREDDIR
+                                 end_for,
+                                 workflow_if, workflow_else, simple_for)
 
 
 class generic_harvesting_workflow(object):
+
     object_type = "harvest"
     workflow = [
         init_harvesting,
         foreach(get_repositories_list(['arxivb']), "repository"),
         [
             harvest_records,
-            foreach(get_files_list(CFG_TMPSHAREDDIR, get_eng_uuid_harvested)),
+            foreach(get_obj_extra_data_key("harvested_files_list")),
             [
                 foreach(get_records_from_file()),
                 [
-                    start_workflow("full_doc_process", None),
-                    write_something_bibsched(["Workflow started : ",
-                                              get_nb_workflow_created, " "]),
+                    workflow_if(filtering_oai_pmh_identifier),
+                    [
+                        workflow_if(num_workflow_running_greater(10),
+                                    neg=True),
+                        [
+                            start_workflow("full_doc_process", None),
+                        ],
+                        workflow_else,
+                        [
+                            wait_for_a_workflow_to_complete,
+                            start_workflow("full_doc_process", None),
+                        ],
+                    ],
                 ],
                 end_for
             ],
             end_for
         ],
         end_for,
-        wait_for_workflows_to_complete,
-        write_something_bibsched("the end"),
-        workflows_reviews()
+        simple_for(0, get_nb_workflow_created, 1),
+        [
+            wait_for_a_workflow_to_complete,
+        ],
+        end_for,
+        workflows_reviews(stop_if_error=True),
+        update_last_update(get_repositories_list(['arxivb']))
     ]
