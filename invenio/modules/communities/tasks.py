d3866bf447c5fc47da85f18c02fe8f5e05d8e00c
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-03-12 18:06:48 +0100

    communities: ranking fix
    * Fixes an issue with the community ranking in the case no records
      have been accepted.
    
    * Fixes some PEP8 errors.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/communities/tasks.py
+++ invenio/modules/communities/tasks.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+#
 ## This file is part of Invenio.
-## Copyright (C) 2013, 2014 CERN.
+## Copyright (C) 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -20,24 +22,23 @@ from __future__ import absolute_import
 from datetime import datetime
 from invenio.ext.sqlalchemy import db
 from invenio.modules.search.models import Collection
-from invenio.celery import celery
 from celery.task.base import PeriodicTask
 
 from .models import Community
 from invenio.base.globals import cfg
 
 
-
 class RankingTask(PeriodicTask):
+
     """
         Task which will be autodiscovered and run periodically
         if celery worker is up and running (with --beat option).
     """
-    run_every = cfg['COMMUNITIES_PERIODIC_TASKS']['ranking_deamon']['run_every']
+    run_every = cfg['COMMUNITIES_PERIODIC_TASKS'][
+        'ranking_deamon']['run_every']
 
     def run(*args, **kwargs):
         ranking_deamon()
-
 
 
 def calculate_rank_for_community(community, max_recs_cnt):
@@ -54,23 +55,25 @@ def calculate_rank_for_community(community, max_recs_cnt):
            C*recs_cnt + max_date_ranking - inteval*day_value + fixed_points
            where:
            max_date_ranking = C2*max_recs_cnt
-           day_value = max_date_ranking/some_value (some_value example: 50 [days])
+           day_value = max_date_ranking/some_value (some_value example:
+           50 [days])
            C, C2 const.
     """
     factor = 5
     period_of_expiration = 50
 
     recs_cnt = community.collection.nbrecs
-    if recs_cnt > 0.3*max_recs_cnt:
-        max_date_ranking = 2*max_recs_cnt
-        day_value = max(max_date_ranking/period_of_expiration, 1)
+    if recs_cnt > 0.3 * max_recs_cnt:
+        max_date_ranking = 2 * max_recs_cnt
+        day_value = max(max_date_ranking / period_of_expiration, 1)
     else:
         max_date_ranking = max_recs_cnt
-        day_value = max(max_date_ranking/period_of_expiration, 1)
+        day_value = max(max_date_ranking / period_of_expiration, 1)
 
-    interval = (datetime.now() - community.last_record_accepted).days
-    date_ranking = max(max_date_ranking - interval*day_value, 0)
-    ranking = factor*recs_cnt + date_ranking + community.fixed_points
+    last_record_accepted = community.last_record_accepted or community.created
+    interval = (datetime.now() - last_record_accepted).days
+    date_ranking = max(max_date_ranking - interval * day_value, 0)
+    ranking = factor * recs_cnt + date_ranking + community.fixed_points
     return ranking
 
 
@@ -80,7 +83,8 @@ def ranking_deamon():
         Task is being run periodically.
     """
     comms = Community.query.all()
-    coll = Collection.query.filter(Collection.community).order_by(db.desc(Collection.nbrecs)).first()
+    coll = Collection.query.filter(Collection.community).order_by(
+        db.desc(Collection.nbrecs)).first()
     if coll:
         max_recs_cnt = coll.nbrecs
     for comm in comms:
