7886094faa68022944427166fb0bacd8297f07e9
Alessio Deiana <alessio.deiana@cern.ch>
2014-03-28 10:02:15 +0100

    BibAuthorID: affiliations bookkeeping
    * Adds a step in rabbit to keep a list of mappings pid -> affiliations.
    
    Signed-off-by: Alessio Deiana <alessio.deiana@cern.ch>
    Signed-off-by: Samuele Carli <samuele.carli@cern.ch>

--- modules/miscutil/sql/tabcreate.sql
+++ modules/miscutil/sql/tabcreate.sql
@@ -4721,6 +4721,14 @@ CREATE TABLE IF NOT EXISTS `aidINVERTEDLISTS` (
  PRIMARY KEY (`qgram`)
 ) ENGINE=MyISAM;
 
+CREATE TABLE IF NOT EXISTS `aidAFFILIATIONS` (
+  `personid` BIGINT( 16 ) UNSIGNED NOT NULL ,
+  `affiliation` VARCHAR( 256 ) NOT NULL,
+  `last_recid` MEDIUMINT( 8 ) UNSIGNED NOT NULL,
+  `last_occurence` datetime NOT NULL,
+ PRIMARY KEY (`personid`)
+) ENGINE=MyISAM;
+
 -- refextract tables:
 
 CREATE TABLE IF NOT EXISTS `xtrJOB` (
@@ -5043,5 +5051,6 @@ INSERT INTO upgrade (upgrade, applied) VALUES ('invenio_2014_08_12_format_code_v
 INSERT INTO upgrade (upgrade, applied) VALUES ('invenio_2014_08_13_tag_recjsonvalue',NOW());
 INSERT INTO upgrade (upgrade, applied) VALUES ('invenio_2014_11_04_format_recjson',NOW());
 INSERT INTO upgrade (upgrade, applied) VALUES ('invenio_2014_08_31_next_collection_tree',NOW());
+INSERT INTO upgrade (upgrade, applied) VALUES ('invenio_2014_04_01_new_aidAFFILIATIONS',NOW());
 
 -- end of file
