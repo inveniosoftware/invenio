f1a84e2493ec17ef14639b23ff93e9e5b14a5e82
Pedro Gaudêncio <pedro.gaudencio@cern.ch>
2014-04-02 15:07:28 +0200

    deposit: simple record deposition
    * Adds initial deposition type for uploading records
      that other depositions may inherit from.
    
    Signed-off-by: Pedro Gaudêncio <pedro.gaudencio@cern.ch>
    Reviewed-by: Javier Martin Montull <javier.martin.montull@cern.ch>
    Reviewed-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/deposit/tasks.py
+++ invenio/modules/deposit/tasks.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-#
-# This file is part of Invenio.
-# Copyright (C) 2012, 2013 CERN.
-#
-# Invenio is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License as
-# published by the Free Software Foundation; either version 2 of the
-# License, or (at your option) any later version.
-#
-# Invenio is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Invenio; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+##
+## This file is part of Invenio.
+## Copyright (C) 2012, 2013, 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
 
@@ -32,9 +32,7 @@ from functools import partial
 from flask import current_app, abort, request
 from flask.ext.login import current_user
 
-from invenio.modules.records.api import Record
 from invenio.modules.records.api import get_record
-from invenio.legacy.dbquery import run_sql
 from invenio.modules.deposit.models import Deposition, Agent, \
     DepositionDraftCacheManager
 from invenio.ext.logging import register_exception
@@ -46,6 +44,19 @@ from invenio.legacy.bibdocfile.api import BibRecDocs
 from invenio.legacy.bibsched.bibtask import task_low_level_submission, \
     bibtask_allocate_sequenceid
 from invenio.modules.pidstore.models import PersistentIdentifier
+
+
+def filter_empty_helper(keys=None):
+    """ Remove empty elements from a list"""
+    def _inner(elem):
+        if isinstance(elem, dict):
+            for k, v in elem.items():
+                if (keys is None or k in keys) and v:
+                    return True
+            return False
+        else:
+            return bool(elem)
+    return _inner
 
 
 def is_api_request(obj, eng):
