d2c9f9a0debb846303244198cf5d5f8db566090e
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-04-03 13:49:47 +0200

    records: bibupload timestamp fix
    * Fixes an issue causing revision verifier to reject uploads. When
      bibupload inserts/updates records it sets hstRECORD.job_date=t1 and
      bibfmt.last_updated=t1 and bibrec.modification_date=t2
      (with t2 > t1). This is intended behavior in master (see #1431).
    
      Revision verifier checks 005 on upload against hstRECORD.job_date
      (i.e. t1). Records model was getting modification_date/005 from
      bibrec.modification_date (i.e. t2).
    
      Thus getting a record and trying to reupload it would get rejected,
      since the XML would have t2 in 005, which revision verifier would
      compares against t1 from hstRECORD.
    
      Modification date ought to be taken from bibrec.modification_date
      and all three timestamps ought to be the same. Thus below is a hack,
      that ought to be fixed in bibupload instead. Unfortunately so much
      other code is might rely on the current behavior that it's hard
      to change.
    
    Co-authored-by: Esteban J. G. Gabancho <egabancho@gmail.com>
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/records/recordext/functions/get_modification_date.py
+++ invenio/modules/records/recordext/functions/get_modification_date.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2013 CERN.
+## Copyright (C) 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -26,8 +26,35 @@ def get_modification_date(recid):
 
     @return: Creation date
     """
+    # WARNING: HACK/FIXME
+    #
+    # When bibupload inserts/updates records it sets hstRECORD.job_date=t1 and
+    # bibfmt.last_updated=t1 and bibrec.modification_date=t2 (with t2 > t1).
+    # This is intended behaviour in master (see ticket #1431).
+    #
+    # Revision verifier checks 005 on upload against hstRECORD.job_date
+    # (i.e. t1).
+    #
+    # JSONAlchemy was getting modification_date/005 from
+    # bibrec.modification_date (i.e. t2).
+    #
+    # Thus if you did a get_record, and tried to reupload the file, the XML
+    # woudl have t2 in 005, which revision verifier would reject since it
+    # compares against t1.
+    #
+    # Modification date ought to be taken from bibrec.modification_date and all
+    # three timestamps ought to be the same. Thus below is a hack, that ought
+    # to be fixed in bibupload instead. Unfortunately so much other code is
+    # might rely on the current behaviour that it's hard to change.
+    from invenio.modules.formatter.models import Bibfmt
     from invenio.modules.records.models import Record as Bibrec
+
     try:
-        return Bibrec.query.get(recid).modification_date
+        return Bibfmt.query.filter_by(
+            id_bibrec=recid, format='xm'
+        ).first().last_updated
     except AttributeError:
-        return None
+        try:
+            return Bibrec.query.get(recid).modification_date
+        except AttributeError:
+            return None
