c4a3d1b791ec60fe8cbc90c56a6b46a31a2abd3d
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-04-04 15:35:14 +0200

    jsonalchemy: failing test fix
    * Fixes failing test cases which was due to side effects introduced
      in 0a7ab631883fe514a28116fbc7fb8b47bd5c1bb4.
    
    * Fixes PEP8 errors.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/jsonalchemy/testsuite/test_parser.py
+++ invenio/modules/jsonalchemy/testsuite/test_parser.py
@@ -22,7 +22,6 @@
 __revision__ = \
     "$Id$"
 
-import os
 import tempfile
 
 from flask.ext.registry import PkgResourcesDirDiscoveryRegistry, \
@@ -33,8 +32,10 @@ from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
 
 Field_parser = lazy_import('invenio.modules.jsonalchemy.parser:FieldParser')
 Model_parser = lazy_import('invenio.modules.jsonalchemy.parser:ModelParser')
-guess_legacy_field_names = lazy_import('invenio.modules.jsonalchemy.parser:guess_legacy_field_names')
-get_producer_rules = lazy_import('invenio.modules.jsonalchemy.parser:get_producer_rules')
+guess_legacy_field_names = lazy_import(
+    'invenio.modules.jsonalchemy.parser:guess_legacy_field_names')
+get_producer_rules = lazy_import(
+    'invenio.modules.jsonalchemy.parser:get_producer_rules')
 
 TEST_PACKAGE = 'invenio.modules.jsonalchemy.testsuite'
 
@@ -56,8 +57,10 @@ def clean_field_model_definitions():
 class TestParser(InvenioTestCase):
 
     def setUp(self):
-        self.app.extensions['registry']['testsuite.fields'] = field_definitions()
-        self.app.extensions['registry']['testsuite.models'] = model_definitions()
+        self.app.extensions['registry'][
+            'testsuite.fields'] = field_definitions()
+        self.app.extensions['registry'][
+            'testsuite.models'] = model_definitions()
 
     def tearDown(self):
         del self.app.extensions['registry']['testsuite.fields']
@@ -85,12 +88,13 @@ class TestParser(InvenioTestCase):
         tmp_file.write(config)
         tmp_file.flush()
 
-        self.app.extensions['registry']['testsuite.fields'].register(tmp_file.name)
+        self.app.extensions['registry'][
+            'testsuite.fields'].register(tmp_file.name)
         clean_field_model_definitions()
-        self.assertRaises(FieldParserException, Field_parser.reparse, 'testsuite')
+        self.assertRaises(
+            FieldParserException, Field_parser.reparse, 'testsuite')
         tmp_file.close()
         clean_field_model_definitions()
-
 
     def test_wrong_field_definitions(self):
         """JSONAlchemy - wrong field definitions"""
@@ -104,8 +108,10 @@ class TestParser(InvenioTestCase):
         tmp_file_4.write(config_4)
         tmp_file_4.flush()
         clean_field_model_definitions()
-        self.app.extensions['registry']['testsuite.fields'].register(tmp_file_4.name)
-        self.assertRaises(FieldParserException, Field_parser.reparse, 'testsuite')
+        self.app.extensions['registry'][
+            'testsuite.fields'].register(tmp_file_4.name)
+        self.assertRaises(
+            FieldParserException, Field_parser.reparse, 'testsuite')
         tmp_file_4.close()
         clean_field_model_definitions()
 
@@ -121,35 +127,52 @@ class TestParser(InvenioTestCase):
         tmp_file_5.write(config_5)
         tmp_file_5.flush()
         clean_field_model_definitions()
-        self.app.extensions['registry']['testsuite.fields'].register(tmp_file_5.name)
-        self.assertRaises(FieldParserException, Field_parser.reparse, 'testsuite')
+        self.app.extensions['registry'][
+            'testsuite.fields'].register(tmp_file_5.name)
+        self.assertRaises(
+            FieldParserException, Field_parser.reparse, 'testsuite')
         tmp_file_5.close()
         clean_field_model_definitions()
 
     def test_field_rules(self):
         """JsonAlchemy - field parser"""
         self.assertTrue(len(Field_parser.field_definitions('testsuite')) >= 22)
-        #Check that all files are parsed
-        self.assertTrue('authors' in Field_parser.field_definitions('testsuite'))
+        # Check that all files are parsed
+        self.assertTrue(
+            'authors' in Field_parser.field_definitions('testsuite'))
         self.assertTrue('title' in Field_parser.field_definitions('testsuite'))
-        #Check work around for [n] and [0]
-        self.assertTrue(Field_parser.field_definitions('testsuite')['doi']['pid'])
-        #Check if derived and calulated are well parserd
+        # Check work around for [n] and [0]
+        self.assertTrue(
+            Field_parser.field_definitions('testsuite')['doi']['pid'])
+        # Check if derived and calulated are well parserd
         self.assertTrue('dummy' in Field_parser.field_definitions('testsuite'))
-        self.assertEquals(Field_parser.field_definitions('testsuite')['dummy']['pid'], 2)
-        self.assertEquals(Field_parser.field_definitions('testsuite')['dummy']['rules'].keys(), ['json', 'derived'])
-        self.assertTrue(len(Field_parser.field_definitions('testsuite')['dummy']['producer']), 2)
+        self.assertEquals(
+            Field_parser.field_definitions('testsuite')['dummy']['pid'], 2)
+        self.assertEquals(Field_parser.field_definitions(
+            'testsuite')['dummy']['rules'].keys(), ['json', 'derived'])
+        self.assertTrue(
+            len(Field_parser.field_definitions(
+                'testsuite')['dummy']['producer']
+                ),
+            2
+        )
         self.assertTrue(Field_parser.field_definitions('testsuite')['_random'])
-        #Check override
-        value = {'a':'a', 'b':'b', 'k':'k'}
-        self.assertEquals(eval(Field_parser.field_definitions('testsuite')['title']['rules']['marc'][0]['function']),
-                {'form': 'k', 'subtitle': 'b', 'title': 'a'})
-        #Check extras
-        self.assertTrue('json_ext' in Field_parser.field_definitions('testsuite')['modification_date'])
+        # Check override
+        value = {'a': 'a', 'b': 'b', 'k': 'k'}  # noqa
+        self.assertEquals(
+            eval(Field_parser.field_definitions('testsuite')
+                 ['title']['rules']['marc'][0]['function']),
+            {'form': 'k', 'subtitle': 'b', 'title': 'a'})
+        # Check extras
+        self.assertTrue(
+            'json_ext' in
+            Field_parser.field_definitions('testsuite')['modification_date']
+        )
 
         tmp = Field_parser.field_definitions('testsuite')
         Field_parser.reparse('testsuite')
-        self.assertEquals(len(Field_parser.field_definitions('testsuite')), len(tmp))
+        self.assertEquals(
+            len(Field_parser.field_definitions('testsuite')), len(tmp))
 
     def test_wrong_field_name_inside_model(self):
         """JSONAlchmey - wrong field name inside model"""
@@ -162,49 +185,74 @@ class TestParser(InvenioTestCase):
         tmp_file_8.write(config_8)
         tmp_file_8.flush()
         clean_field_model_definitions()
-        self.app.extensions['registry']['testsuite.models'].register(tmp_file_8.name)
-        self.assertRaises(ModelParserException, Model_parser.reparse, 'testsuite')
+        self.app.extensions['registry'][
+            'testsuite.models'].register(tmp_file_8.name)
+        self.assertRaises(
+            ModelParserException, Model_parser.reparse, 'testsuite')
         tmp_file_8.close()
         clean_field_model_definitions()
 
     def test_model_definitions(self):
         """JsonAlchemy - model parser"""
+        clean_field_model_definitions()
         self.assertTrue(len(Model_parser.model_definitions('testsuite')) >= 2)
-        self.assertTrue('test_base' in Model_parser.model_definitions('testsuite'))
+        self.assertTrue(
+            'test_base' in Model_parser.model_definitions('testsuite'))
         tmp = Model_parser.model_definitions('testsuite')
         Model_parser.reparse('testsuite')
-        self.assertEquals(len(Model_parser.model_definitions('testsuite')), len(tmp))
+        self.assertEquals(
+            len(Model_parser.model_definitions('testsuite')), len(tmp))
+        clean_field_model_definitions()
 
     def test_resolve_several_models(self):
         """JSONAlchemy - test resolve several models"""
         test_model = Model_parser.model_definitions('testsuite')['test_model']
-        self.assertEquals(Model_parser.resolve_models('test_model', 'testsuite')['fields'],
-                test_model['fields'])
-        self.assertEquals(Model_parser.resolve_models(['test_base', 'test_model'], 'testsuite')['fields'],
-                test_model['fields'])
+        clean_field_model_definitions()
+        self.assertEquals(
+            Model_parser.resolve_models('test_model', 'testsuite')['fields'],
+            test_model['fields'])
+        self.assertEquals(
+            Model_parser.resolve_models(
+                ['test_base', 'test_model'], 'testsuite')['fields'],
+            test_model['fields'])
+        clean_field_model_definitions()
 
     def test_field_name_model_based(self):
         """JSONAlchemy - field name model based"""
-        field_model_def = Field_parser.field_definition_model_based('title', 'test_model', 'testsuite')
+        clean_field_model_definitions()
+        field_model_def = Field_parser.field_definition_model_based(
+            'title', 'test_model', 'testsuite')
         field_def = Field_parser.field_definitions('testsuite')['title_title']
 
-        value = {'a': 'Awesome title', 'b':'sub title', 'k':'form'}
+        value = {'a': 'Awesome title', 'b': 'sub title', 'k': 'form'}
         from invenio.base.utils import try_to_eval
 
-        self.assertEqual(try_to_eval(field_model_def['rules']['marc'][0]['function'], value=value),
-                         try_to_eval(field_def['rules']['marc'][0]['function'], value=value))
-
+        self.assertEqual(
+            try_to_eval(field_model_def['rules'][
+                        'marc'][0]['function'], value=value),
+            try_to_eval(field_def['rules']['marc'][0]['function'],
+                        value=value))
+        clean_field_model_definitions()
 
     def test_guess_legacy_field_names(self):
         """JsonAlchemy - check legacy field names"""
-        self.assertEquals(guess_legacy_field_names(('100__a', '245'), 'marc', 'testsuite'),
-                {'100__a':['_first_author.full_name'], '245':['title']})
-        self.assertEquals(guess_legacy_field_names('foo', 'bar', 'baz'), {'foo': []})
+        self.assertEquals(
+            guess_legacy_field_names(('100__a', '245'), 'marc', 'testsuite'),
+            {'100__a': ['_first_author.full_name'], '245': ['title']})
+        self.assertEquals(
+            guess_legacy_field_names('foo', 'bar', 'baz'), {'foo': []})
 
     def test_get_producer_rules(self):
         """JsonAlchemy - check producer rules"""
-        self.assertEquals(len(get_producer_rules('keywords', 'json_for_marc', 'testsuite')), 1)
-        self.assertRaises(KeyError, lambda: get_producer_rules('foo', 'json_for_marc', 'testsuite'))
+        clean_field_model_definitions()
+        self.assertEquals(
+            len(get_producer_rules('keywords', 'json_for_marc', 'testsuite')),
+            1
+        )
+        self.assertRaises(
+            KeyError,
+            lambda: get_producer_rules('foo', 'json_for_marc', 'testsuite'))
+        clean_field_model_definitions()
 
 
 TEST_SUITE = make_test_suite(TestParser)
