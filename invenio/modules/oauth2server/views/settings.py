5099b13974c66838d27d11053a33ebe8c598e008
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-04-07 09:48:05 +0200

    oauth2server: settings test
    * Add simple test of settings view.
    
    * Fixes file header issue and some PEP8 errors.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/oauth2server/views/settings.py
+++ invenio/modules/oauth2server/views/settings.py
@@ -1,24 +1,24 @@
 # -*- coding: utf-8 -*-
 #
-## This file is part of ZENODO.
-## Copyright (C) 2014 CERN.
-##
-## ZENODO is free software: you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or
-## (at your option) any later version.
-##
-## ZENODO is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-##
-## You should have received a copy of the GNU General Public License
-## along with ZENODO. If not, see <http://www.gnu.org/licenses/>.
-##
-## In applying this licence, CERN does not waive the privileges and immunities
-## granted to it by virtue of its status as an Intergovernmental Organization
-## or submit itself to any jurisdiction.
+# This file is part of ZENODO.
+# Copyright (C) 2014 CERN.
+#
+# ZENODO is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ZENODO is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ZENODO. If not, see <http://www.gnu.org/licenses/>.
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
 
 
 """
@@ -131,10 +131,10 @@ def index():
     ).all()
 
     authorized_apps = Token.query.options(db.joinedload('client')).filter(
-        Token.user_id==current_user.get_id(),
-        Token.is_personal==False,
-        Token.is_internal==False,
-        Client.is_internal==False,
+        Token.user_id == current_user.get_id(),
+        Token.is_personal == False,
+        Token.is_internal == False,
+        Client.is_internal == False,
     ).all()
 
     return render_template(
