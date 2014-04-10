d4699250cd5d26d5617ac26db5a4f78445f7e3be
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-04-10 15:53:38 +0200

    workflows: registry refactoring
    * Refactors registry into generic registry integrated in
      invenio.ext.registry and workflow specific registry.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/modules/workflows/registry.py
+++ invenio/modules/workflows/registry.py
@@ -18,36 +18,24 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 import inspect
-from invenio.ext.registry import ModuleAutoDiscoverySubRegistry
+from invenio.ext.registry import DictModuleAutoDiscoverySubRegistry
 from flask.ext.registry import RegistryError, RegistryProxy
 
 
-class WorkflowsRegistry(ModuleAutoDiscoverySubRegistry):
-    def __init__(self, *args, **kwargs):
-        self._registry = {}
-        self.registry = None
-        super(WorkflowsRegistry, self).__init__(*args, **kwargs)
+class WorkflowsRegistry(DictModuleAutoDiscoverySubRegistry):
+    def keygetter(self, key, class_):
+        return class_.__name__ if key is None else key
 
-    def register(self, class_or_module, key=None):  # pylint: disable=W0221
-        """
-        Workflow discovery:
-
-            .workflows.<module name>:<class name>
-
-        where module name and class name are identical.
-
-        :param key: Key to register object under
-        :param item: Object to register
-        """
+    def valuegetter(self, class_or_module):
         if inspect.ismodule(class_or_module):
             attr_name = class_or_module.__name__.split('.')[-1]
             if attr_name == '__init__':
                 # Ignore __init__ modules.
-                return
+                return None
 
             if hasattr(class_or_module, attr_name):
-                key = attr_name if key is None else key
-                class_or_module = getattr(class_or_module, attr_name)
+                #key = attr_name if key is None else key
+                return getattr(class_or_module, attr_name)
             else:
                 all_ = getattr(class_or_module, '__all__', [])
                 if len(all_) == 0:
@@ -55,48 +43,8 @@ class WorkflowsRegistry(ModuleAutoDiscoverySubRegistry):
                         "Workflow class not found. Class name must match "
                         "module name or be first element in  __all__."
                     )
-                class_or_module = getattr(class_or_module, all_[0])
-
-        key = class_or_module.__name__ if key is None else key
-
-        if key in self._registry:
-            raise RegistryError("Key %s already registered." % key)
-
-        self._registry[key] = class_or_module
-
-    def unregister(self, key):  # pylint: disable=W0221
-        """
-        Unregister an object under a given key. Raises ``KeyError`` in case
-        the given key doesn't exists.
-        """
-        del self[key]
-
-    def __iter__(self):
-        # pylint: disable=R0921
-        return self._registry.__iter__()
-
-    def __len__(self):
-        return self._registry.__len__()
-
-    def __contains__(self, item):
-        return self._registry.__contains__(item)
-
-    def __getitem__(self, key):
-        return self._registry[key]
-
-    def __setitem__(self, key, value):
-        return self.register(value, key=key)
-
-    def __delitem__(self, key):
-        return self._registry.__delitem__(key)
-
-    def items(self):
-        """
-        Get list of key/value pairs.
-
-        :param item: Object to register
-        """
-        return self._registry.items()
+                return getattr(class_or_module, all_[0])
+        return class_or_module
 
 
 workflows = RegistryProxy('workflows', WorkflowsRegistry, 'workflows')
