53f44798339236dd169fcdc8f3cee433e5d2c525
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-04-22 17:46:04 +0200

    celery: test case helper
    * Adds testcase class helper for easier testing of Celery tasks. The
      helper will create a new Celery application per test, similar to the
      way InvenioTestCase creates a Flask application per test.
    
    * Adds example test case using the new helper.
    
    * Removes no longer needed test runner hack.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/celery/testsuite/test_celery.py
+++ invenio/celery/testsuite/test_celery.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2013 CERN.
+## Copyright (C) 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -23,71 +23,38 @@ Invenio Celery unit tests
 
 from __future__ import absolute_import
 
-# Invenio runs tests as standalone modules, instead of from their package
-# hierarchy. This means that this test module is executed as if you ran
-# 'python celery_unit_tests.py'. This causes the directory of this file to be added
-# to the sys.path. This causes the module invenio.celery to be importable
-# as just 'celery' since modules/miscutil/lib is now in sys.path. Thus, the real
-# celery package is no longer importable, causing an import error in
-# invenio.celery. Solutions could be to rename invenio.celery, however then you
-# cannot start celery workers with 'celeryd -A invenio' and must use something
-# like 'celeryd -A invenio.<new name>.celery' instead.
-#
-# Below lines removes the directory of this file from the sys.path so modules
-# are imported from their proper location.
-import sys
-import os
-from distutils.sysconfig import get_python_lib
-EXCLUDE = [
-    '',
-    os.path.dirname(os.path.abspath(__file__)),
-    os.path.join(get_python_lib(), 'invenio'),
-]
-sys.path = filter(
-    lambda x: x not in EXCLUDE,
-    sys.path
-)
-
-from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
-from invenio.celery import celery
+from invenio.testsuite import make_test_suite, run_test_suite
+from invenio.celery.testsuite.helpers import CeleryTestCase
 
 
-class CeleryTest(InvenioTestCase):
-    def setUp(self):
-        # Execute tasks synchronously
-        celery.conf.CELERY_ALWAYS_EAGER = True
-        # Set in-memory result backend
-        celery.conf.CELERY_RESULT_BACKEND = 'cache'
-        celery.conf.CELERY_CACHE_BACKEND = 'memory'
-        # Don't silence exceptions in tasks.
-        celery.conf.CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
-        # Trigger task registering (this is done by the worker, to register all tasks)
-        celery.loader.import_default_modules()
-
+class CeleryTest(CeleryTestCase):
     def test_loader(self):
         """ Test if `workers.py` files are correctly registered. """
-        self.assertTrue('invenio.celery.tasks.invenio_version' in celery.tasks)
+        self.assertTrue('invenio.celery.tasks.invenio_version' in
+                        self.celery_app.tasks)
 
     def test_task_invenio_version(self):
         """ Test calling of tasks """
 
-        from invenio.config import CFG_VERSION
+        from invenio.base.globals import cfg
         from invenio.celery.tasks import invenio_version
 
         # Call task function without celery
-        self.assertEqual(invenio_version(), CFG_VERSION)
+        self.assertEqual(invenio_version(), cfg['CFG_VERSION'])
         # Call task via Celery machinery
-        self.assertEqual(invenio_version.delay().get(), CFG_VERSION)
+        self.assertEqual(invenio_version.delay().get(), cfg['CFG_VERSION'])
 
     def test_task_invenio_db_test(self):
         """ Test Flask request context in tasks """
         from invenio.celery.tasks import invenio_db_test
+
         # Call task via Celery machinery
         self.assertEqual(invenio_db_test.delay(1).get(), 1)
         self.assertEqual(invenio_db_test.delay(2).get(), 2)
         self.assertEqual(invenio_db_test.delay(3).get(), 3)
+
         # Call task without Celery machinery.
-        with celery.loader.flask_app.test_request_context():
+        with self.celery_app.loader.flask_app.test_request_context():
             self.assertEqual(invenio_db_test(1), 1)
 
 
