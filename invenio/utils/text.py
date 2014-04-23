87ca3f7e68b24f5594e893ccee533b3de61cf901
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-04-23 17:29:52 +0200

    utils: slugify text function addition
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/utils/text.py
+++ invenio/utils/text.py
@@ -741,6 +741,16 @@ def strip_accents(x):
     # return UTF-8 representation of the Unicode string:
     return y.encode("utf-8")
 
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+
+def slugify(text, delim=u'-'):
+    """Generates an ASCII-only slug."""
+    result = []
+    for word in _punct_re.split(text.lower()):
+        result.extend(unidecode(word).split())
+    return unicode(delim.join(result))
+
 
 def show_diff(original, modified, prefix="<pre>", sufix="</pre>"):
     """
