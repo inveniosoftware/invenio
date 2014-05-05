9c93bbf4efa8491053215cca23c923f749889992
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-05-05 15:57:36 +0200

    base: dead code removal
    * Remove legacy unused functions and variables.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/base/utils.py
+++ invenio/base/utils.py
@@ -24,31 +24,9 @@
     Implements various utils.
 """
 
-import os
-import pkg_resources
-import re
-
 from flask import has_app_context, current_app
 from werkzeug.utils import import_string, find_modules
 from functools import partial
-
-
-def register_extensions(app):
-    for ext_name in app.config.get('EXTENSIONS', []):
-
-        ext = import_string(ext_name)
-        ext = getattr(ext, 'setup_app', ext)
-        #try:
-
-        #except:
-        #    continue
-
-#        try:
-        ext(app)
-        #except Exception as e:
-        #    app.logger.error('%s: %s' % (ext_name, str(e)))
-
-    return app
 
 
 def import_module_from_packages(name, app=None, packages=None, silent=False):
@@ -103,58 +81,16 @@ def import_submodules_from_packages(name, app=None, packages=None,
     return out
 
 
-collect_blueprints = partial(import_module_from_packages, 'views')
 autodiscover_user_settings = partial(import_module_from_packages,
                                      'user_settings')
-autodiscover_configs = partial(import_module_from_packages, 'config')
 autodiscover_facets = partial(import_submodules_from_packages, 'facets')
 autodiscover_managers = partial(import_module_from_packages, 'manage')
-autodiscover_workflows = partial(import_module_from_packages, 'workflows')
 autodiscover_redirect_methods = partial(import_submodules_from_packages,
                                         'redirect_methods')
-autodiscover_celery_tasks = partial(import_module_from_packages, 'tasks')
 autodiscover_template_context_functions = partial(
     import_submodules_from_packages, 'template_context_functions')
 autodiscover_format_elements = partial(
     import_submodules_from_packages, 'format_elements')
-autodiscover_widgets = partial(import_module_from_packages, 'widgets')
-
-
-def autodiscover_non_python_files(file_name, package_name, app=None,
-                                  packages=None):
-    """
-    Helper function to autodiscover non python files that are included inside
-    the package. (see MANIFEST.in)
-
-    :param file_name: it could be a string or a regex to search for the files
-    inside the package
-    :type file_name: string or string regex
-    :param package_name:
-    :param app:
-    :param packages:
-
-    :return: List of full paths of non python files discovered
-    """
-    return [os.path.join(os.path.dirname(m.__file__), f)
-            for m in import_module_from_packages(package_name, app, packages)
-            for f in pkg_resources.resource_listdir(m.__name__, '')
-            if re.match(file_name, f)]
-
-
-def register_configurations(app):
-    """Includes the configuration parameters of the config file.
-
-    E.g. If the blueprint specify the config string `invenio.messages.config`
-    any uppercase variable defined in the module `invenio.messages.config` is
-    loaded into the system.
-    """
-    from flask import Config
-    new_config = Config(app.config.root_path)
-    for config in autodiscover_configs(app):
-        new_config.from_object(config)
-
-    new_config.update(app.config)
-    app.config = new_config
 
 
 def try_to_eval(string, context={}, **general_context):
