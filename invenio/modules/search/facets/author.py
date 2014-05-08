547ab1c5c29272e2647aa71a479e56f2bb376eb5
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-05-08 11:48:39 +0200

    search: facet registry
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/search/facets/author.py
+++ invenio/modules/search/facets/author.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2013 CERN.
+## Copyright (C) 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,8 +17,8 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""Collection Facet"""
+"""Author facet implementation."""
 
-from ..facet_builders import CollectionFacetBuilder
+from ..facet_builders import FacetBuilder
 
-facet = CollectionFacetBuilder('collection', order=1)
+facet = FacetBuilder('author', order=2)
