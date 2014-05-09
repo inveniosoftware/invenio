39872212a38f6e787791d61780ca52fb999f9f61
Federico Poli <federico.poli@cern.ch>
2014-05-09 13:34:56 +0200

    WebSearch: new Journal Hint Service
    * Adds the Journal Hint Service, that displays
      'Were you looking for a journal reference? Try: <link>'
      when the user search for a raw journal reference.
    
    * Adds unit and regression tests for the new search service.
    
    * Adds two unit tests to `search_from_reference`, just to be sure that
      it accepts unicode
    
    Signed-off-by: Federico Poli <federico.poli@cern.ch>

--- modules/websearch/lib/websearch_services_regression_tests.py
+++ modules/websearch/lib/websearch_services_regression_tests.py
@@ -309,10 +309,164 @@ class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
         if error_messages:
             self.fail(merge_error_messages(error_messages))
 
+class WebSearchServicesJournalHintService(InvenioTestCase):
+    """Check JournalHintService plugin"""
+
+    def setUp(self):
+        """Load plugin"""
+        search_service_plugins = PluginContainer(os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
+                                                 api_version=__required_plugin_API_version__,
+                                                 plugin_signature=SearchService)
+        self.plugin = search_service_plugins.get('JournalHintService')()
+
+    def test_search_author_Tom(self):
+        """websearch - search for an author using invenio sintax, with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'author:Tom'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_find_a_John(self):
+        """websearch - search 'find a John', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'find a John'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_date_after_2001(self):
+        """websearch - search 'find date after 2001', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'find date after 2001'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_year_2001_or_year_2002(self):
+        """websearch - search 'year:2001 OR year:2002', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'year:2001 OR year:2002'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_Monkey(self):
+        """websearch - search 'Monkey', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'Monkey'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_Monkey_monkey(self):
+        """websearch - search 'Monkey monkey', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'Monkey monkey'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_Monkey_monkey_2014(self):
+        """websearch - search 'Monkey monkey (2014)', with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = 'Monkey monkey (2014)'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_empty_string(self):
+        """websearch - search empty string, with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = ''
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_Nucl_Phys_B75_1974_461(self):
+        """websearch - search 'Nucl.Phys.,B75,(1974),461', with JournalHintService"""
+        user_info = collect_user_info(1)
+        pattern = "Nucl.Phys.,B75,(1974),461"
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assert_(response[0] >=50)
+        self.assert_('journal:"Nucl. Phys. B75 (1974) 461"' in response[1])
+
+    def test_search_Nucl_Phys_B75_1974_461_with_spaces(self):
+        """websearch - search '  Nucl.  Phys.   B75   (1974)  461   ', with JournalHintService"""
+        user_info = collect_user_info(1)
+        pattern = '  Nucl.  Phys.   B75   (1974)  461   '
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assert_(response[0] >=50)
+        self.assert_('journal:"Nucl. Phys. B75 (1974) 461"' in response[1])
+
+    def test_search_D_S_Salopek_J_R_Bond_and_J_M_Bardeen_Phys_Rev_D40_1989_1753(self):
+        """websearch - search 'D.S. Salopek, J.R.Bond and J.M.Bardeen,Phys.Rev.D40(1989)1753.', with JournalHintService"""
+        user_info = collect_user_info(1)
+        pattern = 'D.S. Salopek, J.R.Bond and J.M.Bardeen,Phys.Rev.D40(1989)1753.'
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assert_(response[0] >=50)
+        self.assert_('journal:"Phys. Rev. D40 (1989) 1753"' in response[1])
+
+    def test_search_Capella_Pere_utf8(self):
+        """websearch - search 'Capellà Pere' utf8, with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = u'Capellà Pere'.encode('utf8')
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+    def test_search_Pais_Valencia_utf8(self):
+        """websearch - search 'País Valencià' utf8, with JournalHintService"""
+        user_info = collect_user_info(0)
+        pattern = u'País Valencià'.encode('utf8')
+        search_units = create_basic_search_units(None, pattern, '')
+        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
+                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+                                      f='', search_units=search_units, ln='en')
+        self.assertEqual(response,
+                         (0, ''))
+
+
 TEST_SUITE = make_test_suite(WebSearchServicesLoading,
                              WebSearchServicesCollectionNameSearch,
-                             WebSearchServicesSubmissionNameSearch)
-
+                             WebSearchServicesSubmissionNameSearch,
+                             WebSearchServicesJournalHintService)
 
 if __name__ == "__main__":
     run_test_suite(TEST_SUITE, warn_user=True)
