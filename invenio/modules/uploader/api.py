892ce622617cee4e31cd002fa38cc49df3f80670
Esteban J. G. Gabancho <egabancho@gmail.com>
2014-05-13 09:12:12 +0200

    uploader: workflows pre and post tasks hooks
    * Adds posibility to enhance uploader workflows with pre and post tasks
      hooks.
    
    Acked-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/uploader/api.py
+++ invenio/modules/uploader/api.py
@@ -17,91 +17,48 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+"""
+    invenio.modules.uploader.api
+    ----------------------------
+
+    Uploader API.
+
+    Following example shows how to use this API for an easy use case::
+
+        >>> from invenio.modules.uploader.api import run
+        >>> blob = open('./testsuite/data/demo_record_marc_data.xml').read()
+        >>> reader_info = dict(schema='xml')
+        >>> run('insert', blob, master_format='marc', reader_info=reader_info)
+
+"""
+
 from __future__ import print_function
 
 from celery import chord
-from werkzeug.utils import import_string
-from workflow.engine import GenericWorkflowEngine as WorkflowEngine
 
 from invenio.base.globals import cfg
-from invenio.celery import celery
-from invenio.modules.jsonalchemy.reader import Reader, split_blob
-from invenio.modules.records.api import Record
+from invenio.modules.jsonalchemy.reader import split_blob
 
 from . import signals
+from .tasks import translate, run_workflow
 
-
-def run(name, input_file, master_format='marc', force=False, pretend=False,
-        sync=False, reader_info={}, **kwargs):
-    """@todo: Docstring for run.
+def run(name, input_file, master_format='marc', reader_info={}, **kwargs):
+    """Entry point to run any of the modes of the uploader.
 
     :name: @todo
     :input_file: @todo
     :master_format: @todo
-    :force: @todo
-    :pretend: @todo
-    :sync: @todo
     :reader_info: @todo
-    :**kwargs: @todo
-
+    :kwargs: @todo force, pretend, sync
     :returns: @todo
     """
-    signals.uploader_started.send(mode=name, blob=input_file,
-                                  master_format=master_format, force=force,
-                                  pretend=pretend, **kwargs)
+    signals.uploader_started.send(mode=name,
+                                  blob=input_file,
+                                  master_format=master_format,
+                                  **kwargs)
     for chunk in split_blob(input_file, master_format,
                             cfg['UPLOADER_NUMBER_RECORD_PER_WORKER'],
                             **reader_info):
-        chord(_translate.starmap(
+        chord(translate.starmap(
             [(blob, master_format, reader_info) for blob in chunk])
-        )(_run_workflow.s(name=name, force=force, pretend=pretend, **kwargs))
-
-
-@celery.task
-def _translate(blob, master_format, kwargs={}):
-    """@todo: Docstring for _translate.
-
-    :blob: @todo
-    :master_format: @todo
-    :kwargs: @todo
-    :returns: @todo
-
-    """
-    return (blob,
-            Reader.translate(blob, Record, master_format, **kwargs).dumps())
-
-
-@celery.task
-def _run_workflow(records, name, **kwargs):
-    """@todo: Docstring for _run_workflow.
-
-    :records: @todo
-    :name: @todo
-    :**kwargs: @todo
-    :returns: @todo
-
-    """
-    workflow = import_string(cfg['UPLOADER_WORKFLOWS'][name])
-    wfe = WorkflowEngine()
-    wfe.setWorkflow(workflow)
-    wfe.setVar('options', kwargs)
-    records = [(obj[0], Record(json=obj[1])) for obj in records[0]]
-    wfe.process(records)
-    records = [obj[1].get('recid') for obj in records]
-    signals.uploader_finished.send(name=name, result=records, **kwargs)
-    return records
-
-
-@celery.task
-def _error_handler(uuid):
-    """@todo: Docstring for _error_handler.
-
-    :uuid: @todo
-    :returns: @todo
-
-    """
-    result = celery.AsyncResult(uuid)
-    exc = result.get(propagate=False)
-    print('Task %r raised exception: %r\n%r'
-          % (uuid, exc, result.traceback))
-    return None
+        )(run_workflow.s(name=name, **kwargs))
--- invenio/modules/uploader/tasks.py
+++ invenio/modules/uploader/tasks.py
@@ -18,191 +18,84 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 """
-Core Uploader tasks
-===================
+    invenio.modules.uploader.tasks
+    ------------------------------
 
-Those are the main/common tasks that the uploader will use, they are used inside
-the workflows defined in the ``worflows`` folder.
+    Uploader celery tasks.
 """
 
+import six
+from werkzeug.utils import import_string
+from workflow.engine import GenericWorkflowEngine as WorkflowEngine
+
 from invenio.base.globals import cfg
-from invenio.modules.pidstore.models import PersistentIdentifier
+from invenio.celery import celery
+from invenio.modules.jsonalchemy.reader import Reader
+from invenio.modules.records.api import Record
 
-# Allow celery to collect uploader tasks
-from .api import _translate, _run_workflow, _error_handler  # pylint: disable=W0611
-from .errors import UploaderWorkflowException
-
-
-def raise_(ex):
-    """
-    Helper task to raise an exception.
-    """
-    def _raise_(obj, eng):
-        raise ex
-    return _raise_
+from . import signals
 
 
-def validate(step):
-    """
-    Validate the record using the `validate` method present in each record and
-    the validation mode, either from the command line `options` or from
-    `UPLOADER_VALIDATION_MODE`.
+@celery.task
+def translate(blob, master_format, kwargs={}):
+    """Translate from the `master_format` to `JSON`.
 
-    The for the validation the `schema` information from the field definition
-    is used, see `invenio.modules.jsonalchemy.jsonext.parsers.schema_parser`
-    """
-    def _validate(obj, eng):
-        record = obj[1]
-        mode = eng.getVar('options', {}).get('validation_mode',
-                                             cfg['UPLOADER_VALIDATION_MODE'])
-        eng.log.info("Validating record using mode: '%s'", (mode, ))
-        if not hasattr(record, 'validate'):
-            raise UploaderWorkflowException(
-                step, msg="An 'validate' method is needed")
-
-        validator_errors = record.validate()
-        eng.log.info('Validation errors: %s' % (str(validator_errors), ))
-        if mode.lower() == 'strict' and validator_errors:
-            raise UploaderWorkflowException(
-                step, msg="One or more validation errors have occurred, please "
-                          " check them or change the 'validation_mode' to "
-                          "'permissive'.\n%s" % (str(validator_errors), ))
-        eng.log.info('Finish validating the current record')
-    return _validate
-
-
-def retrieve_record_id_from_pids(step):
-    """
-    Retrieve the record identifier from a record by using all the persistent
-    identifiers present in the record.
-
-    If any PID matches with the any in DB then, the record id found is set to
-    the current `record`
-    """
-    def _retrieve_record_id_from_pids(obj, eng):
-        record = obj[1]
-        eng.log.info('Look for PIDs inside the current record')
-        if not hasattr(record, 'persistent_identifiers'):
-            raise UploaderWorkflowException(
-                step, msg="An 'persistent_identifiers' method is needed")
-
-        for pid_name, pid_values in record.persistent_identifiers:
-            eng.log.info("Found PID '%s' with value '%s', trying to match it",
-                         (pid_name, pid_values))
-            matching_recids = set()
-            for possible_pid in pid_values:
-                eng.log.info("Looking for PID %s", (possible_pid, ))
-                pid = PersistentIdentifier.get(
-                    possible_pid.get('type'), possible_pid.get('value'),
-                    possible_pid.get('provider'))
-                if pid:
-                    eng.log.inf("PID found in the data base %s",
-                                (pid.object_value, ))
-                    matching_recids.add(pid.object_value)
-            if len(matching_recids) > 1:
-                raise UploaderWorkflowException(
-                    step, msg="Multiple records found in the database, %s that "
-                              "match '%s'" % (repr(matching_recids), pid_name))
-            elif matching_recids:
-                record['recid'] = matching_recids.pop()
-                eng.log.info(
-                    'Finish looking for PIDs inside the current record')
-                break
-        eng.log.info('Finish looking for PIDs inside the current record')
-    return _retrieve_record_id_from_pids
-
-
-def reserve_record_id(step):
-    """
-    Reserve a new record id for the current object and add it to
-    `record['recid']`.
-    """
-    # TODO: manage exceptions in a better way
-    def _reserve_record_id(obj, eng):
-        record = obj[1]
-        eng.log.info('Reserve a recid for the new record')
-        try:
-            pid = PersistentIdentifier.create('recid', pid_value=None,
-                                              pid_provider='invenio')
-            record['recid'] = int(pid.pid_value)
-            pid.reserve()
-            eng.log.info("Finish reserving a recid '%s' for the new record",
-                         (pid.pid_value, ))
-        except Exception as e:
-            raise UploaderWorkflowException(step, e.message)
-    return _reserve_record_id
-
-
-def save_record(step):
-    """
-    Save the record to the DB using the `_save` method from it.
-    """
-    def _save(obj, eng):
-        record = obj[1]
-        eng.log.info('Saving record to DB')
-        if not hasattr(record, '_save'):
-            raise UploaderWorkflowException(
-                step, msg="An '_save' method is needed")
-        try:
-
-            record._save()
-            eng.log.info('Record saved to DB')
-        except Exception as e:
-            raise UploaderWorkflowException(step, e.message)
-    return _save
-
-
-def save_master_format(step):
-    """@todo: Docstring for save_master_format.
-
-    :step: @todo
-    :returns: @todo
+    :blob: String contain the input file.
+    :master_format: Format of the blob, it will used to decide which reader to
+        use.
+    :kwargs: Arguments to be used by the reader.
+        See :class:`invenio.modules.jsonalchemy.reader:Reader`
+    :returns: The blob and the `JSON` representation of the input file created
+        by the reader.
 
     """
-    def _save_master_format(obj, eng):
-        from invenio.base.helpers import utf8ifier
-        from invenio.modules.formatter.models import Bibfmt
-        from invenio.ext.sqlalchemy import db
-        from zlib import compress
-        eng.log.info('Saving master record to DB')
-        bibfmt = Bibfmt(id_bibrec=obj[1]['recid'],
-                        format=obj[1].additional_info.master_format,
-                        kind='master',
-                        last_updated=obj[1]['modification_date'],
-                        value=compress(utf8ifier(obj[0])))
-        db.session.add(bibfmt)
-        db.session.commit()
-        eng.log.info('Master record saved to DB')
-    return _save_master_format
+    return (blob,
+            Reader.translate(blob, Record, master_format, **kwargs).dumps())
 
 
-def update_pidstore(step):
+@celery.task
+def run_workflow(records, name, **kwargs):
+    """Run the uploader workflow itself.
+
+    :records: List of tuples `(blob, json_record)` from :func:`translate`.
+    :name: Name of the workflow to be run.
+    :kwargs: Additional arguments to be used by the tasks of the workflow.
+    :returns: Typically the list of record Ids that has been process, although
+        this value could be modify by the `post_tasks`.
+
     """
-    Save each PID present in the record to the `PersistentIdentifier` data
-    storage.
-    """
-    # TODO: manage exceptions
-    def _update_pidstore(obj, eng):
-        record = obj[1]
-        eng.log.info('Look for PIDs inside the current record and register '
-                     'them in the DB')
-        if not hasattr(record, 'persistent_identifiers'):
-            raise UploaderWorkflowException(
-                step, msg="An 'persistent_identifiers' method is needed")
+    def _run_pre_post_tasks(tasks):
+        """Helper function to run list of functions"""
+        for task in tasks:
+            task(records, **kwargs)
 
-        for pid_name, pid_values in record.persistent_identifiers:
-            eng.log.info("Found PID '%s'", (pid_name, ))
-            for pid_value in pid_values:
-                pid = PersistentIdentifier.get(
-                    pid_value.get('type'), pid_value.get('value'),
-                    pid_value.get('provider'))
-                if pid is None:
-                    pid = PersistentIdentifier.create(
-                        pid_value.get('type'), pid_value.get('value'),
-                        pid_value.get('provider'))
-                if not pid.has_object('rec', record['recid']):
-                    pid.assign('rec', record['recid'])
-        eng.log.info('Finish looking for PIDs inside the current record and '
-                     'register them in the DB')
+    #FIXME: don't know why this is needed but IT IS!
+    records = records[0]
 
-    return _update_pidstore
+    workflow = import_string(cfg['UPLOADER_WORKFLOWS'][name])
+    _run_pre_post_tasks(workflow['pre_tasks'])
+    tasks = workflow['tasks']
+    wfe = WorkflowEngine()
+    wfe.setWorkflow(tasks)
+    wfe.setVar('options', kwargs)
+    wfe.process(records)
+    _run_pre_post_tasks(workflow['post_tasks'])
+    signals.uploader_finished.send(name=name, result=records, **kwargs)
+    return records
+
+
+# @celery.task
+# def error_handler(uuid):
+#     """@todo: Docstring for _error_handler.
+#
+#     :uuid: @todo
+#     :returns: @todo
+#
+#     """
+#     result = celery.AsyncResult(uuid)
+#     exc = result.get(propagate=False)
+#     print('Task %r raised exception: %r\n%r'
+#           % (uuid, exc, result.traceback))
+#     return None
+
+
