0c1740ec579b0fe07de26a52658ad0a58e7bc055
Artem Tsikiridis <artem.tsikiridis@cern.ch>
2014-07-02 13:40:37 +0200

    BibAuthorID: Updated test suite.
    * Inspire specific tests run only on inspire.
    * Fix for nosetests in bibauthorid_name_utils.
    * Test updated for Atlantis demo site
    
    Signed-off-by: Minn Soe <minn.soe@cern.ch>

--- modules/bibauthorid/lib/bibauthorid_dbinterface_regression_tests.py
+++ modules/bibauthorid/lib/bibauthorid_dbinterface_regression_tests.py
@@ -10,6 +10,9 @@ from invenio.bibauthorid_dbinterface import _get_grouped_records_using_marc_cach
 from invenio.bibauthorid_dbinterface import _get_grouped_records_from_db
 from invenio.bibauthorid_rabbit_regression_tests import BibAuthorIDRabbitTestCase
 from invenio.bibauthorid_webapi import get_bibrefs_from_bibrecs
+from invenio.bibauthorid_testutils import get_modified_marc_for_test
+from invenio.bibauthorid_testutils import get_new_marc_for_test
+from invenio.bibauthorid_testutils import get_bibrec_for_record
 
 from invenio.testutils import make_test_suite
 from invenio.testutils import run_test_suite
@@ -22,12 +25,12 @@ class Marc100700CacheTestCase(BibAuthorIDRabbitTestCase):  # TODO Refactor, crea
 
     def setUp(self):
         super(Marc100700CacheTestCase, self).setUp()
-        marc_xml_record = self.get_marc_for_rabbit_test(opt_mode='insert',
-                                                        author_name=self.author_name,
-                                                        ext_id=None,
-                                                        co_authors_names=self.coauthors_names)
-        self.bibrec_to_test = self.get_bibrec_for_record(marc_xml_record,
-                                                         opt_mode='insert')
+        marc_xml_record = get_new_marc_for_test("Test", author_name=self.author_name,
+                                                ext_id=None,
+                                                co_authors_names=self.co_authors_names)
+        self.bibrec_to_test = get_bibrec_for_record(marc_xml_record,
+                                                    opt_mode='insert')
+        self.bibrecs_to_clean = [self.bibrec_to_test]
         bibrefs = get_bibrefs_from_bibrecs([self.bibrec_to_test])
         for bibref in bibrefs:
             self.ref_table = bibref[1][0][0].split(":")[0]
@@ -41,8 +44,8 @@ class Marc100700CacheTestCase(BibAuthorIDRabbitTestCase):  # TODO Refactor, crea
         self.assertEquals(from_cache, from_db)
 
     def test_cache_coauthor_refs(self):
-        from_cache = _get_coauthor_refs_from_marc_caches_of_paper(self.bibrec_to_test)
-        from_db = _get_coauthor_refs_from_db_of_paper(self.bibrec_to_test)
+        from_cache = sorted(_get_coauthor_refs_from_marc_caches_of_paper(self.bibrec_to_test))
+        from_db = sorted(_get_coauthor_refs_from_db_of_paper(self.bibrec_to_test))
         self.assertEquals(from_cache, from_db)
 
     def test_cache_name_by_bibref(self):
@@ -56,6 +59,7 @@ class Marc100700CacheTestCase(BibAuthorIDRabbitTestCase):  # TODO Refactor, crea
             self.assertEquals(from_cache, from_db)
 
     def tearDown(self):
+        super(Marc100700CacheTestCase, self).tearDown()
         destroy_partial_marc_caches()
 
 
