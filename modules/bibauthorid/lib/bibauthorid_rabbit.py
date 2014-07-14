b680ca37896b0022601d9c36c03bd2c7e530e03a
Samuele Kaplun <samuele.kaplun@cern.ch>
2014-07-14 17:44:13 +0200

    BibAuthorID: Python 2.6 compatibility
    * Fixes a small Python 2.6 compatibility issue where
      frozensets are not subscriptable.
    
    Co-authored-by: Samuele Carli <samuele.carli@cern.ch>
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

--- modules/bibauthorid/lib/bibauthorid_rabbit.py
+++ modules/bibauthorid/lib/bibauthorid_rabbit.py
@@ -131,6 +131,7 @@ def rabbit(bibrecs=None, check_invalid_papers=False,
     updated_pids = set()
     deleted = frozenset(p[0] for p in get_deleted_papers())
 
+    bibrecs = list(bibrecs)
     for idx, rec in enumerate(bibrecs):
 
         logger.log("Considering %s" % str(rec))
