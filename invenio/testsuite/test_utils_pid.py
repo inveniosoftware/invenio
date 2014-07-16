f3f4e7e9002d73202e77af8884cc256375b36ded
Pedro Gaudêncio <pmgaudencio@gmail.com>
2014-07-16 16:18:12 +0200

    utils: arXiv identifier normalization
    * Adds support for normalizing arxiv identifiers according
      to http://arxiv.org/help/arxiv_identifier and
      http://arxiv.org/help/arxiv_identifier_for_services.
      (closes #1958, #1961)
    
    * Fixes issues with pre-2007 arxiv identifier validation.
    
    * Fixes PEP257 problems.
    
    * Adds tests including missing assertion of to_url() method.
    
    Co-authored-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/testsuite/test_utils_pid.py
+++ invenio/testsuite/test_utils_pid.py
@@ -25,59 +25,88 @@ from invenio.testsuite import make_test_suite, run_test_suite, InvenioTestCase
 from invenio.utils import persistentid as pidutils
 
 identifiers = [
-    ('urn:isbn:0451450523', ['urn', ], ''),
-    ('urn:isan:0000-0000-9E59-0000-O-0000-0000-2', ['urn', ], ''),
-    ('urn:issn:0167-6423', ['urn', ], ''),
-    ('urn:ietf:rfc:2648', ['urn', ], ''),
-    ('urn:mpeg:mpeg7:schema:2001', ['urn', ], ''),
-    ('urn:oid:2.16.840', ['urn', ], ''),
-    ('urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66', ['urn', ], ''),
-    ('urn:nbn:de:bvb:19-146642', ['urn', ], ''),
-    ('urn:lex:eu:council:directive:2010-03-09;2010-19-UE', ['urn', ], ''),
-    ('ark:/13030/tqb3kh97gh8w', ['ark'], ''),
-    ('http://www.example.org/ark:/13030/tqb3kh97gh8w', ['ark', 'url'], ''),
+    ('urn:isbn:0451450523', ['urn', ], '', ''),
+    ('urn:isan:0000-0000-9E59-0000-O-0000-0000-2', ['urn', ], '', ''),
+    ('urn:issn:0167-6423', ['urn', ], '', ''),
+    ('urn:ietf:rfc:2648', ['urn', ], '', ''),
+    ('urn:mpeg:mpeg7:schema:2001', ['urn', ], '', ''),
+    ('urn:oid:2.16.840', ['urn', ], '', ''),
+    ('urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66', ['urn', ], '', ''),
+    ('urn:nbn:de:bvb:19-146642', ['urn', ], '',
+        'http://nbn-resolving.org/urn:nbn:de:bvb:19-146642'),
+    ('urn:lex:eu:council:directive:2010-03-09;2010-19-UE', ['urn', ], '', ''),
+    ('ark:/13030/tqb3kh97gh8w', ['ark'], '', ''),
+    ('http://www.example.org/ark:/13030/tqb3kh97gh8w', ['ark', 'url'], '', ''),
     ('10.1016/j.epsl.2011.11.037', ['doi', 'handle'],
-        '10.1016/j.epsl.2011.11.037'),
+        '10.1016/j.epsl.2011.11.037',
+        'http://dx.doi.org/10.1016/j.epsl.2011.11.037'),
     ('doi:10.1016/j.epsl.2011.11.037', ['doi', 'handle'],
-        '10.1016/j.epsl.2011.11.037'),
+        '10.1016/j.epsl.2011.11.037',
+        'http://dx.doi.org/10.1016/j.epsl.2011.11.037'),
     ('http://dx.doi.org/10.1016/j.epsl.2011.11.037', ['doi', 'url', ],
-        '10.1016/j.epsl.2011.11.037'),
-    ('9783468111242', ['isbn', 'ean13'], ''),
-    ('4006381333931', ['ean13'], ''),
-    ('73513537', ['ean8'], ''),
-    ('1562-6865', ['issn'], ''),  # 'eiss, ''n'
-    ('10013/epic.10033', ['handle'], ''),
-    ('hdl:10013/epic.10033', ['handle'], '10013/epic.10033'),
+        '10.1016/j.epsl.2011.11.037',
+        'http://dx.doi.org/10.1016/j.epsl.2011.11.037'),
+    ('9783468111242', ['isbn', 'ean13'], '', ''),
+    ('4006381333931', ['ean13'], '', ''),
+    ('73513537', ['ean8'], '', ''),
+    ('1562-6865', ['issn'], '', ''),  # 'eiss, ''n'
+    ('10013/epic.10033', ['handle'], '',
+        'http://hdl.handle.net/10013/epic.10033'),
+    ('hdl:10013/epic.10033', ['handle'], '10013/epic.10033',
+        'http://hdl.handle.net/10013/epic.10033'),
     ('http://hdl.handle.net/10013/epic.10033', ['handle', 'url'],
-        '10013/epic.10033'),
-    ('978-3-905673-82-1', ['isbn'], ''),
-    ('0-9752298-0-X', ['isbn'], ''),
-    ('0077-5606', ['issn'], ''),
-    ('urn:lsid:ubio.org:namebank:11815', ['lsid', 'urn'], ''),
-    ('0A9 2002 12B4A105 7', ['istc'], ''),
-    ('1188-1534', ['issn'], ''),  # 'liss, ''n'
-    ('12082125', ['pmid'], ''),
-    ('pmid:12082125', ['pmid'], '12082125'),
-    ('http://purl.oclc.org/foo/bar', ['purl', 'url'], ''),
+        '10013/epic.10033', 'http://hdl.handle.net/10013/epic.10033'),
+    ('978-3-905673-82-1', ['isbn'], '', ''),
+    ('0-9752298-0-X', ['isbn'], '', ''),
+    ('0077-5606', ['issn'], '', ''),
+    ('urn:lsid:ubio.org:namebank:11815', ['lsid', 'urn'], '', ''),
+    ('0A9 2002 12B4A105 7', ['istc'], '', ''),
+    ('1188-1534', ['issn'], '', ''),  # 'liss, ''n'
+    ('12082125', ['pmid'], '',
+        'http://www.ncbi.nlm.nih.gov/pubmed/12082125'),
+    ('pmid:12082125', ['pmid'], '12082125',
+        'http://www.ncbi.nlm.nih.gov/pubmed/12082125'),
+    ('http://purl.oclc.org/foo/bar', ['purl', 'url'], '',
+        'http://purl.oclc.org/foo/bar'),
     #('123456789999', ['upc'], ''),
-    ('http://www.heatflow.und.edu/index2.html', ['url'], ''),
-    ('urn:nbn:de:101:1-201102033592', ['urn'], ''),
-    ('PMC2631623', ['pmcid'], ''),
-    ('2011ApJS..192...18K', ['ads'], ''),
-    ('ads:2011ApJS..192...18K', ['ads'], '2011ApJS..192...18K'),
-    ('0000 0002 1825 0097', ['orcid', 'isni'], ''),
-    ('0000-0002-1694-233X', ['orcid', 'isni'], ''),
-    ('1422-4586-3573-0476', ['isni'], ''),
-    ('arXiv:1310.2590', ['arxiv', ], ''),
-    ('arxiv:1310.2590', ['arxiv', ], ''),
-    ('1310.2590', ['arxiv', ], ''),
+    ('http://www.heatflow.und.edu/index2.html', ['url'], '',
+        'http://www.heatflow.und.edu/index2.html'),
+    ('urn:nbn:de:101:1-201102033592', ['urn'], '',
+        'http://nbn-resolving.org/urn:nbn:de:101:1-201102033592'),
+    ('PMC2631623', ['pmcid'], '',
+        'http://www.ncbi.nlm.nih.gov/pmc/PMC2631623'),
+    ('2011ApJS..192...18K', ['ads'], '',
+        'http://adsabs.harvard.edu/abs/2011ApJS..192...18K'),
+    ('ads:2011ApJS..192...18K', ['ads'], '2011ApJS..192...18K',
+        'http://adsabs.harvard.edu/abs/2011ApJS..192...18K'),
+    ('0000 0002 1825 0097', ['orcid', 'isni'], '',
+        'http://orcid.org/0000 0002 1825 0097'),
+    ('0000-0002-1694-233X', ['orcid', 'isni'], '',
+        'http://orcid.org/0000-0002-1694-233X'),
+    ('1422-4586-3573-0476', ['isni'], '', ''),
+    ('arXiv:1310.2590', ['arxiv', ], 'arXiv:1310.2590',
+        'http://arxiv.org/abs/arXiv:1310.2590'),
+    ('arxiv:1310.2590', ['arxiv', ], 'arXiv:1310.2590',
+        'http://arxiv.org/abs/arXiv:1310.2590'),
+    ('1310.2590', ['arxiv', ], 'arXiv:1310.2590',
+        'http://arxiv.org/abs/arXiv:1310.2590'),
+    ('math.GT/0309136', ['arxiv', ], 'arXiv:math/0309136',
+        'http://arxiv.org/abs/arXiv:math/0309136'),
+    ('hep-th/9901001v27', ['arxiv', ], 'arXiv:hep-th/9901001v27',
+        'http://arxiv.org/abs/arXiv:hep-th/9901001v27'),
+    ('arxiv:math.GT/0309136v2', ['arxiv', ], 'arXiv:math/0309136v2',
+        'http://arxiv.org/abs/arXiv:math/0309136v2'),
+    ('arXiv:hep-th/9901001v27', ['arxiv', ], 'arXiv:hep-th/9901001v27',
+        'http://arxiv.org/abs/arXiv:hep-th/9901001v27'),
+    ('9912.12345v2', ['arxiv', ], 'arXiv:9912.12345v2',
+        'http://arxiv.org/abs/arXiv:9912.12345v2'),
 ]
 
 
 class PersistentIdentifierUtilities(InvenioTestCase):
 
     def test_detect_schemes(self):
-        for i, expected_schemes, normalized_value in identifiers:
+        for i, expected_schemes, normalized_value, url_value in identifiers:
             schemes = pidutils.detect_identifier_schemes(i)
             self.assertEqual(
                 schemes,
@@ -88,12 +117,12 @@ class PersistentIdentifierUtilities(InvenioTestCase):
             )
 
     def test_is_type(self):
-        for i, schemes, normalized_value in identifiers:
+        for i, schemes, normalized_value, url_value in identifiers:
             for s in schemes:
                 self.assertTrue(getattr(pidutils, 'is_%s' % s)(i))
 
     def test_normalize_pid(self):
-        for i, expected_schemes, normalized_value in identifiers:
+        for i, expected_schemes, normalized_value, url_value in identifiers:
             self.assertEqual(
                 pidutils.normalize_pid(i, expected_schemes[0]),
                 normalized_value or i
@@ -102,16 +131,17 @@ class PersistentIdentifierUtilities(InvenioTestCase):
         assert pidutils.normalize_pid(None, 'handle') is None
 
     def test_tourl(self):
-        for i, expected_schemes, normalized_value in identifiers:
-            pidutils.to_url(
-                pidutils.normalize_pid(i, expected_schemes[0]),
-                expected_schemes[0]
+        for i, expected_schemes, normalized_value, url_value in identifiers:
+            self.assertEqual(
+                pidutils.to_url(pidutils.normalize_pid(i, expected_schemes[0]),
+                                expected_schemes[0]),
+                url_value,
             )
 
     def test_valueerror(self):
         # Many validators rely on a special length of the identifier before
         # testing further. This test, checks that the validators are still
-        # well-behaved when the length matches, but the persisitent identifier
+        # well-behaved when the length matches, but the persistent identifier
         # is invalid.
         for i in range(20):
             nonsense_pid = "a" * i
--- invenio/utils/persistentid.py
+++ invenio/utils/persistentid.py
@@ -15,6 +15,8 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+"""Persistent identifier validation and scheme detection."""
+
 import re
 from six.moves.urllib.parse import urlparse
 
@@ -23,16 +25,13 @@ doi_regexp = re.compile(
     "(doi:|http://dx.doi.org/)?(10\.\d+(.\d+)*/.*)$",
     flags=re.I
 )
-"""
-See http://en.wikipedia.org/wiki/Digital_object_identifier
-"""
+"""See http://en.wikipedia.org/wiki/Digital_object_identifier."""
 
 handle_regexp = re.compile(
     "(hdl:|http://hdl.handle.net/)?([^/\.]+(\.[^/\.]+)*/.*)$",
     flags=re.I
 )
-"""
-See http://handle.net/rfc/rfc3651.html
+"""See http://handle.net/rfc/rfc3651.html.
 
 <Handle>          = <NamingAuthority> "/" <LocalName>
 <NamingAuthority> = *(<NamingAuthority>  ".") <NAsegment>
@@ -41,55 +40,40 @@ See http://handle.net/rfc/rfc3651.html
 """
 
 arxiv_post_2007_regexp = re.compile(
-    "(?:arXiv:)?(\d{4})\.(\d{4})(v\d+)?$",
+    "(arxiv:)?(\d{4})\.(\d{4,5})(v\d+)?$",
     flags=re.I
 )
-"""
-See http://arxiv.org/help/arxiv_identifier
-"""
+"""See http://arxiv.org/help/arxiv_identifier and http://arxiv.org/help/arxiv_identifier_for_services."""
 
-arxiv_pre_2007_regexp = re.compile("[a-z\-]+(\.[A-Z]{2})?/\d{4}\d+$")
-"""
-See http://arxiv.org/help/arxiv_identifier
-"""
+arxiv_pre_2007_regexp = re.compile(
+    "(arxiv:)?([a-z\-]+)(\.[a-z]{2})?(/\d{4})(\d+)(v\d+)?$",
+    flags=re.I
+)
+"""See http://arxiv.org/help/arxiv_identifier and http://arxiv.org/help/arxiv_identifier_for_services."""
 
 ads_regexp = re.compile("(ads:|ADS:)?(\d{4}[A-Z]\S{13}[A-Z.:])$")
-"""
-See http://adsabs.harvard.edu/abs_doc/help_pages/data.html
-"""
+"""See http://adsabs.harvard.edu/abs_doc/help_pages/data.html"""
 
 pmcid_regexp = re.compile("PMC\d+$", flags=re.I)
-"""
-PubMed Central ID regular expression
-"""
+"""PubMed Central ID regular expression."""
 
 pmid_regexp = re.compile("(pmid:)?(\d+)$", flags=re.I)
-"""
-PubMed ID regular expression
-"""
+"""PubMed ID regular expression."""
 
 ark_suffix_regexp = re.compile("ark:/\d+/.+$")
-"""
-See http://en.wikipedia.org/wiki/Archival_Resource_Key and
-https://confluence.ucop.edu/display/Curation/ARK
-"""
+"""See http://en.wikipedia.org/wiki/Archival_Resource_Key and https://confluence.ucop.edu/display/Curation/ARK."""
 
 lsid_regexp = re.compile("urn:lsid:[^:]+(:[^:]+){2,3}$", flags=re.I)
-"""
-See http://en.wikipedia.org/wiki/LSID
-"""
+"""See http://en.wikipedia.org/wiki/LSID."""
 
 
 def _convert_x_to_10(x):
-    """
-    Convert char to int with X being converted to 10.
-    """
+    """Convert char to int with X being converted to 10."""
     return int(x) if x != 'X' else 10
 
 
 def is_isbn10(val):
-    """
-    Test if argument is an ISBN-10 number
+    """Test if argument is an ISBN-10 number.
 
     Courtesy Wikipedia:
     http://en.wikipedia.org/wiki/International_Standard_Book_Number
@@ -105,8 +89,7 @@ def is_isbn10(val):
 
 
 def is_isbn13(val):
-    """
-    Test if argument is an ISBN-13 number
+    """Test if argument is an ISBN-13 number.
 
     Courtesy Wikipedia:
     http://en.wikipedia.org/wiki/International_Standard_Book_Number
@@ -125,7 +108,7 @@ def is_isbn13(val):
 
 
 def is_isbn(val):
-    """ Test if argument is an ISBN-10 or ISBN-13 number """
+    """Test if argument is an ISBN-10 or ISBN-13 number."""
     if is_isbn10(val) or is_isbn13(val):
         if val[0:3] in ["978", "979"] or not is_ean13(val):
             return True
@@ -133,7 +116,7 @@ def is_isbn(val):
 
 
 def is_issn(val):
-    """ Test if argument is an ISSN number """
+    """Test if argument is an ISSN number."""
     try:
         val = val.replace("-", "").replace(" ", "").upper()
         if len(val) != 8:
@@ -145,8 +128,7 @@ def is_issn(val):
 
 
 def is_istc(val):
-    """
-    Test if argument is a International Standard Text Code
+    """Test if argument is a International Standard Text Code.
 
     See http://www.istc-international.org/html/about_structure_syntax.aspx
     """
@@ -163,13 +145,12 @@ def is_istc(val):
 
 
 def is_doi(val):
-    """ Test if argument is a DOI """
+    """Test if argument is a DOI."""
     return doi_regexp.match(val)
 
 
 def is_handle(val):
-    """
-    Test if argument is a Handle
+    """Test if argument is a Handle.
 
     Note, DOIs are also handles, and handle are very generic so they will also
     match e.g. any URL your parse
@@ -178,9 +159,7 @@ def is_handle(val):
 
 
 def is_ean8(val):
-    """
-    Test if argument is a International Article Number (EAN-8)
-    """
+    """Test if argument is a International Article Number (EAN-8)."""
     if len(val) != 8:
         return False
     sequence = [3, 1]
@@ -193,8 +172,7 @@ def is_ean8(val):
 
 
 def is_ean13(val):
-    """
-    Test if argument is a International Article Number (EAN-13)
+    """Test if argument is a International Article Number (EAN-13).
 
     http://en.wikipedia.org/wiki/International_Article_Number_(EAN)
     """
@@ -210,16 +188,12 @@ def is_ean13(val):
 
 
 def is_ean(val):
-    """
-    Test if argument is a International Article Number (EAN-13 or EAN-8)
-    """
+    """Test if argument is a International Article Number (EAN-13 or EAN-8)."""
     return is_ean13(val) or is_ean8(val)
 
 
 def is_isni(val):
-    """
-    Test if argument is an International Standard Name Identifier
-    """
+    """Test if argument is an International Standard Name Identifier."""
     val = val.replace("-", "").replace(" ", "").upper()
     if len(val) != 16:
         return False
@@ -234,8 +208,7 @@ def is_isni(val):
 
 
 def is_orcid(val):
-    """
-    Test if argument is an ORCID ID
+    """Test if argument is an ORCID ID.
 
     See http://support.orcid.org/knowledgebase/articles/116780-structure-of-the-orcid-identifier
     """
@@ -247,7 +220,7 @@ def is_orcid(val):
 
 
 def is_ark(val):
-    """ Test if argument is an ARK """
+    """Test if argument is an ARK."""
     res = urlparse(val)
     return ark_suffix_regexp.match(val) or (
         res.scheme == 'http'
@@ -259,7 +232,7 @@ def is_ark(val):
 
 
 def is_purl(val):
-    """ Test if argument is a PURL """
+    """Test if argument is a PURL."""
     res = urlparse(val)
     return (res.scheme == 'http'
             and res.netloc in ['purl.org', 'purl.oclc.org', 'purl.net',
@@ -268,59 +241,56 @@ def is_purl(val):
 
 
 def is_url(val):
-    """
-    Test if argument is a URL
-    """
+    """Test if argument is a URL."""
     res = urlparse(val)
     return bool(res.scheme and res.netloc and res.params == '')
 
 
 def is_lsid(val):
-    """
-    Test if argument is a LSID
-    """
+    """Test if argument is a LSID."""
     return is_urn(val) and lsid_regexp.match(val)
 
 
 def is_urn(val):
-    """
-    Test if argument is a URN
-    """
+    """Test if argument is a URN."""
     res = urlparse(val)
     return bool(res.scheme == 'urn' and res.netloc == '' and res.path != '')
 
 
 def is_ads(val):
-    """ Test if argument is an ADS id """
+    """Test if argument is an ADS bibliographic code."""
     return ads_regexp.match(val)
 
 
 def is_arxiv_post_2007(val):
+    """Test if argument is a post-2007 arXiv ID."""
     return arxiv_post_2007_regexp.match(val)
 
 
 def is_arxiv_pre_2007(val):
-    return arxiv_post_2007_regexp.match(val)
+    """Test if argument is a pre-2007 arXiv ID."""
+    return arxiv_pre_2007_regexp.match(val)
 
 
 def is_arxiv(val):
-    """ Test if argument is an arXiv ID """
+    """Test if argument is an arXiv ID.
+
+    See http://arxiv.org/help/arxiv_identifier and http://arxiv.org/help/arxiv_identifier_for_services.
+    """
     return is_arxiv_post_2007(val) or is_arxiv_pre_2007(val)
 
 
 def is_pmid(val):
-    """ Test if argument is a PubMed ID
+    """Test if argument is a PubMed ID.
 
-    Warning: PMID are just integers, with not structure, so this function might
-    wrongly say any integer is a PubMed ID
+    Warning: PMID are just integers, with no structure, so this function will
+    say any integer is a PubMed ID
     """
     return pmid_regexp.match(val)
 
 
 def is_pmcid(val):
-    """
-    PubMed Central ID
-    """
+    """Test if argument is a PubMed Central ID."""
     return pmcid_regexp.match(val)
 
 
@@ -344,11 +314,14 @@ CFG_PID_SCHEMES = [
     ('istc', is_istc),
     ('pmid', is_pmid),
 ]
+"""Definition of scheme name and associated test function.
+
+Order of list is important, as identifier scheme detection will test in the
+order given by this list."""
 
 
 def detect_identifier_schemes(val):
-    """
-    Detect persistent identifier scheme for a given value.
+    """Detect persistent identifier scheme for a given value.
 
     Note, some schemes like PMID are very generic.
     """
@@ -363,34 +336,55 @@ def detect_identifier_schemes(val):
     elif 'handle' in schemes and 'url' in schemes \
          and not val.startswith("http://hdl.handle.net/"):
         schemes = filter(lambda x: x != 'handle', schemes)
-    elif 'handle' in schemes and 'ark' in schemes:
+    elif 'handle' in schemes and ('ark' in schemes or 'arxiv' in schemes):
         schemes = filter(lambda x: x != 'handle', schemes)
     return schemes
 
 
 def normalize_doi(val):
+    """Normalize a DOI."""
     m = doi_regexp.match(val)
     return m.group(2)
 
 
 def normalize_handle(val):
+    """Normalize a Handle identifier."""
     m = handle_regexp.match(val)
     return m.group(2)
 
 
 def normalize_ads(val):
+    """Normalize an ADS bibliographic code."""
     m = ads_regexp.match(val)
     return m.group(2)
 
 
 def normalize_pmid(val):
+    """Normalize an PubMed ID."""
     m = pmid_regexp.match(val)
     return m.group(2)
 
 
+def normalize_arxiv(val):
+    """Normalize an arXiv identifier."""
+    if not val.lower().startswith("arxiv:"):
+        val = "arXiv:%s" % val
+    elif val[:6] != "arXiv:":
+        val = "arXiv:%s" % val[6:]
+
+    # Normalize old identifiers to preferred scheme as specified by
+    # http://arxiv.org/help/arxiv_identifier_for_services
+    # (i.e. arXiv:math.GT/0309136 -> arXiv:math/0309136)
+    m = is_arxiv_pre_2007(val)
+    if m and m.group(3):
+        val = "".join(m.group(1, 2, 4, 5))
+        if m.group(6):
+            val += m.group(6)
+    return val
+
+
 def normalize_pid(val, scheme):
-    """
-    Normalize a persistent identifier
+    """Normalize an identifier.
 
     E.g. doi:10.1234/foo and http://dx.doi.org/10.1234/foo and 10.1234/foo
     will all be normalized to 10.1234/foo.
@@ -406,10 +400,13 @@ def normalize_pid(val, scheme):
         return normalize_ads(val)
     elif scheme == 'pmid':
         return normalize_pmid(val)
+    elif scheme == 'arxiv':
+        return normalize_arxiv(val)
     return val
 
 
 def to_url(val, scheme):
+    """Convert a resolvable identifier into a URL for a landing page."""
     val = normalize_pid(val, scheme)
     if scheme == 'doi':
         return "http://dx.doi.org/%s" % val
@@ -424,7 +421,7 @@ def to_url(val, scheme):
     elif scheme == 'ads':
         return "http://adsabs.harvard.edu/abs/%s" % val
     elif scheme == 'pmcid':
-        return "http://www.ncbi.nlm.nih.gov/pmc/articles/%s/" % val
+        return "http://www.ncbi.nlm.nih.gov/pmc/%s" % val
     elif scheme == 'urn':
         if val.lower().startswith("urn:nbn:"):
             return "http://nbn-resolving.org/%s" % val
