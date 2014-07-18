d73bd94b2fbe8666af31dfec25c334c466e6f6df
Samuele Kaplun <samuele.kaplun@cern.ch>
2014-07-18 09:17:46 +0200

    errorlib: context support in sentry
    * Introduces support for HTTP, and user context in sentry, by
      exploiting information provided in req object when available.
      (closes #1960) (PR #2147)
    
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>
    Tested-by: Tibor Simko <tibor.simko@cern.ch>

--- modules/miscutil/lib/errorlib.py
+++ modules/miscutil/lib/errorlib.py
@@ -37,7 +37,8 @@ from invenio.config import CFG_SITE_LANG, CFG_LOGDIR, \
     CFG_SITE_ADMIN_EMAIL_EXCEPTIONS, \
     CFG_ERRORLIB_RESET_EXCEPTION_NOTIFICATION_COUNTER_AFTER, \
     CFG_PROPAGATE_EXCEPTIONS, \
-    CFG_ERRORLIB_SENTRY_URI
+    CFG_ERRORLIB_SENTRY_URI, \
+    CFG_VERSION
 from invenio.urlutils import wash_url_argument
 from invenio.messages import wash_language, gettext_set_language
 from invenio.dateutils import convert_datestruct_to_datetext
@@ -387,7 +388,31 @@ def register_exception(stream='error',
     if CFG_ERRORLIB_SENTRY_URI:
         from raven import Client
         client = Client(CFG_ERRORLIB_SENTRY_URI)
-        client.captureException()
+        try:
+            if req:
+                from invenio.webuser import collect_user_info
+                user_info = collect_user_info(req)
+                client.user_context({'id': user_info['uid'],
+                                    'is_anonymous': user_info['guest'] == '1',
+                                    'is_authenticated': user_info['guest'] == '0',
+                                    'email': user_info['email'],
+                                    'group': user_info['group'],
+                                    'nickname': user_info['nickname']})
+                client.http_context({'url': req.full_uri,
+                                    'remote_ip': user_info['remote_ip'],
+                                    'agent': user_info['agent'],
+                                    'referer': user_info['referer'],
+                                    'method': req.method,
+                                    'query_string': req.args,
+                                    'headers': dict(req.headers_in),
+                                    'https': req.is_https(),
+                                    'env': req.environ})
+                client.extra_context(user_info)
+            filename = _get_filename_and_line(sys.exc_info())[0]
+            client.tags_context({'filename': filename}, {'version', CFG_VERSION})
+            client.captureException()
+        finally:
+            client.context.clear()
 
     if CFG_PROPAGATE_EXCEPTIONS:
         raise
