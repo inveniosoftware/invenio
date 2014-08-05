4015c03e0bb17b897e0aae2944b4c124dae79c2d
Federico Poli <federico.poli@cern.ch>
2014-08-05 13:52:00 +0200

    WebSearch: kwalitee fix
    * Runs `autopep8` on `websearch_services_regression_tests.py`
    
    Signed-off-by: Federico Poli <federico.poli@cern.ch>

--- modules/websearch/lib/websearch_services_regression_tests.py
+++ modules/websearch/lib/websearch_services_regression_tests.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012 CERN.
+## Copyright (C) 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -30,21 +30,23 @@ import os
 import traceback
 
 from invenio.testutils import make_test_suite, \
-                              run_test_suite, \
-                              test_web_page_content, \
-                              merge_error_messages
+    run_test_suite, \
+    test_web_page_content, \
+    merge_error_messages
 from invenio.webuser import collect_user_info, get_uid_from_email
 from invenio.pluginutils import PluginContainer
 from invenio.search_engine import create_basic_search_units
 from invenio.config import CFG_SITE_NAME, \
-     CFG_SITE_SECURE_URL
+    CFG_SITE_SECURE_URL
 from invenio.websearch_services import \
-     get_search_services, \
-     CFG_SEARCH_SERVICES_PATH, \
-     __required_plugin_API_version__, \
-     SearchService
+    get_search_services, \
+    CFG_SEARCH_SERVICES_PATH, \
+    __required_plugin_API_version__, \
+    SearchService
+
 
 class WebSearchServicesLoading(InvenioTestCase):
+
     """Check loading of WebSearch services"""
 
     def test_search_services_loading_time(self):
@@ -52,7 +54,7 @@ class WebSearchServicesLoading(InvenioTestCase):
 
         # Load search services, maybe first time
         t1 = time.time()
-        services = get_search_services()
+        get_search_services()
         t2 = time.time()
         search_services_loading_time = t2 - t1
 
@@ -67,7 +69,7 @@ Limit: %s seconds""" % (search_services_loading_time,
 
         # Load search services, hopefully from cache
         t1 = time.time()
-        services = get_search_services()
+        get_search_services()
         t2 = time.time()
         search_services_loading_time = t2 - t1
 
@@ -83,48 +85,60 @@ Limit: %s second""" % (search_services_loading_time,
     def test_search_services_loading(self):
         """websearch - loading default search services"""
         error_messages = []
-        services_classes = [str(service.__class__) for service in get_search_services()]
-        for expected_service_class in ('CollectionNameSearchService.CollectionNameSearchService',
-                                       'FAQKBService.FAQKBService',
-                                       'SubmissionNameSearchService.SubmissionNameSearchService'):
+        services_classes = [str(service.__class__)
+                            for service in get_search_services()]
+        for expected_service_class in (
+                'CollectionNameSearchService.CollectionNameSearchService',
+                'FAQKBService.FAQKBService',
+                'SubmissionNameSearchService.SubmissionNameSearchService'):
             if not expected_service_class in services_classes:
-                error_messages.append('%s not found in list of loaded services.' % expected_service_class)
+                error_messages.append(
+                    '%s not found in list of loaded services.' % expected_service_class)
 
         if error_messages:
-            self.fail(merge_error_messages(error_messages) + \
+            self.fail(merge_error_messages(error_messages) +
                       '\nList of loaded services: \n%s' % repr(services_classes))
 
     def test_no_broken_search_services_(self):
         """websearch - no broken search services"""
         error_messages = []
-        search_service_plugins = PluginContainer(os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
-                                                 api_version=__required_plugin_API_version__,
-                                                 plugin_signature=SearchService)
+        search_service_plugins = PluginContainer(
+            os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
+            api_version=__required_plugin_API_version__,
+            plugin_signature=SearchService
+        )
         for name, error in search_service_plugins.get_broken_plugins().iteritems():
-            error_messages.append("Service '%s' could not be loaded:\n%s" % \
+            error_messages.append("Service '%s' could not be loaded:\n%s" %
                                   (name, repr(error[0]) + " " + repr(error[1]) + "\n" + "\n".join(traceback.format_tb(error[2]))))
 
         if error_messages:
             self.fail(merge_error_messages(error_messages))
 
+
 class WebSearchServicesCollectionNameSearch(InvenioTestCase):
+
     """Check CollectionNameSearchService plugin"""
 
     def setUp(self):
         """Load plugin"""
-        search_service_plugins = PluginContainer(os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
-                                                 api_version=__required_plugin_API_version__,
-                                                 plugin_signature=SearchService)
-        self.plugin = search_service_plugins.get('CollectionNameSearchService')()
+        search_service_plugins = PluginContainer(
+            os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
+            api_version=__required_plugin_API_version__,
+            plugin_signature=SearchService
+        )
+        self.plugin = search_service_plugins.get(
+            'CollectionNameSearchService')()
 
     def test_search_restricted_collection_as_guest(self):
         """websearch - search for restricted collection as guest, with CollectionNameSearchService"""
         user_info = collect_user_info(0)
         pattern = 'Theses'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
         self.assertEqual(response,
                          (0, ''))
 
@@ -133,9 +147,11 @@ class WebSearchServicesCollectionNameSearch(InvenioTestCase):
         user_info = collect_user_info(get_uid_from_email('jekyll@cds.cern.ch'))
         pattern = 'Theses'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
         self.assert_(response[0] > 75)
         self.assert_('collection/Theses' in response[1])
 
@@ -144,9 +160,11 @@ class WebSearchServicesCollectionNameSearch(InvenioTestCase):
         user_info = collect_user_info(0)
         pattern = 'Atlantis Times Arts'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
         self.assert_(response[0] > 50)
         self.assert_('collection/Atlantis%20Times%20Arts' in response[1])
 
@@ -180,24 +198,31 @@ class WebSearchServicesCollectionNameSearch(InvenioTestCase):
         if error_messages:
             self.fail(merge_error_messages(error_messages))
 
+
 class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
+
     """Check SubmissionNameSearchService plugin"""
 
     def setUp(self):
         """Load plugin"""
-        search_service_plugins = PluginContainer(os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
-                                                 api_version=__required_plugin_API_version__,
-                                                 plugin_signature=SearchService)
-        self.plugin = search_service_plugins.get('SubmissionNameSearchService')()
+        search_service_plugins = PluginContainer(
+            os.path.join(CFG_SEARCH_SERVICES_PATH, '*Service.py'),
+            api_version=__required_plugin_API_version__,
+            plugin_signature=SearchService
+        )
+        self.plugin = search_service_plugins.get(
+            'SubmissionNameSearchService')()
 
     def test_search_restricted_submission_as_guest(self):
         """websearch - search for restricted submission as guest, with SubmissionNameSearchService"""
         user_info = collect_user_info(0)
         pattern = 'submit thesis'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
         self.assertEqual(response,
                          (0, ''))
 
@@ -206,10 +231,12 @@ class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
         user_info = collect_user_info(get_uid_from_email('jekyll@cds.cern.ch'))
         pattern = 'submit thesis'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
-        self.assert_(response[0] >=50)
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
+        self.assert_(response[0] >= 50)
         self.assert_('doctype=DEMOTHE' in response[1])
 
     def test_search_restricted_submission_category(self):
@@ -217,10 +244,12 @@ class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
         user_info = collect_user_info(1)
         pattern = 'submit news'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
-        self.assert_(response[0] >=50)
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
+        self.assert_(response[0] >= 50)
         self.assert_('doctype=DEMOJRN' in response[1])
 
     def test_search_public_submission_as_guest(self):
@@ -228,9 +257,11 @@ class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
         user_info = collect_user_info(0)
         pattern = 'submit article'
         search_units = create_basic_search_units(None, pattern, '')
-        response = self.plugin.answer(req=user_info, user_info=user_info, of='hb',
-                                      cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
-                                      f='', search_units=search_units, ln='en')
+        response = self.plugin.answer(
+            req=user_info, user_info=user_info, of='hb',
+            cc=CFG_SITE_NAME, colls_to_search='', p=pattern,
+            f='', search_units=search_units, ln='en'
+        )
         self.assert_(response[0] >= 50)
         self.assert_('doctype=DEMOART' in response[1])
 
@@ -238,8 +269,9 @@ class WebSearchServicesSubmissionNameSearch(InvenioTestCase):
         """websearch - search for restricted submission as guest, from search page"""
         url = CFG_SITE_SECURE_URL + '/search?p=submit%20thesis&ln=en'
         error_messages = test_web_page_content(url,
-                                               unexpected_text=['Demo Thesis Submission',
-                                                                'Looking for a particular submission?'])
+                                               unexpected_text=[
+                                                   'Demo Thesis Submission',
+                                                   'Looking for a particular submission?'])
         if error_messages:
             self.fail(merge_error_messages(error_messages))
 
