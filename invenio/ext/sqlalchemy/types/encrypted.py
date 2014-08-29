88d5a464d60b31a1a18195c590117b5b3955e352
Konstantinos Kostis <konstantinos.kostis@cern.ch>
2014-08-29 15:38:38 +0200

    sqlalchemy: addition of Encrypted column type
    * Adds submodule `types` inside `invenio.ext.sqlalchemy` package.
    
    * Refactors the `types.py` into separate packages.
    
    * Adds of a type named `Encrypted` that will be used for
      encrypting and decrypting `access_tokens`.  (PR #2173)
    
    * Adds of pycrypto>=2.6.1 to `setup.py`.
    
    Co-authored-by: Jiri Kuncar <jiri.kuncar@cern.ch>
    Signed-off-by: Konstantinos Kostis <konstantinos.kostis@cern.ch>

---
+++ invenio/ext/sqlalchemy/types/encrypted.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+## This file is part of Invenio.
+## Copyright (C) 2011, 2012, 2013, 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""Implement an Encrypted column type."""
+
+import base64
+from Crypto.Cipher import AES
+import hashlib
+from sqlalchemy.types import TypeDecorator, VARCHAR
+
+
+class Encrypted(TypeDecorator):
+
+    """Implement an Encrypted column type."""
+
+    impl = VARCHAR
+    BLOCK_SIZE = 16
+    PADDING = '*'
+
+    def __init__(self, key, *args, **kwargs):
+        """Initialization."""
+        super(Encrypted, self).__init__(*args, **kwargs)
+        # produce a 32-bytes key
+        secret_key = hashlib.sha256(key).digest()
+        self.cipher = AES.new(secret_key)
+
+    def _pad(self, value):
+        """Pad the message to be encrypted, if needed."""
+        padded = (value + (self.BLOCK_SIZE - len(value) % self.BLOCK_SIZE) *
+                  self.PADDING)
+        return padded
+
+    def _aes_encrypt(self, value):
+        """Encrypt a message."""
+        encrypted = base64.b64encode(self.cipher.encrypt(self._pad(value)))
+        return encrypted
+
+    def _aes_decrypt(self, value):
+        """Decrypt a message."""
+        decrypted = (self.cipher.decrypt(base64.b64decode(value)).
+                     decode('utf-8').rstrip(self.PADDING))
+        return decrypted
+
+    def process_bind_param(self, value, dialect):
+        """Encrypt a value on the way in."""
+        return self._aes_enrypt(value)
+
+    def process_result_value(self, value, dialect):
+        """Decrypt value on the way out."""
+        return self._aes_decrypt(value)
