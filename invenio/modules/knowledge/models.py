f1b3f55fc0a64ba45d5d2ee44d80fbba1f700ae7
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-09-01 17:43:42 +0200

    knowledge: PEP8 and PEP257 improvements
    * Removes `__init__` methods.  (PR #2184)
    
    Co-authored-by: Javier Martin Montull <javier.martin.montull@cern.ch>
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/knowledge/models.py
+++ invenio/modules/knowledge/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 ## This file is part of Invenio.
-## Copyright (C) 2011, 2012 CERN.
+## Copyright (C) 2011, 2012, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,9 +17,7 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""
-bibknowledge database models.
-"""
+"""Knowledge database models."""
 
 # General imports.
 from invenio.ext.sqlalchemy import db
@@ -28,48 +26,51 @@ from invenio.ext.sqlalchemy import db
 
 from invenio.modules.search.models import Collection
 
+
 class KnwKB(db.Model):
-    """Represents a KnwKB record."""
-    def __init__(self):
-        pass
+
+    """Represent a KnwKB record."""
+
     __tablename__ = 'knwKB'
     id = db.Column(db.MediumInteger(8, unsigned=True), nullable=False,
-                primary_key=True, autoincrement=True)
+                   primary_key=True, autoincrement=True)
     name = db.Column(db.String(255), server_default='', unique=True)
     description = db.Column(db.Text, nullable=True)
     kbtype = db.Column(db.Char(1), nullable=True)
 
+
 class KnwKBDDEF(db.Model):
-    """Represents a KnwKBDDEF record."""
-    def __init__(self):
-        pass
+
+    """Represent a KnwKBDDEF record."""
+
     __tablename__ = 'knwKBDDEF'
     id_knwKB = db.Column(db.MediumInteger(8, unsigned=True),
-                db.ForeignKey(KnwKB.id), nullable=False, primary_key=True)
+                         db.ForeignKey(KnwKB.id), nullable=False,
+                         primary_key=True)
     id_collection = db.Column(db.MediumInteger(9, unsigned=True),
-                db.ForeignKey(Collection.id), nullable=True)
+                              db.ForeignKey(Collection.id), nullable=True)
     output_tag = db.Column(db.Text, nullable=True)
     search_expression = db.Column(db.Text, nullable=True)
     kb = db.relationship(KnwKB, backref='kbdefs')
     collection = db.relationship(Collection, backref='kbdefs')
 
+
 class KnwKBRVAL(db.Model):
-    """Represents a KnwKBRVAL record."""
-    def __init__(self):
-        pass
+
+    """Represent a KnwKBRVAL record."""
+
     __tablename__ = 'knwKBRVAL'
     id = db.Column(db.MediumInteger(8, unsigned=True), nullable=False,
-                primary_key=True,
-                autoincrement=True)
+                   primary_key=True, autoincrement=True)
     m_key = db.Column(db.String(255), nullable=False, server_default='',
-                index=True)
-    m_value = db.Column(db.Text(30), nullable=False,
-                index=True)
-    id_knwKB = db.Column(db.MediumInteger(8), db.ForeignKey(KnwKB.id), nullable=False,
-                server_default='0')
+                      index=True)
+    m_value = db.Column(db.Text(30), nullable=False, index=True)
+    id_knwKB = db.Column(db.MediumInteger(8), db.ForeignKey(KnwKB.id),
+                         nullable=False, server_default='0')
     kb = db.relationship(KnwKB, backref='kbrvals')
 
 
-__all__ = ['KnwKB',
+__all__ = ('KnwKB',
            'KnwKBDDEF',
-           'KnwKBRVAL']
+           'KnwKBRVAL',
+           )
