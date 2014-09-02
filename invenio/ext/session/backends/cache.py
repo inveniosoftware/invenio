17106da6e78003975b625e49556c5f6c247e1167
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-09-02 15:02:49 +0200

    session: simple cache fix
    * Fixes issue with simple in-memory cache not being able to
      work with timedelta timeouts.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- invenio/ext/session/backends/cache.py
+++ invenio/ext/session/backends/cache.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ## This file is part of Invenio.
-## Copyright (C) 2011, 2012, 2013 CERN.
+## Copyright (C) 2011, 2012, 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,17 +17,15 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""
-    invenio.ext.session.backends.cache
-    ----------------------------------
+"""Session cache backend.
 
-    Configuration:
-
-    - SESSION_BACKEND_CACHE = 'invenio.ext.cache:cache'
-    - SESSION_BACKEND_CACHE_PREFIX = 'session::'
-    - SESSION_BACKEND_CACHE_TIMEOUT = 3600
+Configuration:
+- SESSION_BACKEND_CACHE = 'invenio.ext.cache:cache'
+- SESSION_BACKEND_CACHE_PREFIX = 'session::'
+- SESSION_BACKEND_CACHE_TIMEOUT = 3600
 """
 
+from datetime import timedelta
 from flask import current_app
 from flask.helpers import locked_cached_property
 from werkzeug.utils import import_string
@@ -36,19 +34,18 @@ from ..storage import SessionStorage
 
 
 class Storage(SessionStorage):
-    """
-    Implements session cache storage.
-    """
+
+    """Implements session cache storage."""
 
     @locked_cached_property
     def cache(self):
-        """Returns cache storage."""
+        """Return cache storage."""
         return import_string(current_app.config.get(
             'SESSION_BACKEND_CACHE', 'invenio.ext.cache:cache'))
 
     @locked_cached_property
     def key(self):
-        """Returns cache key prefix."""
+        """Return cache key prefix."""
         return current_app.config.get('SESSION_BACKEND_CACHE_PREFIX',
                                       'session::')
 
@@ -59,6 +56,9 @@ class Storage(SessionStorage):
 
     def set(self, name, value, timeout=None):
         timeout = timeout if timeout is not None else self.timeout
+        # Convert datetime.timedeltas to seconds.
+        if isinstance(timeout, timedelta):
+            timeout = timeout.seconds + timeout.days * 24 * 3600
         self.cache.set(self.key+name, value, timeout=timeout)
 
     def get(self, name):
