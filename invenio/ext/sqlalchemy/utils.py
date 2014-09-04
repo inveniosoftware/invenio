c4510387a1f3936bfe0486193d86b87ea590146a
Jan Aage Lavik <jan.age.lavik@cern.ch>
2014-09-04 08:53:17 +0200

    sqlalchemy: add session_manager
    * Adds a new utility decorator called session_manager for use
      in models to handle commit/rollback actions.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- invenio/ext/sqlalchemy/utils.py
+++ invenio/ext/sqlalchemy/utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 ## This file is part of Invenio.
-## Copyright (C) 2011, 2012, 2013 CERN.
+## Copyright (C) 2011, 2012, 2013, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,11 +16,17 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-"""
-    invenio.ext.sqlalchemy.utils
-    ----------------------------
 
-    Implements various utilities.
+"""Implements various utility functions.
+
+For example, session_manager used to handle commit/rollback:
+
+    .. code-block:: python
+
+        class SomeModel(db.Model):
+            @session_manager
+            def save(self):
+                db.session.add(self)
 """
 
 import sqlalchemy
@@ -30,10 +36,9 @@ from sqlalchemy.orm import class_mapper, properties
 
 
 def get_model_type(ModelBase):
-    """Returns extended model type."""
-
+    """Return extended model type."""
     def getRelationships(self):
-        """Returns table relations."""
+        """Return table relations."""
         retval = list()
         mapper = class_mapper(self)
         synonyms = dict()
@@ -54,8 +59,7 @@ def get_model_type(ModelBase):
         return retval
 
     def todict(self):
-        """Converts model to dictionary."""
-
+        """Convert model to dictionary."""
         def convert_datetime(value):
             try:
                 return value.strftime("%Y-%m-%d %H:%M:%S")
@@ -78,7 +82,7 @@ def get_model_type(ModelBase):
             yield(c.name, value)
 
     def fromdict(self, args):
-        """Updates instance from dictionary."""
+        """Update instance from dictionary."""
         #NOTE Why not to do things simple ...
         self.__dict__.update(args)
 
@@ -92,10 +96,7 @@ def get_model_type(ModelBase):
         #    setattr(self, c.name, d)
 
     def __iter__(self):
-        """Returns an iterable that supports .next()
-            so we can do dict(sa_instance)
-
-        """
+        """Return an iterable that supports .next() for dict(sa_instance)."""
         return self.todict()
 
     ModelBase.todict = todict
@@ -104,3 +105,39 @@ def get_model_type(ModelBase):
     ModelBase.__table_args__ = {}
 
     return ModelBase
+
+
+def session_manager(orig_func):
+    """Decorator to wrap function with the session.
+
+    Useful to add to models functions that is meant to
+    commit itself to DB when called.
+
+    .. code-block:: python
+
+        class SomeModel(db.Model):
+            @session_manager
+            def save(self):
+                db.session.add(self)
+
+    Now the session manager will handle committing and
+    rollbacks on errors and re-raise.
+
+    :param orig_func: original function
+    :type orig_func: callable
+
+    :return: decorated function.
+    """
+    from invenio.ext.sqlalchemy import db
+
+    def new_func(self, *a, **k):
+        """Wrapper function to manage DB session."""
+        try:
+            resp = orig_func(self, *a, **k)
+            db.session.commit()
+            return resp
+        except:
+            db.session.rollback()
+            raise
+
+    return new_func
