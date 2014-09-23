ca159562bc4c32eca99cb565f6c6c1d8ba9b0987
Antonios Manaras <antonios.manaras@cern.ch>
2014-09-23 11:57:32 +0200

    initial Zip file previewer
    * Adds files for integration.
    
    Signed-off-by: Antonios Manaras <antonios.manaras@cern.ch>

---
+++ invenio/modules/previewer/previewerext/zip.py
@@ -0,0 +1,79 @@
+## This file is part of Invenio.
+## Copyright (C) 2013, 2014, 2014 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+
+from flask import render_template, request
+import zipfile
+import datetime
+import os
+import json
+
+def can_preview(f):
+    '''
+    Returns if filetype can be previewed.
+    '''
+    return f.superformat == '.zip'
+
+def preview(f):
+    '''
+    Returns appropiate template and passes the filea and an embed flag.
+    '''
+    tree = children_to_list(make_tree(f.get_full_path()))['children']
+    return render_template("previewer/zip.html", f=f, tree=tree,
+                           embed=request.args.get('embed', type=bool))
+
+
+
+# TEMPLATE_TPL = ("")
+    
+# def print_template(archive_name, tree):
+#     from jinja2 import Template
+#     template = Template(TEMPLATE_TPL)
+#     print template.render(tree=tree, archive_name=archive_name)
+
+
+def make_tree(archive_name):
+    zf = zipfile.ZipFile(archive_name)
+    
+    tree = {'type': 'folder', 'id': -1, 'children': {}}
+    
+
+    for i, info in enumerate(zf.infolist()):
+        comps = info.filename.split(os.sep)
+
+        node = tree
+        for c in comps:
+            if c not in node['children']:
+                if c == '':
+                    node['type'] = 'folder'
+                    continue
+                node['children'][c] = {'name': c, 'type': 'item', 'id': 'item%s' % i, 'children': {}}
+            node = node['children'][c]
+
+        node['size'] = info.file_size
+
+    return tree
+
+
+def children_to_list(node):
+    if node['type'] == 'item':
+        del node['children']
+    else:
+        node['children'] =  list(node['children'].values())
+        node['children'].sort(key=lambda x: x['name'])
+        node['children'] = map(children_to_list, node['children'])
+    return node
