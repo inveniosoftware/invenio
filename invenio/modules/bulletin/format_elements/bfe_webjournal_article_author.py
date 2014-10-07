c34734e7732b19d7500b3e60874f57cb66f52bcb
Eirini Psallida <eirini.psallida@cern.ch>
2014-10-07 15:49:14 +0200

    bulletin: translation fix
    Signed-off-by: Eirini Psallida <eirini.psallida@cern.ch>
    Reviewed-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/bulletin/format_elements/bfe_webjournal_article_author.py
+++ invenio/modules/bulletin/format_elements/bfe_webjournal_article_author.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 ## This file is part of Invenio.
-## Copyright (C) 2009, 2010, 2011 CERN.
+## Copyright (C) 2009, 2010, 2011, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -16,23 +16,26 @@
 ## You should have received a copy of the GNU General Public License
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
-"""
-WebJournal Element - display article author(s)
-"""
-from invenio.config import \
-     CFG_SITE_URL, \
-     CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE
+
+"""WebJournal Element - display article author(s)."""
+
+from invenio.config import (CFG_SITE_URL,
+                            CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE)
+
 from invenio.utils.url import create_html_mailto
 from invenio.base.i18n import gettext_set_language
 
-def format_element(bfo, separator, display_email='yes',
-           email_obfuscation_mode=CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE):
-    """
-    Display article author(s)
+
+def format_element(
+        bfo, separator, display_email='yes',
+        email_obfuscation_mode=CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE):
+    """Display article author(s).
 
     @param separator: separator between authors
     @param display_email: if yes, display link to authors' emails
-    @param email_obfuscation_mode: how email are protected. See possible values in CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE in invenio.conf.
+    @param email_obfuscation_mode: how email are protected.
+    See possible values in CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE
+    in invenio.conf.
     """
     ln = bfo.lang
     _ = gettext_set_language(ln)
@@ -40,10 +43,12 @@ def format_element(bfo, separator, display_email='yes',
     try:
         email_obfuscation_mode_int = int(str(email_obfuscation_mode))
     except:
-        email_obfuscation_mode_int = CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE
+        email_obfuscation_mode_int = (
+            CFG_WEBSTYLE_EMAIL_ADDRESSES_OBFUSCATION_MODE
+        )
 
-    email_subject = _("About your article at %(url)s") % \
-                    {'url': CFG_SITE_URL + bfo.user_info['uri']}
+    email_subject = _("About your article at %(x_url)s",
+                      x_url=CFG_SITE_URL + bfo.user_info['uri'])
 
     authors = bfo.fields('100__a', escape=1)
     emails = bfo.fields('859__a', escape=1)
@@ -54,18 +59,21 @@ def format_element(bfo, separator, display_email='yes',
     for author, email in zip(authors, emails):
         if not author:
             continue
-        if email.strip() and display_email.lower() == 'yes' :
-            authors_list.append(create_html_mailto(email,
-                                                   link_label=author,
-                                                   subject=email_subject,
-                                                   email_obfuscation_mode=email_obfuscation_mode_int))
+        if email.strip() and display_email.lower() == 'yes':
+            authors_list.append(create_html_mailto(
+                email,
+                link_label=author,
+                subject=email_subject,
+                email_obfuscation_mode=email_obfuscation_mode_int))
         else:
             authors_list.append(author)
 
     return separator.join(authors_list)
 
+
 def escape_values(bfo):
-    """
+    """Escape values.
+
     Called by BibFormat in order to check if output of this element
     should be escaped.
     """
