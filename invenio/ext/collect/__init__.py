cedca0d75b80943a24ca91ea06b7ec0882f194be
Javier Martin Montull <javier.martin.montull@cern.ch>
2014-10-08 11:06:57 +0200

    collect: addition of sorting filter
    Signed-off-by: Javier Martin Montull <javier.martin.montull@cern.ch>

--- invenio/ext/collect/__init__.py
+++ invenio/ext/collect/__init__.py
@@ -25,12 +25,26 @@ collect = Collect()
 
 
 def setup_app(app):
-    """Set the application up with the corresct static root."""
-    app.config['COLLECT_STATIC_ROOT'] = app.static_folder
+    """Set the application up with the correct static root."""
+    def filter_(items):
+        """Filter application blueprints."""
+        order = [blueprint.name for blueprint in
+                 app.extensions['registry']['blueprints']]
+
+        def _key(item):
+            if item.name in order:
+                return order.index(item.name)
+            return -1
+
+        return sorted(items, key=_key)
+
+    app.config.setdefault('COLLECT_FILTER', filter_)
+    app.config.setdefault('COLLECT_STATIC_ROOT', app.static_folder)
     collect.init_app(app)
 
     # unsetting the static_folder so it's not picked up by collect.
     class FakeApp(object):
+        name = "fakeapp"
         has_static_folder = False
         static_folder = None
 
