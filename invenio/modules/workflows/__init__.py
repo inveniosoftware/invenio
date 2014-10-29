e9f2cebcc471b8d6acd5b6b9a420c47eb7591655
Jan Aage Lavik <jan.age.lavik@cern.ch>
2014-10-29 11:24:15 +0100

    workflows: documentation enrichment
    * Enriches the documentation of the Workflows module by adding
      more information on actions and the Holding Pen.
    
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- invenio/modules/workflows/__init__.py
+++ invenio/modules/workflows/__init__.py
@@ -17,8 +17,6 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 r"""
-Create a workflow for your data using functions as individual tasks.
-
 This module allows you to easily push your data through a determined set of tasks
 and stop/continue execution if necessary.
 
@@ -29,8 +27,12 @@ and stop/continue execution if necessary.
 
     Here you can interact with the workflows and data directly via a GUI.
 
+
+=================
 Create a workflow
------------------
+=================
+
+Create a workflow for your data using functions as individual tasks.
 
 .. code-block:: python
 
@@ -61,8 +63,10 @@ underlying `workflows-module`_.
     The workflows registry will make sure pick up any files under `workflows/`.
 
 
-Create tasks
-------------
+=============
+Create a task
+=============
+
 
 The functions in the workflow are called tasks. Each task must *at least*
 take two arguments:
@@ -90,7 +94,7 @@ take two arguments:
 Other parameters may be passed as `*args` or `**kwargs`.
 
 Pass additional arguments
-^^^^^^^^^^^^^^^^^^^^^^^^^
+=========================
 
 To allow arguments being passed to the task from the workflow definition,
 simply wrap your task in a closure:
@@ -109,8 +113,9 @@ It can then be called from the workflow definition as `add_data(20)`,
 returning the inner function.
 
 
-Run it
-------
+==============
+Run a workflow
+==============
 
 Finally, to run your workflow you there are mainly two use-cases:
 
@@ -120,8 +125,8 @@ Finally, to run your workflow you there are mainly two use-cases:
 The former use the :py:class:`.models.BibWorkflowObject` model API, and
 the latter use the :py:mod:`.api`.
 
-Single object
-^^^^^^^^^^^^^
+Run a single data object
+========================
 
 .. note:: This method is recommended if you only have one data
     item you want to run through the workflow.
@@ -144,8 +149,8 @@ To get the data, simply call the `get_data()` function of
     myobj.get_data()  # outputs: 30
 
 
-Multiple objects
-^^^^^^^^^^^^^^^^
+Run multiple data objects
+=========================
 
 .. note:: This method is recommended if you need to run several objects through a workflow.
 
@@ -156,6 +161,8 @@ a list of objects:
 
     from invenio.modules.workflows.api import start
     eng = start(workflow_name="myworkflow", data=[5, 10])
+
+*Here we are passing simple data objects in form integers.*
 
 As usual, the `start()` function returns the `eng` instance that ran the
 workflow. You can query this object to retrieve the data you sent in:
@@ -202,8 +209,8 @@ For example, to retrieve the data from the first object, you can use
     print res
     # outputs: 25
 
-Run asynchronously
-------------------
+Run workflows asynchronously
+============================
 
 So far we have in been running our workflows in the current process. However,
 for long running processes we do not want to wait for the workflow to finish
@@ -228,22 +235,21 @@ such as `Celery`_ that will run the workflow in a separate process.
 In order to communicate with such a task queue we make use of *worker plugins*.
 
 Workers
-^^^^^^^
+=======
 A worker is a plugin (or bridge) from the Invenio workflows module to some
 distributed task queue. By default, we have provided workers for `Celery`_ and
 `RQ`_.
 
-These plugins are used by the :py:mod:`.worker_engine` to launch as task in
-the task queue.
+These plugins are used by the :py:mod:`.worker_engine` to launch workflows
+asynchronously in a task queue.
+
+*We recommend to use Celery as the default asynchronous worker.*
 
 
-Additional features
--------------------
+Working with extra data
+=======================
 
-Add extra data
-^^^^^^^^^^^^^^
-
-If you need to add some additional data to the :py:class:`.models.BibWorkflowObject` that is
+If you need to add some extra data to the :py:class:`.models.BibWorkflowObject` that is
 not suitable to add to the ``obj.data`` attribute, you can make use if the
 ``obj.extra_data`` attribute.
 
@@ -261,22 +267,68 @@ it contains some additional information by default.
         "latest_object": -1,
         "_action": None,
         "redis_search": {},
-        "source": ""
+        "source": "",
+        "_task_history: [],
     }
 
 This information is used by the :py:class:`.models.BibWorkflowObject` to store some additional
-data related to the workflow execution and additional data added by tasks. There
-are two important concepts related to this: actions and task results.
+data related to the workflow execution and additional data added by tasks.
+
+It also stores information that is integrated with Holding Pen - the graphical interface
+for all the data objects.
+
+===========
+Holding Pen
+===========
+
+The graphical interface over all the data objects that have been executed in a workflow.
+
+The name *Holding Pen* originates from a library use case of having some incoming bibliographical
+meta-data records on "hold" - awaiting some human curator to analyze the record and decide if
+the record should be inserted into the repository.
+
+One common usage of this interface is acceptance of user submissions.
+
+We will take this concept of record approval further throughout this guide as we explain the
+most common use cases for the Holding Pen.
+
+.. note:: The Holding Pen is accessible under `/admin/holdingpen`
+
+
+Data object display in Holding Pen
+==================================
+
+To properly represent a data objects in the Holding Pen, the workflow definition
+explained above can be further enriched by adding some static functions to the class.
+
+    * `get_title`: return the "title" of the data object shown in the table display.
+       E.g. title of meta-data record
+    * `get_description`: return a short desciption of the data object shown in the table display.
+       E.g. identifiers and categories
+    * `formatter`: used in the object detailed display to render the data in the object for the user.
+       E.g. the detailed record format of a meta-data record.
+
 
 Actions in Holding Pen
-^^^^^^^^^^^^^^^^^^^^^^
+======================
 
-Actions are simple GUI components that can be assigned to data objects for use
-in Holding Pen. For example, it can be adding a GUI component for accepting or
-rejecting a data object.
+An action in Holding Pen is a generic term describing an action that can be taken
+on a data object.
 
-These actions consists of both a Python back-end and a front-end (jinja templates
-+ JavaScript/CSS).
+To use the example of record approval, we basically mean adding GUI buttons to
+accept or reject a data object. The action taken (the button pressed) on the data object
+is then connected to a custom action back-end that may then decide to
+e.g. continue the workflow or simply delete the object.
+
+.. sidebar:: Approval action
+
+    In our example of the *approval action* we will make use of front-end assets
+    (JavaScript/HTML/templates) to display and listen to events on buttons and
+    a Python back-end plugin to react on the chosen action.
+
+
+Adding an action
+----------------
 
 By default we have added an approval action which can be used to allow a data object
 to continue the workflow or be deleted.
@@ -284,13 +336,13 @@ to continue the workflow or be deleted.
 `workflows/actions/approval.py`
     Action back-end located in ``workflows/actions/approval.py`` that implements
     ``render()``, ``render_mini()`` and ``resolve()``. ``resolve()`` handles the
-    continuation or deletion of the data object.
+    continuation or deletion of the data object using the workflows API.
 
 `templates/workflows/actions/approval_(main|mini|side).html`
     jinja templates used to render the action UI. There are different templates
     in play here depending on position.
 
-        * `mini`: displayed in the main table (for quick action).
+        * `mini`: displayed in the main table (for a quick action).
         * `side`: displayed on the right side of the object details page.
         * `main`: displayed in the middle of the object details page.
 
@@ -298,6 +350,12 @@ to continue the workflow or be deleted.
     JavaScript file listening to events in the approval UI to call the backend
     via ajax calls.
 
+To enable the JavaScript to be loaded via requireJS, you need to override the
+actions "init" JavaScript file `static/workflows/actions/init.js` on your overlay
+and add any initialization code for the action (e.g. attaching events).
+
+Using an action
+---------------
 
 There are two ways of activating an action:
 
@@ -305,16 +363,16 @@ There are two ways of activating an action:
       a parameter that allows you to set an action that needs to be taken in
       the Holding Pen - along with a message to be displayed.
 
-
-    * **Using the :py:class:`.models.BibWorkflowObject` API**. :py:meth:`.models.BibWorkflowObject.set_action`
+    * **Directly using the :py:class:`.models.BibWorkflowObject` API**. :py:meth:`.models.BibWorkflowObject.set_action`
       :py:meth:`.models.BibWorkflowObject.remove_action` :py:meth:`.models.BibWorkflowObject.get_action`.
 
 
-Display results in Holding Pen
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Task results in Holding Pen
+===========================
 
-If you need to add some special task results to be displayed in the Holding Pen, you
-can use the task results API available in :py:class:`.models.BibWorkflowObject`.
+If you want to add some special task results to be displayed on the details page
+of the data object in Holding Pen, you can use the task results API available
+in :py:class:`.models.BibWorkflowObject`.
 
 The API provides functions to manipulate the task results:
 
@@ -326,6 +384,18 @@ The API provides functions to manipulate the task results:
 
 :py:meth:`.models.BibWorkflowObject.get_tasks_results`
     Return all tasks results as a dictionary as ``{ name: [result, ..] }``
+
+
+The *task result* is a dictionary given as context to the template
+when rendered. The result given here is added to a list of results
+for this name.
+
+.. code-block:: python
+
+        obj = BibWorkflowObject()  # or BibWorkflowObject.query.get(id)
+        obj.add_task_result("foo", my_result, "path/to/template")
+
+See sample templates under `templates/workflows/results/*.html`.
 
 
 .. _workflows-module: https://pypi.python.org/pypi/workflow/1.01
