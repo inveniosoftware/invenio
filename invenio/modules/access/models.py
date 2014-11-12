50734c1bc5add79f6a9a505a19905f996f4f641b
Jiri Kuncar <jiri.kuncar@cern.ch>
2014-11-12 16:05:59 +0100

    access: fix PEP8 and PEP257 for models
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/modules/access/models.py
+++ invenio/modules/access/models.py
@@ -17,9 +17,7 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
-"""
-webaccess database models.
-"""
+"""Access database models."""
 
 # General imports.
 from invenio.ext.sqlalchemy import db
@@ -28,100 +26,102 @@ from invenio.ext.sqlalchemy import db
 
 from invenio.modules.accounts.models import User
 
+
 class AccACTION(db.Model):
-    """Represents a AccACTION record."""
+
+    """Represent an access action."""
+
     __tablename__ = 'accACTION'
     id = db.Column(db.Integer(15, unsigned=True),
-                primary_key=True,
-                autoincrement=True)
-    name = db.Column(db.String(32), unique=True,
-                nullable=True)
+                   primary_key=True, autoincrement=True)
+    name = db.Column(db.String(32), unique=True, nullable=True)
     description = db.Column(db.String(255), nullable=True)
     allowedkeywords = db.Column(db.String(255), nullable=True)
     optional = db.Column(db.Enum('yes', 'no', name='yes_no'), nullable=False,
                          server_default='no')
 
+
 class AccARGUMENT(db.Model):
-    """Represents a AccARGUMENT record."""
+
+    """Represent an authorization argument."""
+
     __tablename__ = 'accARGUMENT'
-    id = db.Column(db.Integer(15),  # , unsigned=True),
-                primary_key=True,
-                autoincrement=True)
+    id = db.Column(db.Integer(15), primary_key=True, autoincrement=True)
     keyword = db.Column(db.String(32), nullable=True)
     value = db.Column(db.String(255), nullable=True)
     __table_args__ = (db.Index('KEYVAL', keyword, value),
                       db.Model.__table_args__)
 
+
 class AccMAILCOOKIE(db.Model):
-    """Represents a AccMAILCOOKIE record."""
+
+    """Represent an email cookie."""
+
     __tablename__ = 'accMAILCOOKIE'
-    id = db.Column(db.Integer(15, unsigned=True),
-                primary_key=True,
-                autoincrement=True)
+    id = db.Column(db.Integer(15, unsigned=True), primary_key=True,
+                   autoincrement=True)
     data = db.Column(db.iBinary, nullable=False)
     expiration = db.Column(db.DateTime, nullable=False,
-                server_default='9999-12-31 23:59:59',
-                index=True)
+                           server_default='9999-12-31 23:59:59', index=True)
     kind = db.Column(db.String(32), nullable=False)
-    onetime = db.Column(db.TinyInteger(1), nullable=False,
-                server_default='0')
-    status = db.Column(db.Char(1), nullable=False,
-                server_default='W')
+    onetime = db.Column(db.TinyInteger(1), nullable=False, server_default='0')
+    status = db.Column(db.Char(1), nullable=False, server_default='W')
+
 
 class AccROLE(db.Model):
-    """Represents a AccROLE record."""
+
+    """Represent an access role."""
+
     __tablename__ = 'accROLE'
-    id = db.Column(db.Integer(15, unsigned=True),
-                primary_key=True,
-                autoincrement=True)
-    name = db.Column(db.String(32), unique=True,
-                nullable=True)
+    id = db.Column(db.Integer(15, unsigned=True), primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(32), unique=True, nullable=True)
     description = db.Column(db.String(255), nullable=True)
     firerole_def_ser = db.Column(db.iBinary, nullable=True)
     firerole_def_src = db.Column(db.Text, nullable=True)
 
+
 class AccAuthorization(db.Model):
-    """Represents a AccAssociation record."""
+
+    """Represent an authorization."""
+
     __tablename__ = 'accROLE_accACTION_accARGUMENT'
     id_accROLE = db.Column(db.Integer(15, unsigned=True),
-                db.ForeignKey(AccROLE.id),
-                nullable=True,
-                autoincrement=False,
-                primary_key=True, index=True)
+                           db.ForeignKey(AccROLE.id), nullable=True,
+                           autoincrement=False, primary_key=True, index=True)
     id_accACTION = db.Column(db.Integer(15, unsigned=True),
-                db.ForeignKey(AccACTION.id),
-                nullable=True,
-                autoincrement=False,
-                primary_key=True, index=True)
-    id_accARGUMENT = db.Column(db.Integer(15),  # , unsigned=True),
-                db.ForeignKey(AccARGUMENT.id),
-                nullable=True, primary_key=True,
-                autoincrement=False,
-                index=True)
+                             db.ForeignKey(AccACTION.id), nullable=True,
+                             autoincrement=False, primary_key=True, index=True)
+    id_accARGUMENT = db.Column(db.Integer(15), db.ForeignKey(AccARGUMENT.id),
+                               nullable=True, primary_key=True,
+                               autoincrement=False, index=True)
     argumentlistid = db.Column(db.MediumInteger(8), nullable=True,
-                autoincrement=False,
-                primary_key=True)
+                               autoincrement=False, primary_key=True)
+
     role = db.relationship(AccROLE, backref='authorizations')
     action = db.relationship(AccACTION, backref='authorizations')
     argument = db.relationship(AccARGUMENT, backref='authorizations')
 
+
 class UserAccROLE(db.Model):
-    """Represents a UserAccROLE record."""
+
+    """Represent an user role relationship."""
+
     __tablename__ = 'user_accROLE'
-    id_user = db.Column(db.Integer(15, unsigned=True),
-                db.ForeignKey(User.id),
-                nullable=False, primary_key=True)
+    id_user = db.Column(db.Integer(15, unsigned=True), db.ForeignKey(User.id),
+                        nullable=False, primary_key=True)
     id_accROLE = db.Column(db.Integer(15, unsigned=True),
-                db.ForeignKey(AccROLE.id),
-                nullable=False, primary_key=True)
+                           db.ForeignKey(AccROLE.id), nullable=False,
+                           primary_key=True)
     expiration = db.Column(db.DateTime, nullable=False,
-                server_default='9999-12-31 23:59:59')
+                           server_default='9999-12-31 23:59:59')
     user = db.relationship(User, backref='roles')
     role = db.relationship(AccROLE, backref='users')
 
-__all__ = ['AccACTION',
+
+__all__ = ('AccACTION',
            'AccARGUMENT',
            'AccMAILCOOKIE',
            'AccROLE',
            'AccAuthorization',
-           'UserAccROLE']
+           'UserAccROLE')
