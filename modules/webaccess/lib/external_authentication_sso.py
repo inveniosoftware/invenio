37e761f07fed304859ee5930e04facfc6b2a2a38
Harris Tzovanakis <me@drjova.com>
2014-11-26 14:50:07 +0100

    WebAccess: automatically fetch SSO nicknames
    * Automatically updates user nicknames for SSO authenticated users.
      (PR #2583)
    
    Co-authored-by: Nikolaos Kasioumis <nikolaos.kasioumis@cern.ch>
    Signed-off-by: Harris Tzovanakis <me@drjova.com>
    Tested-by: Tibor Simko <tibor.simko@cern.ch>

--- modules/webaccess/lib/external_authentication_sso.py
+++ modules/webaccess/lib/external_authentication_sso.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2007, 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2007, 2008, 2009, 2010, 2011, 2014 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -69,6 +69,12 @@ CFG_EXTERNAL_AUTH_SSO_LOGIN_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + '
 CFG_EXTERNAL_AUTH_SSO_EMAIL_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + 'EMAIL'
 # Name of the variable containing groups
 CFG_EXTERNAL_AUTH_SSO_GROUP_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + 'GROUP'
+# Name of the variable containing federation
+CFG_EXTERNAL_AUTH_SSO_FEDERATION_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + 'FEDERATION'
+# Name of the variable containing fullname
+CFG_EXTERNAL_AUTH_SSO_FULLNAME_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + 'FULLNAME'
+# Name of the variable containing role
+CFG_EXTERNAL_AUTH_SSO_ROLE_VARIABLE = CFG_EXTERNAL_AUTH_SSO_PREFIX_NAMES[0] + 'ROLE'
 # Separator character for group variable
 CFG_EXTERNAL_AUTH_SSO_GROUPS_SEPARATOR = ';'
 
@@ -131,15 +137,34 @@ class ExternalAuthSSO(ExternalAuth):
         return self._fetch_egroups(req)
 
     def fetch_user_nickname(self, username, password=None, req=None):
-        """Given a username and a password, returns the right nickname belonging
-        to that user (username could be an email).
+        """Given a username and a password, returns the right nickname
+        belonging to that user (username could be an email).
         Note: for SSO the parameter are discarded and overloaded by Shibboleth
         variables
         """
         if req:
             req.add_common_vars()
-            if req.subprocess_env.has_key(CFG_EXTERNAL_AUTH_SSO_LOGIN_VARIABLE):
-                return req.subprocess_env[CFG_EXTERNAL_AUTH_SSO_LOGIN_VARIABLE]
+            # Extract all necessary adfs variables
+            federation = req.subprocess_env.get(
+                CFG_EXTERNAL_AUTH_SSO_FEDERATION_VARIABLE)
+            fullname = req.subprocess_env.get(
+                CFG_EXTERNAL_AUTH_SSO_FULLNAME_VARIABLE)
+            email = req.subprocess_env.get(
+                CFG_EXTERNAL_AUTH_SSO_EMAIL_VARIABLE)
+            role = req.subprocess_env.get(
+                CFG_EXTERNAL_AUTH_SSO_ROLE_VARIABLE)
+            if federation == "CERN" and role == "CERN Users":
+                nickname = fullname
+            else:
+                if fullname != email:
+                    nickname = fullname
+                else:
+                    local_part, domain_part = email.split("@", 1)
+                    joined_name = " ".join(domain_part.split(".")[:-1]).upper()
+                    nickname = "{0} [{1}]".format(
+                        local_part, joined_name
+                    )
+            return nickname
         else:
             return None
 
