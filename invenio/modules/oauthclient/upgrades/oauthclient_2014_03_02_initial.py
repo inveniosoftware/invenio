369619c2168ff6229ae6464e910f5e052ccee456
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2015-02-05 17:10:11 +0100

    oauthclient: upgrade recipe fix
    * Fixes error when trying to get the estimate of the upgrade.
    
    * Enhances initial creation of tables by checking if they exist
      previously.
    
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

--- invenio/modules/oauthclient/upgrades/oauthclient_2014_03_02_initial.py
+++ invenio/modules/oauthclient/upgrades/oauthclient_2014_03_02_initial.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2014 CERN.
+## Copyright (C) 2014, 2015 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -17,6 +17,7 @@
 ## along with Invenio; if not, write to the Free Software Foundation, Inc.,
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
+import warnings
 from sqlalchemy import *
 from invenio.ext.sqlalchemy import db
 from invenio.modules.upgrader.api import op
@@ -30,30 +31,38 @@ def info():
 
 def do_upgrade():
     """ Implement your upgrades here  """
-    op.create_table(
-        'remoteACCOUNT',
-        db.Column('id', db.Integer(display_width=15), nullable=False),
-        db.Column('user_id', db.Integer(display_width=15), nullable=False),
-        db.Column('client_id', db.String(length=255), nullable=False),
-        db.Column('extra_data', db.JSON, nullable=True),
-        db.ForeignKeyConstraint(['user_id'], ['user.id'], ),
-        db.PrimaryKeyConstraint('id'),
-        db.UniqueConstraint('user_id', 'client_id'),
-        mysql_charset='utf8',
-        mysql_engine='MyISAM'
-    )
-    op.create_table(
-        'remoteTOKEN',
-        db.Column('id_remote_account', db.Integer(display_width=15),
-                  nullable=False),
-        db.Column('token_type', db.String(length=40), nullable=False),
-        db.Column('access_token', db.Text(), nullable=False),
-        db.Column('secret', db.Text(), nullable=False),
-        db.ForeignKeyConstraint(['id_remote_account'], ['remoteACCOUNT.id'], ),
-        db.PrimaryKeyConstraint('id_remote_account', 'token_type'),
-        mysql_charset='utf8',
-        mysql_engine='MyISAM'
-    )
+    if not op.has_table('remoteACCOUNT'):
+        op.create_table(
+            'remoteACCOUNT',
+            db.Column('id', db.Integer(display_width=15), nullable=False),
+            db.Column('user_id', db.Integer(display_width=15), nullable=False),
+            db.Column('client_id', db.String(length=255), nullable=False),
+            db.Column('extra_data', db.JSON, nullable=True),
+            db.ForeignKeyConstraint(['user_id'], ['user.id'], ),
+            db.PrimaryKeyConstraint('id'),
+            db.UniqueConstraint('user_id', 'client_id'),
+            mysql_charset='utf8',
+            mysql_engine='MyISAM'
+        )
+    else:
+        warnings.warn("*** Creation of table 'remoteACCOUNT table skipped!'")
+
+    if not op.has_table('remoteTOKEN'):
+        op.create_table(
+            'remoteTOKEN',
+            db.Column('id_remote_account', db.Integer(display_width=15),
+                    nullable=False),
+            db.Column('token_type', db.String(length=40), nullable=False),
+            db.Column('access_token', db.Text(), nullable=False),
+            db.Column('secret', db.Text(), nullable=False),
+            db.ForeignKeyConstraint(['id_remote_account'],
+                                    ['remoteACCOUNT.id'], ),
+            db.PrimaryKeyConstraint('id_remote_account', 'token_type'),
+            mysql_charset='utf8',
+            mysql_engine='MyISAM'
+        )
+    else:
+        warnings.warn("*** Creation of table 'remoteTOKEN' skipped!'")
 
 
 def estimate():
--- invenio/modules/oauthclient/upgrades/oauthclient_2014_10_21_encrypted_token_column.py
+++ invenio/modules/oauthclient/upgrades/oauthclient_2014_10_21_encrypted_token_column.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2014 CERN.
+## Copyright (C) 2014, 2015 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -18,6 +18,7 @@
 ## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 
 from invenio.legacy.dbquery import run_sql
+from invenio.modules.upgrader.api import op
 
 
 depends_on = [u'oauthclient_2014_08_25_extra_data_nullable']
@@ -31,7 +32,8 @@ def do_upgrade():
     """Implement your upgrades here."""
     from invenio.config import SECRET_KEY
     from sqlalchemy_utils.types.encrypted import AesEngine
-    engine = AesEngine(SECRET_KEY)
+    engine = AesEngine()
+    engine._update_key(SECRET_KEY)
     for row in run_sql(
             "SELECT id_remote_account, token_type, access_token "
             "FROM remoteTOKEN"):
@@ -43,6 +45,8 @@ def do_upgrade():
 
 def estimate():
     """Estimate running time of upgrade in seconds (optional)."""
-    return run_sql(
-        "SELECT COUNT(*) AS ids FROM remoteTOKEN"
-    )[0][0]
+    if op.has_table('remoteTOKEN'):
+        return run_sql(
+            "SELECT COUNT(*) AS ids FROM remoteTOKEN"
+        )[0][0]
+    return 1
