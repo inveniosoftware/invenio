c9683ecbac87201ab684f4b7db0210f62b27d055
Esteban J. G. Gabancho <esteban.gabancho@gmail.com>
2015-02-11 16:33:38 +0100

    pidstore: initial release backport
    Signed-off-by: Esteban J. G. Gabancho <esteban.gabancho@gmail.com>

---
+++ modules/miscutil/lib/pid_providers/local_doi.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+##
+## This file is part of Invenio.
+## Copyright (C) 2015 CERN.
+##
+## Invenio is free software; you can redistribute it and/or
+## modify it under the terms of the GNU General Public License as
+## published by the Free Software Foundation; either version 2 of the
+## License, or (at your option) any later version.
+##
+## Invenio is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+## General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Invenio; if not, write to the Free Software Foundation, Inc.,
+## 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+
+"""
+LocalDOI provider.
+"""
+
+from invenio import config
+
+from invenio.pid_provider import LocalPidProvider
+
+
+class LocalDOI(LocalPidProvider):
+    """
+    Provider for locally unmanaged DOIs.
+    """
+    pid_type = 'doi'
+
+    @classmethod
+    def is_provider_for_pid(cls, pid_str):
+        """
+        Check if DOI is not the local datacite managed one.
+        """
+        CFG_DATACITE_DOI_PREFIX = getattr(config,
+                                          'CFG_DATACITE_DOI_PREFIX',
+                                          '10.5072')
+        return pid_str.startswith("%s/" % CFG_DATACITE_DOI_PREFIX)
+
+provider = LocalDOI
