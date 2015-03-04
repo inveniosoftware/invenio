531653f874a1dcda7bb0a5b2a5198afffcf5bd32
Jiri Kuncar <jiri.kuncar@cern.ch>
2015-03-04 17:41:02 +0100

    bibrank: fix legacy import
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- invenio/legacy/bibrank/web/admin/bibrankadmin.py
+++ invenio/legacy/bibrank/web/admin/bibrankadmin.py
@@ -21,7 +21,7 @@ __revision__ = "$Id$"
 
 __lastupdated__ = """$Date$"""
 
-import invenio.bibrankadminlib as brc
+import invenio.legacy.bibrank.adminlib as brc
 #reload(brc)
 from invenio.legacy.webpage import page, error_page
 from invenio.config import CFG_SITE_URL, CFG_SITE_LANG, CFG_SITE_NAME
