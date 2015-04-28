e83f600fea906a03bfb0d59d85fddf2a4e4e95c2
Tibor Simko <tibor.simko@cern.ch>
2015-04-28 13:01:35 +0200

    WebStyle: HttpOnly cookie attribute
    * SECURITY Adds back the `HttpOnly` cookie attribute in order to better
      protect against potential XSS vulnerabilities.  (closes #3064)
    
    Signed-off-by: Tibor Simko <tibor.simko@cern.ch>
    Reviewed-by: Samuele Kaplun <samuele.kaplun@cern.ch>

--- modules/websession/lib/session.py
+++ modules/websession/lib/session.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ## This file is part of Invenio.
-## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010 CERN.
+## Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2015 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -271,7 +271,7 @@ class InvenioSession(dict):
         @return: a session cookie.
         @rtpye: {mod_python.Cookie.Cookie}
         """
-        cookie = Cookie(CFG_WEBSESSION_COOKIE_NAME, self._sid)
+        cookie = Cookie(CFG_WEBSESSION_COOKIE_NAME, self._sid, HttpOnly=True)
         cookie.path = '/'
 
         if self._remember_me:
