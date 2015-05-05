694cabfa3b2df236173a42ebe9b186326ac40fd7
Marco Neumann <marco@crepererum.net>
2015-05-05 16:50:22 +0200

    docker: debug mode
    * Enables debug mode when using the Docker developer setup.
    
    Signed-off-by: Marco Neumann <marco@crepererum.net>

--- scripts/docker_devboot.sh
+++ scripts/docker_devboot.sh
@@ -91,6 +91,8 @@ CFG_WEBSUBMIT_COUNTERSDIR = u'/home/invenio/var/data/submit/counters'
 CFG_WEBSUBMIT_STORAGEDIR = u'/home/invenio/var/data/submit/storage'
 
 DEPOSIT_STORAGEDIR = u'/home/invenio/var/data/deposit/storage'
+
+DEBUG = True
 EOF
 
         # load dev config
