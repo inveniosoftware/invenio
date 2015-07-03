674e5481f2d967d086cdfdd3be1c9457fdd84f90
Mateusz Susik <mateusz.susik@cern.ch>
2015-07-03 10:36:32 +0200

    Orcidutils: removal of additional isbn field.
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>

--- modules/miscutil/lib/orcidutils.py
+++ modules/miscutil/lib/orcidutils.py
@@ -797,7 +797,6 @@ def _get_external_ids(recid, url, recstruct, old_external_ids, orcid):
     doi = _get_doi_for_paper(recid, recstruct)
     # There are two different fields in MARC records responsiple for ISBN id.
     isbn = record_get_field_value(recstruct, '020', '', '', 'a')
-    isbn2 = record_get_field_value(recstruct, '773', '', '', 'z')
     record_ext_ids = record_get_field_instances(recstruct, '037')
     if doi:
         for single_doi in doi:
@@ -818,10 +817,6 @@ def _get_external_ids(recid, url, recstruct, old_external_ids, orcid):
         if isbn in old_external_ids['ISBN']:
             raise OrcidRecordExisting
         external_ids.append(('isbn', encode_for_jinja_and_xml(isbn)))
-    if isbn2:
-        if isbn2 in old_external_ids['ISBN']:
-            raise OrcidRecordExisting
-        external_ids.append(('isbn', encode_for_jinja_and_xml(isbn2)))
 
     for rec in record_ext_ids:
         arxiv = False
