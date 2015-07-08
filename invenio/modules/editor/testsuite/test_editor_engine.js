8e236cb8488c6e0d1072447b3cd2e7e03aefe826
Marco Neumann <marco@crepererum.net>
2015-07-08 08:16:17 +0000

    editor: legacy testsuite removal
    * Removes testsuite for legacy code. (addresses #3233)
    
    Signed-off-by: Marco Neumann <marco@crepererum.net>

--- invenio/modules/editor/testsuite/test_editor_engine.js
+++
@@ -1,7 +0,0 @@
-ValueContainsSubfieldsTest = TestCase("ValueContainsSubfieldsTest");
-
-ValueContainsSubfieldsTest.prototype.testContainsSubfield = function() {
-	var value = "This is a test$$aAnd some subfield content";
-
-	assertEquals(true, valueContainsSubfields(value));
-};
