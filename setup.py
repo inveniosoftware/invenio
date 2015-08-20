662deb267678228b8f4105ee408608f45f4522c5
Jiri Kuncar <jiri.kuncar@cern.ch>
2015-08-20 16:35:06 +0200

    installation: invenio-unapi>=0.1.0
    Signed-off-by: Jiri Kuncar <jiri.kuncar@cern.ch>

--- setup.py
+++ setup.py
@@ -117,6 +117,7 @@ install_requires = [
     "invenio-deposit>=0.1.0",
     "invenio-documents>=0.1.0.post2",
     "invenio-upgrader>=0.1.0",
+    "invenio-unapi>=0.1.0",
     "itsdangerous>=0.24",
     "jellyfish>=0.3.2",
     "Jinja2>=2.7",
