98fbba5a75dc8784e69fd8782e8d287e6b04aa1e
Thorsten Schwander <thorsten.schwander@gmail.com>
2015-10-13 10:32:55 -0600

    BibRank: work around NFS race condition
    Signed-off-by: Thorsten Schwander <thorsten.schwander@gmail.com>

--- modules/bibrank/lib/bibrank_citation_grapher.py
+++ modules/bibrank/lib/bibrank_citation_grapher.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##
 ## This file is part of Invenio.
-## Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011 CERN.
+## Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2015 CERN.
 ##
 ## Invenio is free software; you can redistribute it and/or
 ## modify it under the terms of the GNU General Public License as
@@ -129,7 +129,14 @@ def remove_old_graph_if_needed(filename):
         mtime = os.stat(filename).st_mtime
     except OSError, e:
         # File does not exist is ok
-        if e.errno != 2:
+        if e.errno == 2:
+            pass
+        # NFS race in create/delete/stat occasionally results in
+        # "OSError: [Errno 17] File exists"
+        # that's ok, it means the file is new
+        elif e.errno == 17:
+            pass
+        else:
             raise
         return True
 
