fad4776a7b9af420f8debf7f7a9da82f6b36a259
Samuele Kaplun <samuele.kaplun@cern.ch>
2016-01-11 15:44:00 +0100

    BibAuthorID: current affiliation
    * Correctly sorts institution history list in author details
      considering also whether a given affiliation is the current one.
    
    Signed-off-by: Samuele Kaplun <samuele.kaplun@cern.ch>

--- modules/bibauthorid/lib/bibauthorid_webapi.py
+++ modules/bibauthorid/lib/bibauthorid_webapi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2011, 2012 CERN.
+# Copyright (C) 2011, 2012, 2013, 2014, 2015, 2016 CERN.
 #
 # Invenio is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -1271,7 +1271,7 @@ def institution_history(record):
     field_instances = record_get_field_instances(record, '371', '', '')
     institutions = [extract_institution(x[0]) for x in field_instances if extract_institution(x[0]) is not None]
 
-    institutions.sort(key=lambda x: (x.get('start'), x.get('end')))
+    institutions.sort(key=lambda x: (x.get('current'), x.get('start'), x.get('end')))
 
     return institutions
 
