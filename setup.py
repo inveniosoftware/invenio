914701b224bcd56f0cba3a9c3c7deacfe2d69012
Alizee Pace <alizee.pace@gmail.com>
2016-08-24 10:32:34 +0200

    tests: integration of pytest-runner
    Signed-off-by: Alizee Pace <alizee.pace@gmail.com>

--- setup.py
+++ setup.py
@@ -110,6 +110,7 @@ for name, reqs in extras_require.items():
 # Flask application loading).
 #
 setup_requires = [
+    'pytest-runner>=2.6.2',
     'Babel>=1.3',
 ]
 
@@ -121,40 +122,6 @@ install_requires = [
 ]
 
 packages = find_packages()
-
-
-class PyTest(TestCommand):
-    """PyTest Test."""
-
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        """Init pytest."""
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-        try:
-            from ConfigParser import ConfigParser
-        except ImportError:
-            from configparser import ConfigParser
-        config = ConfigParser()
-        config.read('pytest.ini')
-        self.pytest_args = config.get('pytest', 'addopts').split(' ')
-
-    def finalize_options(self):
-        """Finalize pytest."""
-        TestCommand.finalize_options(self)
-        if hasattr(self, '_test_args'):
-            self.test_suite = ''
-        else:
-            self.test_args = []
-            self.test_suite = True
-
-    def run_tests(self):
-        """Run tests."""
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 # Get the version string. Cannot be done with import!
 g = {}
@@ -197,5 +164,4 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Development Status :: 3 - Alpha',
     ],
-    cmdclass={'test': PyTest},
 )
