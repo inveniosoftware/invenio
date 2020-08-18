0df2bdedbf3b40ea970eed1883f465e24e26d100
Diego Rodriguez <diego.rodriguez@cern.ch>
2020-08-18 17:07:14 +0200

    tests: fix isort compatibility

--- run-tests.sh
+++ run-tests.sh
@@ -8,7 +8,7 @@
 # under the terms of the MIT License; see LICENSE file for more details.
 
 pydocstyle invenio && \
-isort -rc -c -df **/*.py && \
+isort invenio tests --check-only --diff && \
 check-manifest --ignore ".travis-*" && \
 sphinx-build -qnNW docs docs/_build/html && \
 python setup.py test && \
