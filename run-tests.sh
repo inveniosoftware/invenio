c55f68e556820f6ff25fa07542c1506aa7360c2d
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2020-12-10 14:14:55 +0100

    github: github actions migration

--- run-tests.sh
+++ run-tests.sh
@@ -7,7 +7,21 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-pytest && \
-sphinx-build -qnNW -b doctest docs docs/_build/doctest
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+# Always bring down services
+function cleanup {
+  eval "$(docker-services-cli down --env)"
+}
+trap cleanup EXIT
+
+python -m check_manifest --ignore ".*-requirements.txt"
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+eval "$(docker-services-cli up --db ${DB:-postgresql} --search ${SEARCH:-elasticsearch} --cache ${CACHE:-redis} --mq ${MQ:-rabbitmq} --env)"
+python -m pytest
+tests_exit_code=$?
+exit "$tests_exit_code"
