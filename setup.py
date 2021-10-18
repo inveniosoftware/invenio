e624a74c6988d1952ddc8f7e58ffb7cd7a498aff
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2021-10-18 17:05:10 +0200

    installation: bump versions

--- setup.py
+++ setup.py
@@ -18,43 +18,40 @@ tests_require = [
     'pytest-invenio>=1.4.0,<1.5.0',
 ]
 
-db_version = '>=1.0.8,<1.1.0'
-search_version = '>=1.4.1,<1.5.0'
+db_version = '>=1.0.9,<1.1.0'
+search_version = '>=1.4.2,<1.5.0'
 
 extras_require = {
     # Bundles
     'base': [
         'invenio-admin>=1.3.0,<1.4.0',
-        'invenio-assets>=1.2.5,<1.3.0',
-        'invenio-formatter>=1.1.0,<1.2.0',
+        'invenio-assets>=1.2.7,<1.3.0',
+        'invenio-formatter>=1.1.3,<1.2.0',
         'invenio-logging>=1.3.0,<1.4.0',
         'invenio-mail>=1.0.2,<1.1.0',
-        'invenio-rest>=1.2.3,<1.3.0',
-        'invenio-theme>=1.3.5,<1.4.0',
+        'invenio-rest>=1.2.4,<1.3.0',
+        'invenio-theme>=1.3.9,<1.4.0',
     ],
     'auth': [
-        'invenio-access>=1.4.1,<1.5.0',
-        'invenio-accounts>=1.4.3,<1.5.0',
-        'invenio-oauth2server>=1.3.2,<1.4.0',
-        'invenio-oauthclient>=1.5.0,<1.6.0',
-        'invenio-userprofiles>=1.2.1,<1.3.0',
+        'invenio-access>=1.4.2,<1.5.0',
+        'invenio-accounts>=1.4.8,<1.5.0',
+        'invenio-oauth2server>=1.3.4,<1.4.0',
+        'invenio-oauthclient>=1.5.3,<1.6.0',
+        'invenio-userprofiles>=1.2.4,<1.3.0',
     ],
     'metadata': [
-        'invenio-indexer>=1.2.0,<1.3.0',
-        'invenio-jsonschemas>=1.1.1,<1.2.0',
-        'invenio-oaiserver>=1.2.0,<1.3.0',
-        'invenio-pidstore>=1.2.1,<1.3.0',
+        'invenio-indexer>=1.2.1,<1.3.0',
+        'invenio-jsonschemas>=1.1.3,<1.2.0',
+        'invenio-oaiserver>=1.3.0,<1.4.0',
+        'invenio-pidstore>=1.2.2,<1.3.0',
         'invenio-records-rest>=1.8.0,<1.9.0',
         'invenio-records-ui>=1.2.0,<1.3.0',
-        # Note: Invenio-Records v1.5.x is allowed on purpose in v3.4 to
-        # allow the relations support to be released once it's more mature
-        # without having to release Invenio v3.5.
-        'invenio-records>=1.4.0,<1.6.0',
-        'invenio-search-ui>=2.0.0,<2.1.0',
+        'invenio-records>=1.6.0,<1.7.0',
+        'invenio-search-ui>=2.0.3,<2.1.0',
     ],
     'files': [
-        'invenio-files-rest>=1.2.0,<1.3.0',
-        'invenio-iiif>=1.1.0,<1.2.0',
+        'invenio-files-rest>=1.3.0,<1.4.0',
+        'invenio-iiif>=1.2.0,<1.3.0',
         'invenio-previewer>=1.3.2,<1.4.0',
         'invenio-records-files>=1.2.1,<1.3.0',
     ],
@@ -95,11 +92,11 @@ for name, reqs in extras_require.items():
 
 install_requires = [
     'invenio-app>=1.3.1,<1.4.0',
-    'invenio-base>=1.2.4,<1.3.0',
+    'invenio-base>=1.2.5,<1.3.0',
     'invenio-cache>=1.1.0,<1.2.0',
-    'invenio-celery>=1.2.2,<1.3.0',
+    'invenio-celery>=1.2.3,<1.3.0',
     'invenio-config>=1.0.3,<1.1.0',
-    'invenio-i18n>=1.3.0,<1.4.0',
+    'invenio-i18n>=1.3.1,<1.4.0',
 ]
 
 packages = find_packages()
